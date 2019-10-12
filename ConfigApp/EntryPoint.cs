81b9898bfee6f73d0f93b8b7ee01bf0b2a5155e5
pongo1231 <pongo1999712@gmail.com>
2019-10-12 16:59:34 +0200

    Only allow one instance of config app

---
+++ ConfigApp/EntryPoint.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace ConfigApp
+{
+    public class EntryPoint
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Mutex mutex = new Mutex(false, "com.pongo1231.chaosmodv");
+
+            if (!mutex.WaitOne(TimeSpan.FromSeconds(5), false))
+            {
+                return;
+            }
+
+            try
+            {
+                App app = new App();
+                app.InitializeComponent();
+                app.Run();
+            }
+            finally
+            {
+                mutex.ReleaseMutex();
+            }
+        }
+    }
+}
