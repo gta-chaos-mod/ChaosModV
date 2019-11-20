7c037f50c89b616e06d17c31fcabd8f80ec72a5f
pongo1231 <pongo1999712@gmail.com>
2019-11-20 16:26:53 +0100

    Dump of some chat voting related work

---
+++ ConfigApp/TwitchVoting.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using TwitchLib.Client;
+
+namespace ConfigApp
+{
+    public class TwitchVoting
+    {
+        private Thread _Thread;
+
+        public TwitchVoting()
+        {
+            _Thread = new Thread(TwitchLoop);
+        }
+
+        public void Init()
+        {
+            _Thread.Start();
+        }
+
+        private void TwitchLoop()
+        {
+            /*TwitchClient client = new TwitchClient();
+            while (true)
+            {
+
+            }*/
+        }
+    }
+}
