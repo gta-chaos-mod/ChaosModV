add8db271fdb1a7b0dbce36510e26b6cb1c72760
pongo1231 <pongo1999712@gmail.com>
2020-02-19 17:34:53 +0100

    Merge launch player up and launch current vehicle up effects together

--- ChaosMod/Effects/db/Player/PlayerLaunchUp.cpp
+++ ChaosMod/Effects/db/Player/PlayerLaunchUp.cpp
@@ -2,7 +2,16 @@
 
 static void OnStart()
 {
-	SET_ENTITY_VELOCITY(PLAYER_PED_ID(), .0f, .0f, 10000.f);
+	auto playerPed = PLAYER_PED_ID();
+
+	if (IS_PED_IN_ANY_VEHICLE(playerPed, false))
+	{
+		SET_ENTITY_VELOCITY(GET_VEHICLE_PED_IS_IN(playerPed, false), .0f, .0f, 10000.f);
+	}
+	else
+	{
+		SET_ENTITY_VELOCITY(playerPed, .0f, .0f, 10000.f);
+	}
 }
 
 static RegisterEffect registerEffect(EFFECT_PLAYER_LAUNCH, OnStart);
