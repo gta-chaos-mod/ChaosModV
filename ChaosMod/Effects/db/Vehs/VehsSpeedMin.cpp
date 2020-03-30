ee02480a79811b840263695915c725bc5a6a217e
Lucas7yoshi <Lucas7yoshi@gmail.com>
2020-03-30 02:28:02 -0400

    CEO of too many commits, changes to text and stuff and yeah

--- ChaosMod/Effects/db/Vehs/VehsSpeedMin.cpp
+++ ChaosMod/Effects/db/Vehs/VehsSpeedMin.cpp
@@ -41,7 +41,7 @@ static void OnTick()
 		else
 		{
 			overlaycolor = 25;
-			timeReserve += tickDelta;
+			timeReserve += tickDelta / 2; //slows down regaining time
 			if (timeReserve > WAIT_TIME)
 			{
 				timeReserve = WAIT_TIME;
@@ -60,13 +60,17 @@ static void OnTick()
 		sprintf_s(charBuf, "%.1f mph", MS_TO_MPH(speedms));
 		SCALEFORM_MOVIE_METHOD_ADD_PARAM_PLAYER_NAME_STRING(charBuf);
 
-		if (timeReserve != WAIT_TIME)
+		if (timeReserve != WAIT_TIME && speedms < minSpeed)
 		{
-			sprintf_s(charBuf, "Minimum: %.1f mph\n%.1f seconds to detonation", MS_TO_MPH(minSpeed), double(timeReserve) / 1000);
+			sprintf_s(charBuf, "Minimum: %1.f mph\nDetonation In: %.1fs", MS_TO_MPH(minSpeed), double(timeReserve) / 1000);
+		}
+		else if (timeReserve != WAIT_TIME && speedms > minSpeed)
+		{
+			sprintf_s(charBuf, "Minimum: %1.f mph\nDetonation In: %.1fs (Recovering)", MS_TO_MPH(minSpeed), double(timeReserve) / 1000);
 		}
 		else
 		{
-			sprintf_s(charBuf, "Minimum: %.1f mph", MS_TO_MPH(minSpeed));
+			sprintf_s(charBuf, "Minimum: %1.f mph", MS_TO_MPH(minSpeed));
 		}
 		SCALEFORM_MOVIE_METHOD_ADD_PARAM_PLAYER_NAME_STRING(charBuf);
 		SCALEFORM_MOVIE_METHOD_ADD_PARAM_INT(overlaycolor);
@@ -80,4 +84,9 @@ static void OnTick()
 	}
 }
 
-static RegisterEffect registerEffect(EFFECT_VEH_SPEED_MINIMUM, nullptr, nullptr, OnTick);
+static void OnStop()
+{
+	static DWORD64 timeReserve = WAIT_TIME;
+}
+
+static RegisterEffect registerEffect(EFFECT_VEH_SPEED_MINIMUM, nullptr, OnStop, OnTick);
