009d3604faf8a4b9bd1164122a900a9e34d20bba
Lucas7yoshi <Lucas7yoshi@gmail.com>
2020-04-14 03:38:18 -0400

    Optimize and improve earthquake.

--- ChaosMod/Effects/db/Misc/MiscEarthquake.cpp
+++ ChaosMod/Effects/db/Misc/MiscEarthquake.cpp
@@ -1,5 +1,7 @@
 #include <stdafx.h>
 
+#define ApplyForce(entity, force) APPLY_FORCE_TO_ENTITY_CENTER_OF_MASS(entity, 1, 0, 0, force, true, false, true, true)
+
 static void OnStop()
 {
 	CAM:STOP_GAMEPLAY_CAM_SHAKING(true);
@@ -8,19 +10,18 @@ static void OnStop()
 static void OnTick()
 {
 	CAM::SHAKE_GAMEPLAY_CAM("LARGE_EXPLOSION_SHAKE", 0.05);
-	auto playerPed = PLAYER_PED_ID();
-	auto playerVeh = GET_VEHICLE_PED_IS_IN(playerPed, false);
+	auto shook = GET_RANDOM_FLOAT_IN_RANGE(-9.f, 7.f); // low slightly lower than oppisite of upper to decrease chances of stuff going into space.
 	for (auto veh : GetAllVehs())
 	{
-		APPLY_FORCE_TO_ENTITY_CENTER_OF_MASS(veh, 1, 0, 0, GET_RANDOM_FLOAT_IN_RANGE(-1500.f, 1500.f), true, false, false, true);
+		ApplyForce(veh, shook);
 	}
 	for (auto prop : GetAllProps())
 	{
-		APPLY_FORCE_TO_ENTITY_CENTER_OF_MASS(prop, 1, 0, 0, GET_RANDOM_FLOAT_IN_RANGE(-750.f, 500.f), true, false, false, true);
+		ApplyForce(prop, shook);
 	}
 	for (auto ped : GetAllPeds())
 	{
-		APPLY_FORCE_TO_ENTITY_CENTER_OF_MASS(ped, 1, 0, 0, GET_RANDOM_FLOAT_IN_RANGE(-1000.f, 750.f), true, false, false, true);
+		ApplyForce(ped, shook);
 	}
 }
 
