90457bc2995a19536baa65d71d550612e545643f
pongo1231 <pongo1999712@gmail.com>
2020-05-17 20:10:42 +0200

    Changes to 30mph speed limit effect

--- ChaosMod/Effects/db/Vehs/Vehs30mphLimit.cpp
+++ ChaosMod/Effects/db/Vehs/Vehs30mphLimit.cpp
@@ -2,17 +2,15 @@
 
 static void OnTick()
 {
-	auto ped = PLAYER_PED_ID();
-	if (IS_PED_IN_ANY_VEHICLE(ped, false))
+	for (Vehicle veh : GetAllVehs())
 	{
-		_SET_VEHICLE_MAX_SPEED(GET_VEHICLE_PED_IS_IN(ped, false), 13.41); // 13.41 Meters Per Second = 30 MPH~
+		_SET_VEHICLE_MAX_SPEED(veh, 13.41); // 13.41 Meters Per Second = 30 MPH~
 	}
 }
 
 static void OnStop()
 {
-	// Cycles through all vehicles and sets their max speed to what it should be in case player changes vehicles mid effect
-	for (auto veh : GetAllVehs())
+	for (Vehicle veh : GetAllVehs())
 	{
 		_SET_VEHICLE_MAX_SPEED(veh, GET_VEHICLE_MODEL_ESTIMATED_MAX_SPEED(GET_ENTITY_MODEL(veh)));
 	}
