528e6d19f80e5cac17a0bb062a26b65768f93725
pongo1231 <pongo1999712@gmail.com>
2020-06-19 23:05:49 +0200

    ChaosMod: Changes to No HUD effect

--- ChaosMod/Effects/db/Misc/MiscNoHUD.cpp
+++ ChaosMod/Effects/db/Misc/MiscNoHUD.cpp
@@ -3,6 +3,10 @@
 static void OnTick()
 {
 	HIDE_HUD_AND_RADAR_THIS_FRAME();
+
+	// Ensure player can't use pause menu map either
+	DISABLE_CONTROL_ACTION(0, 199, true);
+	DISABLE_CONTROL_ACTION(0, 200, true);
 }
 
 static RegisterEffect registerEffect(EFFECT_NO_HUD, nullptr, nullptr, OnTick);
