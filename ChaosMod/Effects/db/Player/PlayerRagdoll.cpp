888833b50d941234a272ea48979dafe9ce4a809b
pongo1231 <pongo1999712@gmail.com>
2020-06-22 12:01:58 +0200

    ChaosMod: Changes to Ragdoll effect

--- ChaosMod/Effects/db/Player/PlayerRagdoll.cpp
+++ ChaosMod/Effects/db/Player/PlayerRagdoll.cpp
@@ -2,12 +2,9 @@
 
 static void OnStart()
 {
-	auto playerPed = PLAYER_PED_ID();
+	Ped playerPed = PLAYER_PED_ID();
 
-	if (!IS_PED_IN_ANY_VEHICLE(playerPed, false))
-	{
-		SET_PED_TO_RAGDOLL(playerPed, 10000, 10000, 0, true, true, false);
-	}
+	SET_PED_TO_RAGDOLL(playerPed, 10000, 10000, 0, true, true, false);
 }
 
 static RegisterEffect registerEffect(EFFECT_PLAYER_RAGDOLL, OnStart);
