cbc553b623b00cdc6c443b3dd5acd662494a1db7
pongo1231 <pongo1999712@gmail.com>
2020-07-06 23:38:08 +0200

    ChaosMod: Changes to effects
    You Are Famous is timed now and peds will also attempt to get into your vehicle, also You Are Famous & Scooter Brothers should reset AI of mission peds again once they are disturbed from their current task

--- ChaosMod/Effects/db/Peds/PedsFollowPlayer.cpp
+++ ChaosMod/Effects/db/Peds/PedsFollowPlayer.cpp
@@ -1,17 +1,55 @@
 #include <stdafx.h>
 
-static void OnStart()
+static void OnStop()
 {
-	auto playerPed = PLAYER_PED_ID();
-
 	for (Ped ped : GetAllPeds())
 	{
 		if (!IS_PED_A_PLAYER(ped))
 		{
-			TASK_FOLLOW_TO_OFFSET_OF_ENTITY(ped, playerPed, .0f, .0f, .0f, 9999.f, -1, .0f, true);
-			SET_PED_KEEP_TASK(ped, true);
+			SET_BLOCKING_OF_NON_TEMPORARY_EVENTS(ped, false);
 		}
 	}
 }
 
-static RegisterEffect registerEffect(EFFECT_PEDS_FOLLOW_PLAYER, OnStart);
+static void OnTick()
+{
+	Ped playerPed = PLAYER_PED_ID();
+	bool isPlayerInAnyVeh = IS_PED_IN_ANY_VEHICLE(playerPed, false);
+	Vehicle playerVeh = GET_VEHICLE_PED_IS_IN(playerPed, false);
+
+	static DWORD64 lastTick = GetTickCount64();
+	DWORD64 curTick = GetTickCount64();
+
+	if (lastTick < curTick - 2000)
+	{
+		lastTick = curTick;
+
+		int count = 5;
+
+		for (Ped ped : GetAllPeds())
+		{
+			if (!IS_PED_A_PLAYER(ped))
+			{
+				if (isPlayerInAnyVeh)
+				{
+					TASK_ENTER_VEHICLE(ped, playerVeh, -1, -2, 9999.f, 1, 0);
+				}
+				else
+				{
+					TASK_FOLLOW_TO_OFFSET_OF_ENTITY(ped, playerPed, .0f, .0f, .0f, 9999.f, -1, .0f, true);
+				}
+
+				SET_BLOCKING_OF_NON_TEMPORARY_EVENTS(ped, true);
+
+				if (--count == 0)
+				{
+					count = 5;
+
+					WAIT(0);
+				}
+			}
+		}
+	}
+}
+
+static RegisterEffect registerEffect(EFFECT_PEDS_FOLLOW_PLAYER, nullptr, OnStop, OnTick);
