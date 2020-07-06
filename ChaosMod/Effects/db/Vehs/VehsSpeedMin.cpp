8080071463487c80106afc1d376b6a398efba6fe
pongo1231 <pongo1999712@gmail.com>
2020-07-06 22:02:09 +0200

    ChaosMod: Changes to Need For Speed
    Fixed constant exploding when respawning inside previously exploded vehicle because of mission or so

--- ChaosMod/Effects/db/Vehs/VehsSpeedMin.cpp
+++ ChaosMod/Effects/db/Vehs/VehsSpeedMin.cpp
@@ -6,7 +6,7 @@
 #include <cmath>
 
 #define WAIT_TIME 6000 // ms
-#define SPEED_THRESHOLD 0.50 // % of max speed must be reached
+#define SPEED_THRESHOLD 0.5f // % of max speed must be reached
 
 #define MPH_TO_MS(mph) mph / 2.236936
 #define MS_TO_MPH(ms) ms * 2.236936
@@ -14,66 +14,73 @@
 static int m_overlay = 0;
 static Vehicle m_lastVeh;
 
+static DWORD64 m_timeReserve;
+
 static inline bool Beepable(DWORD64 reserveValue)
 {
-	return std::remainder(std::log(double(reserveValue)) / std::log(1.0019), 100) < 20;
+	return std::remainder(std::log(double(reserveValue)) / std::log(1.0019f), 100) < 20;
 }
 
 static void OnTick()
 {
 	static DWORD64 lastTick = GetTickCount64();
-	static DWORD64 timeReserve = WAIT_TIME;
+	static bool enteredVehicle = false;
 
 	Ped playerPed = PLAYER_PED_ID();
-	if (IS_PED_DEAD_OR_DYING(playerPed, 1))
-	{
-		m_lastVeh = 0;
-		return;
-	}
-
 	Vehicle veh = GET_VEHICLE_PED_IS_IN(playerPed, false);
+
 	if (m_lastVeh != 0 && veh != m_lastVeh)
 	{
 		EXPLODE_VEHICLE(m_lastVeh, true, false);
-
-		m_lastVeh = 0;
 	}
 
-	if (IS_PED_IN_ANY_VEHICLE(playerPed, false) && GET_IS_VEHICLE_ENGINE_RUNNING(veh) && !IS_PED_IN_ANY_HELI(playerPed) && !IS_PED_IN_ANY_PLANE(playerPed))
+	if (!IS_PED_DEAD_OR_DYING(playerPed, false) && IS_PED_IN_ANY_VEHICLE(playerPed, false) && GET_IS_VEHICLE_ENGINE_RUNNING(veh))
 	{
+		if (!enteredVehicle)
+		{
+			enteredVehicle = true;
+
+			m_lastVeh = 0;
+			m_timeReserve = WAIT_TIME;
+			lastTick = GetTickCount64();
+
+			return;
+		}
+
 		m_lastVeh = veh;
 
-		auto minSpeed = GET_VEHICLE_MODEL_ESTIMATED_MAX_SPEED(GET_ENTITY_MODEL(veh)) * SPEED_THRESHOLD;
-		auto speedms = GET_ENTITY_SPEED(veh);
+		float minSpeed = GET_VEHICLE_MODEL_ESTIMATED_MAX_SPEED(GET_ENTITY_MODEL(veh)) * SPEED_THRESHOLD;
+		float speedms = GET_ENTITY_SPEED(veh);
 		DWORD64 currentTick = GetTickCount64();
-		auto tickDelta = currentTick - lastTick;
-		auto overlaycolor = 0;
+		DWORD64 tickDelta = currentTick - lastTick;
+		int overlaycolor = 0;
 		if (speedms < minSpeed)
 		{
 			overlaycolor = 75;
-			if (timeReserve < tickDelta)
+			if (m_timeReserve < tickDelta)
 			{
 				EXPLODE_VEHICLE(veh, true, false);
-				timeReserve = WAIT_TIME;
+				m_timeReserve = WAIT_TIME;
 				return;
 			}
 			
-			if (Beepable(timeReserve - tickDelta) && !Beepable(timeReserve))
+			if (Beepable(m_timeReserve - tickDelta) && !Beepable(m_timeReserve))
 			{
 				PLAY_SOUND_FRONTEND(-1, "Beep_Red", "DLC_HEIST_HACKING_SNAKE_SOUNDS", true);
 			}
 
-		    timeReserve -= tickDelta;
+			m_timeReserve -= tickDelta;
 		}
 		else
 		{
 			overlaycolor = 25;
-			timeReserve += tickDelta / 2; //slows down regaining time
-			if (timeReserve > WAIT_TIME)
+			m_timeReserve += tickDelta / 2; //slows down regaining time
+			if (m_timeReserve > WAIT_TIME)
 			{
-				timeReserve = WAIT_TIME;
+				m_timeReserve = WAIT_TIME;
 			}
 		}
+
 		lastTick = currentTick;
 
 		BEGIN_SCALEFORM_MOVIE_METHOD(m_overlay, "SHOW_SHARD_RANKUP_MP_MESSAGE");
@@ -82,13 +89,13 @@ static void OnTick()
 		sprintf_s(charBuf, "%.1f mph", MS_TO_MPH(speedms));
 		SCALEFORM_MOVIE_METHOD_ADD_PARAM_PLAYER_NAME_STRING(charBuf);
 
-		if (timeReserve != WAIT_TIME && speedms < minSpeed)
+		if (m_timeReserve != WAIT_TIME && speedms < minSpeed)
 		{
-			sprintf_s(charBuf, "Minimum: %1.f mph\nDetonation In: %.1fs", MS_TO_MPH(minSpeed), double(timeReserve) / 1000);
+			sprintf_s(charBuf, "Minimum: %1.f mph\nDetonation In: %.1fs", MS_TO_MPH(minSpeed), float(m_timeReserve) / 1000);
 		}
-		else if (timeReserve != WAIT_TIME && speedms > minSpeed)
+		else if (m_timeReserve != WAIT_TIME && speedms > minSpeed)
 		{
-			sprintf_s(charBuf, "Minimum: %1.f mph\nDetonation In: %.1fs (Recovering)", MS_TO_MPH(minSpeed), double(timeReserve) / 1000);
+			sprintf_s(charBuf, "Minimum: %1.f mph\nDetonation In: %.1fs (Recovering)", MS_TO_MPH(minSpeed), float(m_timeReserve) / 1000);
 		}
 		else
 		{
@@ -101,14 +108,8 @@ static void OnTick()
 	}
 	else
 	{
-		lastTick = GetTickCount64();
-		timeReserve = WAIT_TIME;
+		enteredVehicle = false;
 	}
-}
-
-static void OnStop()
-{
-	static DWORD64 timeReserve = WAIT_TIME;
 }
 
 static void OnStart()
@@ -120,6 +121,7 @@ static void OnStart()
 	}
 
 	m_lastVeh = 0;
+	m_timeReserve = WAIT_TIME;
 }
 
-static RegisterEffect registerEffect(EFFECT_VEH_SPEED_MINIMUM, OnStart, OnStop, OnTick);
+static RegisterEffect registerEffect(EFFECT_VEH_SPEED_MINIMUM, OnStart, nullptr, OnTick);
