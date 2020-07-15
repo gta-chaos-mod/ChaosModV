29703893d36023a724cc3145e68557fdfd5d42b4
pongo1231 <pongo1999712@gmail.com>
2020-07-15 21:20:11 +0200

    ChaosMod: Changes to You Are Famous
    Now makes peds who stole your car stay in it & drive away

--- ChaosMod/Effects/db/Peds/PedsFollowPlayer.cpp
+++ ChaosMod/Effects/db/Peds/PedsFollowPlayer.cpp
@@ -1,5 +1,12 @@
 #include <stdafx.h>
 
+static Vehicle m_savedPlayerVeh;
+
+static void OnStart()
+{
+	m_savedPlayerVeh = 0;
+}
+
 static void OnStop()
 {
 	for (Ped ped : GetAllPeds())
@@ -17,6 +24,11 @@ static void OnTick()
 	bool isPlayerInAnyVeh = IS_PED_IN_ANY_VEHICLE(playerPed, false);
 	Vehicle playerVeh = GET_VEHICLE_PED_IS_IN(playerPed, false);
 
+	if (isPlayerInAnyVeh)
+	{
+		m_savedPlayerVeh = playerVeh;
+	}
+
 	static DWORD64 lastTick = GetTickCount64();
 	DWORD64 curTick = GetTickCount64();
 
@@ -24,15 +36,27 @@ static void OnTick()
 	{
 		lastTick = curTick;
 
-		int count = 5;
+		int count = 3;
 
 		for (Ped ped : GetAllPeds())
 		{
 			if (!IS_PED_A_PLAYER(ped))
 			{
-				if (isPlayerInAnyVeh)
+				bool isPedInAnyVeh = IS_PED_IN_ANY_VEHICLE(ped, true);
+				bool isPedGettingInAnyVeh = IS_PED_GETTING_INTO_A_VEHICLE(ped);
+				Vehicle pedVeh = GET_VEHICLE_PED_IS_IN(ped, false);
+				Vehicle pedTargetVeh = GET_VEHICLE_PED_IS_ENTERING(ped);
+
+				if (isPlayerInAnyVeh && (!isPedInAnyVeh || pedVeh != m_savedPlayerVeh) && (!isPedGettingInAnyVeh || pedTargetVeh != m_savedPlayerVeh))
 				{
-					TASK_ENTER_VEHICLE(ped, playerVeh, -1, -2, 9999.f, 1, 0);
+					TASK_ENTER_VEHICLE(ped, m_savedPlayerVeh, -1, -2, 2.f, 1, 0);
+				}
+				else if ((isPedInAnyVeh && pedVeh == m_savedPlayerVeh) || (isPedGettingInAnyVeh && pedTargetVeh == m_savedPlayerVeh))
+				{
+					if (GET_PED_IN_VEHICLE_SEAT(m_savedPlayerVeh, -1, 0) == ped)
+					{
+						TASK_VEHICLE_DRIVE_WANDER(ped, m_savedPlayerVeh, 9999.f, 10);
+					}
 				}
 				else
 				{
@@ -43,7 +67,7 @@ static void OnTick()
 
 				if (--count == 0)
 				{
-					count = 5;
+					count = 3;
 
 					WAIT(0);
 				}
@@ -52,4 +76,4 @@ static void OnTick()
 	}
 }
 
-static RegisterEffect registerEffect(EFFECT_PEDS_FOLLOW_PLAYER, nullptr, OnStop, OnTick);
+static RegisterEffect registerEffect(EFFECT_PEDS_FOLLOW_PLAYER, OnStart, OnStop, OnTick);
