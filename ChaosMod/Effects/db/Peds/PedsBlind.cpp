6ccc584b1b36848f9f516527f91e3c8047559eff
pongo1231 <pongo1999712@gmail.com>
2020-07-15 17:45:58 +0200

    ChaosMod: Changes to effects
    Merged together Peds Ignore Player and Peds Don't See Very Well into 1 effect along with new additions to it

--- ChaosMod/Effects/db/Peds/PedsBlind.cpp
+++ ChaosMod/Effects/db/Peds/PedsBlind.cpp
@@ -1,27 +1,54 @@
 #include <stdafx.h>
 
-static void OnStop()
+static void OnStart()
 {
-	for (auto ped : GetAllPeds())
+	for (Ped ped : GetAllPeds())
 	{
 		if (!IS_PED_A_PLAYER(ped))
 		{
-			SET_PED_SEEING_RANGE(ped, 1000.f);
-			SET_PED_HEARING_RANGE(ped, 1000.f);
+			CLEAR_PED_TASKS(ped);
+
+			SET_BLOCKING_OF_NON_TEMPORARY_EVENTS(ped, true);
+		}
+	}
+}
+
+static void OnStop()
+{
+	SET_EVERYONE_IGNORE_PLAYER(PLAYER_ID(), false);
+
+	for (Ped ped : GetAllPeds())
+	{
+		if (!IS_PED_A_PLAYER(ped))
+		{
+			SET_PED_SEEING_RANGE(ped, 9999.f);
+			SET_PED_HEARING_RANGE(ped, 9999.f);
+
+			SET_BLOCKING_OF_NON_TEMPORARY_EVENTS(ped, false);
+			SET_PED_SHOOT_RATE(ped, 100);
+			SET_PED_FIRING_PATTERN(ped, -957453492);
+			SET_PED_COMBAT_ATTRIBUTES(ped, 1424, true);
 		}
 	}
 }
 
 static void OnTick()
 {
-	for (auto ped : GetAllPeds())
+	SET_EVERYONE_IGNORE_PLAYER(PLAYER_ID(), true);
+
+	for (Ped ped : GetAllPeds())
 	{
 		if (!IS_PED_A_PLAYER(ped))
 		{
 			SET_PED_SEEING_RANGE(ped, .0f);
 			SET_PED_HEARING_RANGE(ped, .0f);
+
+			SET_BLOCKING_OF_NON_TEMPORARY_EVENTS(ped, true);
+			SET_PED_SHOOT_RATE(ped, 0);
+			SET_PED_FIRING_PATTERN(ped, -490063247);
+			SET_PED_COMBAT_ATTRIBUTES(ped, 1424, false);
 		}
 	}
 }
 
-static RegisterEffect registerEffect(EFFECT_PEDS_BLIND, nullptr, OnStop, OnTick);
+static RegisterEffect registerEffect(EFFECT_PEDS_BLIND, OnStart, OnStop, OnTick);
