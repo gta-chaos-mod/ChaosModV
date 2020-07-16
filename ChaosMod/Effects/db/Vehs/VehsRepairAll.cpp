09f4779048f4cc3bc9b1e515d82db78beae25ac3
pongo1231 <pongo1999712@gmail.com>
2020-07-16 23:18:58 +0200

    ChaosMod: Changes to Repair All Vehicles

--- ChaosMod/Effects/db/Vehs/VehsRepairAll.cpp
+++ ChaosMod/Effects/db/Vehs/VehsRepairAll.cpp
@@ -7,6 +7,12 @@ static void OnStart()
 	for (Vehicle veh : GetAllVehs())
 	{
 		SET_VEHICLE_FIXED(veh);
+		SET_VEHICLE_DIRT_LEVEL(veh, .0f);
+		SET_VEHICLE_ENGINE_HEALTH(veh, 1000.f);
+		SET_VEHICLE_PETROL_TANK_HEALTH(veh, 1000.f);
+		SET_VEHICLE_BODY_HEALTH(veh, 1000.f);
+		SET_VEHICLE_UNDRIVEABLE(veh, false);
+		SET_VEHICLE_ON_GROUND_PROPERLY(veh, 5.f);
 
 		if (--count == 0)
 		{
