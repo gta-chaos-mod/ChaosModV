3dc4148d53b6767d752f10d1b2e48d9d861eea9b
pongo1231 <pongo1999712@gmail.com>
2020-07-16 21:01:08 +0200

    ChaosMod: Merge Super Run & Super Jump together

--- ChaosMod/Effects/db/Player/PlayerSuperJump.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdafx.h>
-
-static void OnTick()
-{
-	SET_SUPER_JUMP_THIS_FRAME(PLAYER_ID());
-}
-
-static RegisterEffect registerEffect(EFFECT_SUPER_JUMP, nullptr, nullptr, OnTick);
--- ChaosMod/Effects/db/Player/PlayerSuperRunJump.cpp
+++ ChaosMod/Effects/db/Player/PlayerSuperRunJump.cpp
@@ -7,7 +7,11 @@ static void OnStop()
 
 static void OnTick()
 {
-	SET_RUN_SPRINT_MULTIPLIER_FOR_PLAYER(PLAYER_ID(), 1.49f);
+	Player player = PLAYER_ID();
+
+	SET_RUN_SPRINT_MULTIPLIER_FOR_PLAYER(player, 1.49f);
+
+	SET_SUPER_JUMP_THIS_FRAME(player);
 }
 
 static RegisterEffect registerEffect(EFFECT_SUPER_RUN, nullptr, OnStop, OnTick);
