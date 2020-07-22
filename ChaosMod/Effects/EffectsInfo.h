27affcff3e86a62291afd352b411c738ab4eb6d9
Michael B <hansebanger24@yahoo.de>
2020-07-22 17:19:40 +0200

    Added "Fog of War" Effect

--- ChaosMod/Effects/EffectsInfo.h
+++ ChaosMod/Effects/EffectsInfo.h
@@ -204,7 +204,7 @@ enum EffectType
 	EFFECT_PEDS_SCOOTERBROTHERS,
 	EFFECT_PEDS_INTORANDOMVEHS,
 	EFFECT_PEDS_CAT_GUNS,
-	EFFECT_PEDS_OIL_LEAK,
+	EFFECT_PEDS_FOG_OF_WAR,
 	_EFFECT_ENUM_MAX
 };
 
@@ -423,5 +423,7 @@ const std::map<EffectType, EffectInfo> g_effectsMap =
 	{EFFECT_PEDS_SCOOTERBROTHERS, {"Scooter Brothers", "peds_scooterbrothers"}},
 	{EFFECT_PEDS_INTORANDOMVEHS, {"Set Everyone Into Random Vehicles", "peds_intorandomvehs"}},
 	{EFFECT_PEDS_CAT_GUNS, {"Catto Guns", "peds_catguns", true}},
-	{EFFECT_PEDS_OIL_LEAK, {"Oil Leak", "peds_oilleak", true}},
+	{EFFECT_PEDS_FOG_OF_WAR, {"Fog of War", "peds_fogofwar", true}},
+		
+>>>>>>> 827d44f... Added "Fog of War" Effect
 };
---
+++ ChaosMod/Effects/db/Peds/PedsFogOfWar.cpp
@@ -0,0 +1,25 @@
+/*
+	Effect by Last0xygen
+*/
+
+#include <stdafx.h>
+
+static void OnTick()
+{
+	REQUEST_NAMED_PTFX_ASSET("scr_sr_tr");
+	while (!HAS_NAMED_PTFX_ASSET_LOADED("scr_sr_tr")) {
+		WAIT(0);
+	}
+	for (Ped ped : GetAllPeds())
+	{
+		if (IS_PED_SHOOTING(ped))
+		{
+			Vector3 pedPos = GET_ENTITY_COORDS(ped, false);
+			USE_PARTICLE_FX_ASSET("scr_sr_tr");
+			START_PARTICLE_FX_NON_LOOPED_AT_COORD("scr_sr_tr_car_change", pedPos.x, pedPos.y, pedPos.z, 0, 0, 0, 1, false, true, false);
+		}
+	}
+	REMOVE_NAMED_PTFX_ASSET("scr_sr_tr");
+}
+
+static RegisterEffect registerEffect(EFFECT_PEDS_FOG_OF_WAR, nullptr, nullptr, OnTick);
