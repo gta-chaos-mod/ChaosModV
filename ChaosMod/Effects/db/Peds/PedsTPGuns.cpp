625f34b17b5d9ce0eb476c7a73a81540deacec2a
pongo1231 <pongo1999712@gmail.com>
2020-07-24 00:02:46 +0200

    ChaosMod: Improvements to Portal Guns

--- ChaosMod/Effects/db/Peds/PedsTPGuns.cpp
+++ ChaosMod/Effects/db/Peds/PedsTPGuns.cpp
@@ -4,6 +4,14 @@
 
 #include <stdafx.h>
 
+static void OnStart()
+{
+	for (Vehicle veh : GetAllVehs())
+	{
+		CLEAR_ENTITY_LAST_WEAPON_DAMAGE(veh);
+	}
+}
+
 static void OnTick()
 {
 	Vector3 impactCoords;
@@ -12,17 +20,63 @@ static void OnTick()
 	{
 		if (GET_PED_LAST_WEAPON_IMPACT_COORD(ped, &impactCoords))
 		{
-			Entity toTeleport = ped;
+			Hash wepHash;
+			GET_CURRENT_PED_WEAPON(ped, &wepHash, true);
 
-			if (IS_PED_IN_ANY_VEHICLE(ped, false))
+			bool alreadyTeleported = false;
+			for (Vehicle veh : GetAllVehs())
 			{
-				toTeleport = GET_VEHICLE_PED_IS_IN(ped, false);
+				if (HAS_ENTITY_BEEN_DAMAGED_BY_ENTITY(veh, ped, true) && ARE_ANY_VEHICLE_SEATS_FREE(veh) && HAS_ENTITY_BEEN_DAMAGED_BY_WEAPON(veh, wepHash, 0))
+				{
+					int seat = -2;
+					if (IS_PED_A_PLAYER(ped))
+					{
+						seat = -1;
+
+						if (!IS_VEHICLE_SEAT_FREE(veh, -1, false))
+						{
+							Ped seatPed = GET_PED_IN_VEHICLE_SEAT(veh, -1, false);
+
+							CLEAR_PED_TASKS_IMMEDIATELY(seatPed);
+
+							WAIT(0);
+						}
+					}
+					else if (IS_VEHICLE_SEAT_FREE(veh, -1, false))
+					{
+						seat = -1;
+					}
+
+					SET_PED_INTO_VEHICLE(ped, veh, seat);
+
+					CLEAR_ENTITY_LAST_WEAPON_DAMAGE(veh);
+
+					alreadyTeleported = true;
+					break;
+				}
 			}
 
-			float heading = GET_ENTITY_HEADING(toTeleport);
-			Vector3 vel = GET_ENTITY_VELOCITY(toTeleport);
-			SET_ENTITY_COORDS(toTeleport, impactCoords.x, impactCoords.y, impactCoords.z, false, false, false, false);
-			SET_ENTITY_VELOCITY(toTeleport, vel.x, vel.y, vel.z);
+			if (!alreadyTeleported)
+			{
+				Entity toTeleport = ped;
+
+				if (IS_PED_IN_ANY_VEHICLE(ped, false))
+				{
+					toTeleport = GET_VEHICLE_PED_IS_IN(ped, false);
+				}
+
+				float heading = GET_ENTITY_HEADING(toTeleport);
+				Vector3 vel = GET_ENTITY_VELOCITY(toTeleport);
+				float forward = GET_ENTITY_SPEED(toTeleport);
+
+				SET_ENTITY_COORDS(toTeleport, impactCoords.x, impactCoords.y, impactCoords.z, false, false, false, false);
+				SET_ENTITY_VELOCITY(toTeleport, vel.x, vel.y, vel.z);
+
+				if (IS_ENTITY_A_VEHICLE(toTeleport))
+				{
+					SET_VEHICLE_FORWARD_SPEED(toTeleport, forward);
+				}
+			}
 		}
 	}
 }
