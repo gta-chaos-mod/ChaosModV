7511e23c26b234273e3abf4127a7bdfd37680e6e
Michael B <hansebanger24@yahoo.de>
2020-07-23 22:57:51 +0200

    newline because i always forget these

--- ChaosMod/Effects/db/Player/PlayerHeavyRecoil.cpp
+++ ChaosMod/Effects/db/Player/PlayerHeavyRecoil.cpp
@@ -8,7 +8,8 @@ static void OnTick()
 {
 	static float verticalRecoil = 2;
 	Ped player = PLAYER_PED_ID();
-	if (IS_PED_SHOOTING(player)) {
+	if (IS_PED_SHOOTING(player)) 
+	{
 		float horizontalRecoil = g_random.GetRandomInt(-30, 30);
 		horizontalRecoil = horizontalRecoil / 10;
 		SET_GAMEPLAY_CAM_RELATIVE_PITCH(GET_GAMEPLAY_CAM_RELATIVE_PITCH() + verticalRecoil, 1.f);
