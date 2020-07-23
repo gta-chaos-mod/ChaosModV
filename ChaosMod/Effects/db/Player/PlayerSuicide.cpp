7834cfa42af62db045e75cc08117b53c904c91e2
Michael B <hansebanger24@yahoo.de>
2020-07-23 23:23:16 +0200

    Added the suicide animation if the player is not in a vehicle

--- ChaosMod/Effects/db/Player/PlayerSuicide.cpp
+++ ChaosMod/Effects/db/Player/PlayerSuicide.cpp
@@ -2,7 +2,18 @@
 
 static void OnStart()
 {
-	SET_ENTITY_HEALTH(PLAYER_PED_ID(), 0, 0);
+	Ped playerPed = PLAYER_PED_ID();
+	if (!IS_PED_IN_ANY_VEHICLE(playerPed, false)) 
+	{
+		REQUEST_ANIM_DICT("mp_suicide");
+		Hash pistolHash = GET_HASH_KEY("WEAPON_PISTOL");
+		GIVE_WEAPON_TO_PED(playerPed, pistolHash, 9999, true, true);
+		TASK_PLAY_ANIM(playerPed, "mp_suicide", "pistol", 8.0f, -1.0f, -1.f, 1, 0.f, false, false, false);
+		WAIT(750);
+		SET_PED_SHOOTS_AT_COORD(playerPed, 0, 0, 0, true);
+		REMOVE_ANIM_DICT("mp_suicide");
+	}
+	SET_ENTITY_HEALTH(playerPed, 0, 0);
 }
 
 static RegisterEffect registerEffect(EFFECT_PLAYER_SUICIDE, OnStart);
