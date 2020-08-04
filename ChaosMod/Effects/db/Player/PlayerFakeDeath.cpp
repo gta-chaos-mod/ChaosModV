ff52cd34b45cfc7885cc4e9ce1d0e284392491b3
Michael B <hansebanger24@yahoo.de>
2020-08-04 21:47:47 +0200

    Show the right deathscreen-intro & play additional sound

--- ChaosMod/Effects/db/Player/PlayerFakeDeath.cpp
+++ ChaosMod/Effects/db/Player/PlayerFakeDeath.cpp
@@ -8,6 +8,7 @@ static int scaleForm = 0;
 static int currentMode = 0;
 static int lastModeTime = 0;
 static int nextModeTime = 0;
+static const char* deathAnimationName = "";
 
 static void OnStart()
 {
@@ -44,10 +45,24 @@ static void OnStart()
 		case 1: // Apply Screen Effect
 			lastModeTime = GetTickCount64();
 			nextModeTime = 2000;
-			if (!ANIMPOSTFX_IS_RUNNING("DeathFailOut"))
+			switch (GET_ENTITY_MODEL(PLAYER_PED_ID()))
 			{
-				ANIMPOSTFX_PLAY("DeathFailOut", 0, false);
+			case 225514697: // Michael 
+				deathAnimationName = "DeathFailMichaelIn";
+				break;
+			case 2602752943: // Franklin
+				deathAnimationName = "DeathFailFranklinIn";
+				break;
+			case 2608926626: // Trevor
+				deathAnimationName = "DeathFailTrevorIn";
+				break;
+			default: // default
+				deathAnimationName = "DeathFailNeutralIn";
+				break;
 			}
+			START_AUDIO_SCENE("DEATH_SCENE");
+			ANIMPOSTFX_PLAY(deathAnimationName, 0, false);
+			PLAY_SOUND_FRONTEND(-1, "ScreenFlash", "WastedSounds", true);
 			PLAY_SOUND_FRONTEND(-1, "Bed", "WastedSounds", true);
 			SET_TIME_SCALE(0.1f);
 			SHAKE_GAMEPLAY_CAM("DEATH_FAIL_IN_EFFECT_SHAKE", 1);
@@ -56,10 +71,13 @@ static void OnStart()
 			scaleForm = REQUEST_SCALEFORM_MOVIE("MP_BIG_MESSAGE_FREEMODE");
 			while (!HAS_SCALEFORM_MOVIE_LOADED(scaleForm))
 			{
+				HIDE_HUD_AND_RADAR_THIS_FRAME();
 				WAIT(0);
 			}
 			lastModeTime = GetTickCount64();
 			nextModeTime = 2000;
+			ANIMPOSTFX_STOP(deathAnimationName);
+			ANIMPOSTFX_PLAY("DeathFailOut", 0, false);
 			BEGIN_SCALEFORM_MOVIE_METHOD(scaleForm, "SHOW_SHARD_WASTED_MP_MESSAGE");
 			SCALEFORM_MOVIE_METHOD_ADD_PARAM_PLAYER_NAME_STRING("~r~wasted");
 			SCALEFORM_MOVIE_METHOD_ADD_PARAM_PLAYER_NAME_STRING("Just kidding, keep playing");
@@ -68,6 +86,7 @@ static void OnStart()
 			break;
 		case 3: // Remove all Effects, so you dont have to see this for the rest of the duration
 			ANIMPOSTFX_STOP("DeathFailOut");
+			STOP_AUDIO_SCENE("DEATH_SCENE");
 			SET_TIME_SCALE(1);
 			STOP_GAMEPLAY_CAM_SHAKING(true);
 			scaleForm = 0;
