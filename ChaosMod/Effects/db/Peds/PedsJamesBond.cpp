069b8d1db8be7e70cc1aa9be51055417f0cbafeb
Slothersbee <69346582+Slothersbee@users.noreply.github.com>
2020-08-07 18:15:19 +0100

    Added James Bond script

---
+++ ChaosMod/Effects/db/Peds/PedsJamesBond.cpp
@@ -0,0 +1,60 @@
+/*
+	Effect by Slothersbee
+*/
+
+#include <stdafx.h>
+#include <math.h>  
+
+#define PI 3.14159265
+
+static void OnStart()
+{
+	Ped playerPed = PLAYER_PED_ID();
+
+	static const Hash model = GET_HASH_KEY("cs_milton");
+
+	static const Hash playerGroup = GET_HASH_KEY("PLAYER");
+	static const Hash civGroup = GET_HASH_KEY("CIVMALE");
+	static const Hash femCivGroup = GET_HASH_KEY("CIVFEMALE");
+
+	Hash relationshipGroup;
+	ADD_RELATIONSHIP_GROUP("_HOSTILE_BOND", &relationshipGroup);
+	SET_RELATIONSHIP_BETWEEN_GROUPS(5, relationshipGroup, playerGroup);
+	SET_RELATIONSHIP_BETWEEN_GROUPS(5, relationshipGroup, civGroup);
+	SET_RELATIONSHIP_BETWEEN_GROUPS(5, relationshipGroup, femCivGroup);
+
+	Vector3 playerPos = GET_ENTITY_COORDS(playerPed, false);
+
+	float heading = GET_ENTITY_HEADING(IS_PED_IN_ANY_VEHICLE(playerPed, false) ? GET_VEHICLE_PED_IS_IN(playerPed, false) : playerPed);
+
+	float x_pos = sin((360 - heading) * PI / 180) * 10;
+	float y_pos = cos((360 - heading) * PI / 180) * 10;
+
+	Vehicle veh = CreatePoolVehicle(GET_HASH_KEY("JB700"), playerPos.x - x_pos, playerPos.y - y_pos, playerPos.z, heading);
+	SET_VEHICLE_ENGINE_ON(veh, true, true, false);
+
+	Vector3 vel = GET_ENTITY_VELOCITY(playerPed);
+	SET_ENTITY_VELOCITY(veh, vel.x, vel.y, vel.z);
+
+	Ped bond = CreatePoolPedInsideVehicle(veh, 4, model, -1);
+
+	SET_PED_RELATIONSHIP_GROUP_HASH(bond, relationshipGroup);
+
+	TASK_SET_BLOCKING_OF_NON_TEMPORARY_EVENTS(bond, true);
+
+	SET_PED_HEARING_RANGE(bond, 9999.f);
+	SET_PED_CONFIG_FLAG(bond, 281, true);
+
+	SET_PED_COMBAT_ATTRIBUTES(bond, 5, true);
+	SET_PED_COMBAT_ATTRIBUTES(bond, 46, true);
+
+	SET_PED_SUFFERS_CRITICAL_HITS(bond, false);
+
+	GIVE_WEAPON_TO_PED(bond, GET_HASH_KEY("WEAPON_SWITCHBLADE"), 9999, true, true);
+	GIVE_WEAPON_TO_PED(bond, GET_HASH_KEY("WEAPON_VINTAGEPISTOL"), 9999, true, true);
+	GIVE_WEAPON_COMPONENT_TO_PED(bond, GET_HASH_KEY("WEAPON_VINTAGEPISTOL"), GET_HASH_KEY("COMPONENT_AT_PI_SUPP"));
+	SET_PED_ACCURACY(bond, 100);
+	TASK_COMBAT_PED(bond, playerPed, 0, 16);
+}
+
+static RegisterEffect registerEffect(EFFECT_JAMES_BOND, OnStart);
