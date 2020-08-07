7b0a4d79bbe997ac7119a3ada09af386d8494201
Slothersbee <69346582+Slothersbee@users.noreply.github.com>
2020-08-07 12:39:28 +0100

    Added PlayerPoof script

---
+++ ChaosMod/Effects/db/Player/PlayerPoof.cpp
@@ -0,0 +1,26 @@
+/*
+	Effect by Slothersbee
+*/
+
+#include <stdafx.h>
+
+static void OnTick()
+{
+	Player player = PLAYER_ID();
+
+	Entity target;
+
+	STOP_GAMEPLAY_CAM_SHAKING(true);
+
+	if (GET_ENTITY_PLAYER_IS_FREE_AIMING_AT(player, &target))
+	{
+		if ((IS_ENTITY_A_PED(target) || IS_ENTITY_A_VEHICLE(target)) && !IS_ENTITY_DEAD(target, false))
+		{
+			Vector3 pos = GET_ENTITY_COORDS(target, false);
+			ADD_EXPLOSION(pos.x, pos.y, pos.z, 9, 100.f, true, false, 3.f, false);
+		}
+	}
+
+}
+
+static RegisterEffect registerEffect(EFFECT_PLAYER_POOF, nullptr, nullptr, OnTick);
