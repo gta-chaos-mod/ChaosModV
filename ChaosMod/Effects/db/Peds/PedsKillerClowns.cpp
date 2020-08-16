ea2d9c0b4f84f9d9813a87e4c23344d31b1b170f
Michael B <hansebanger24@yahoo.de>
2020-08-17 00:15:37 +0200

    Added Effect "Killer Clowns" Spawns up to 5 Clowns attacking the player

---
+++ ChaosMod/Effects/db/Peds/PedsKillerClowns.cpp
@@ -0,0 +1,118 @@
+/*
+	Effect by Last0xygen
+*/
+
+#include <stdafx.h>
+
+static std::list<Ped> clownEnemies;
+static int spawnTimer = -1;
+static Hash relationshipGroup;
+static int maxClownsToSpawn = 5;
+
+static Vector3 getRandomOffsetCoord(Vector3 startCoord, int minOffset, int maxOffset)
+{
+	Vector3 randomCoord;
+	randomCoord.z = startCoord.z;
+	float groundZ;
+	for (int i = 0; i < 10; i++) {
+		if (g_random.GetRandomInt(0, 2) % 2 == 0)
+		{
+			randomCoord.x = startCoord.x + g_random.GetRandomInt(minOffset, maxOffset);
+		}
+		else
+		{
+			randomCoord.x = startCoord.x - g_random.GetRandomInt(minOffset, maxOffset);
+		}
+		if (g_random.GetRandomInt(0, 2) % 2 == 0)
+		{
+			randomCoord.y = startCoord.y + g_random.GetRandomInt(minOffset, maxOffset);
+		}
+		else
+		{
+			randomCoord.y = startCoord.y - g_random.GetRandomInt(minOffset, maxOffset);
+		}
+		randomCoord.z = startCoord.z;
+		if (GET_GROUND_Z_FOR_3D_COORD(randomCoord.x, randomCoord.y, randomCoord.z, &groundZ, false, false))
+		{
+			randomCoord.z = groundZ;
+			break;
+		}
+	}
+	return randomCoord;
+}
+
+static void OnStop()
+{
+	ANIMPOSTFX_STOP("DrugsTrevorClownsFightOut");
+	REMOVE_NAMED_PTFX_ASSET("scr_rcbarry2");
+}
+
+static void OnStart()
+{
+	static const Hash playerGroup = GET_HASH_KEY("PLAYER");
+	ADD_RELATIONSHIP_GROUP("_HOSTILE_KILLER_CLOWNS", &relationshipGroup);
+	SET_RELATIONSHIP_BETWEEN_GROUPS(5, relationshipGroup, playerGroup);
+	SET_RELATIONSHIP_BETWEEN_GROUPS(5, playerGroup, relationshipGroup);
+	SET_RELATIONSHIP_BETWEEN_GROUPS(0, relationshipGroup, relationshipGroup);
+}
+
+static void OnTick()
+{
+	REQUEST_NAMED_PTFX_ASSET("scr_rcbarry2");
+	while (!HAS_NAMED_PTFX_ASSET_LOADED("scr_rcbarry2"))
+	{
+		WAIT(0);
+	}
+	if (!ANIMPOSTFX_IS_RUNNING("DrugsTrevorClownsFightOut"))
+	{
+		ANIMPOSTFX_PLAY("DrugsTrevorClownsFightOut", -1, true);
+	}
+
+	Ped playerPed = PLAYER_PED_ID();
+	Vector3 playerPos = GET_ENTITY_COORDS(playerPed, false);
+	int current_time = GET_GAME_TIMER();
+
+	for (std::list<Entity>::iterator it = clownEnemies.begin(); it != clownEnemies.end(); )
+	{
+		Ped clown = *it;
+		Vector3 clownPos = GET_ENTITY_COORDS(clown, false);
+		if (IS_PED_DEAD_OR_DYING(clown, false) || IS_PED_INJURED(clown) || GET_DISTANCE_BETWEEN_COORDS(playerPos.x, playerPos.y, playerPos.z, clownPos.x, clownPos.y, clownPos.z, false) > 100.f)
+		{
+			SET_ENTITY_HEALTH(clown, 0, 0);
+			USE_PARTICLE_FX_ASSET("scr_rcbarry2");
+			START_PARTICLE_FX_NON_LOOPED_AT_COORD("scr_clown_death", clownPos.x, clownPos.y, clownPos.z, 0, 0, 0, 3, false, false, false);
+			WAIT(300);
+			SET_ENTITY_ALPHA(clown, 0, true);
+			SET_PED_AS_NO_LONGER_NEEDED(&clown);
+			DELETE_PED(&clown);
+			it = clownEnemies.erase(it);
+			WAIT(0);
+		}
+		else
+		{
+			it++;
+		}
+	}
+
+	if (clownEnemies.size() < maxClownsToSpawn && current_time > spawnTimer + 2000)
+	{
+		spawnTimer = current_time;
+		Vector3 spawnPos = getRandomOffsetCoord(playerPos, 10, 25);
+		USE_PARTICLE_FX_ASSET("scr_rcbarry2");
+		START_PARTICLE_FX_NON_LOOPED_AT_COORD("scr_clown_appears", spawnPos.x, spawnPos.y, spawnPos.z, 0, 0, 0, 2, true, true, true);
+		WAIT(300);
+		Hash clownHash = GET_HASH_KEY("s_m_y_clown_01");
+		Hash weaponHash = GET_HASH_KEY("WEAPON_MICROSMG");
+		LoadModel(clownHash);
+		Ped ped = CREATE_PED(-1, clownHash, spawnPos.x, spawnPos.y, spawnPos.z, 0, true, false);
+		SET_BLOCKING_OF_NON_TEMPORARY_EVENTS(ped, true);
+		SET_PED_RELATIONSHIP_GROUP_HASH(ped, relationshipGroup);
+		SET_PED_HEARING_RANGE(ped, 9999.f);
+		GIVE_WEAPON_TO_PED(ped, weaponHash, 9999, true, true);
+		SET_PED_ACCURACY(ped, 50);
+		TASK_COMBAT_PED(ped, playerPed, 0, 16);
+		clownEnemies.push_back(ped);
+	}
+}
+
+static RegisterEffect registerEffect(EFFECT_KILLER_CLOWNS, OnStart, OnStop, OnTick);
--- ConfigApp/Effects.cs
+++ ConfigApp/Effects.cs
@@ -248,6 +248,7 @@ namespace ConfigApp
             EFFECT_MISC_FLAMETHROWER,
             EFFECT_PLAYER_FAKEDEATH,
             EFFECT_GAMESPEED_SUPERHOT,
+            EFFECT_KILLER_CLOWNS,
             _EFFECT_ENUM_MAX
         }
 
@@ -461,6 +462,7 @@ namespace ConfigApp
             {EffectType.EFFECT_MISC_FLAMETHROWER, new EffectInfo("Flamethrowers", EffectCategory.MISC, "misc_flamethrower", true)},
             {EffectType.EFFECT_PLAYER_FAKEDEATH, new EffectInfo("Fake Death", EffectCategory.PLAYER, "player_fakedeath")},
             {EffectType.EFFECT_GAMESPEED_SUPERHOT, new EffectInfo("Superhot", EffectCategory.TIME, "time_superhot", true)},
+            {EffectType.EFFECT_KILLER_CLOWNS, new EffectInfo("Killer Clowns", EffectCategory.PEDS, "peds_killerclowns", true)},
         };
     }
 }
