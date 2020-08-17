2dca54a1b117ca083377b703a0827b52baa189a2
Michael B <hansebanger24@yahoo.de>
2020-08-17 22:33:45 +0200

    Added Effect 'Beyblade' Don't know how to make them rotate faster, maybe someone else knows something

--- ConfigApp/Effects.cs
+++ ConfigApp/Effects.cs
@@ -248,6 +248,7 @@ namespace ConfigApp
             EFFECT_MISC_FLAMETHROWER,
             EFFECT_PLAYER_FAKEDEATH,
             EFFECT_GAMESPEED_SUPERHOT,
+            EFFECT_VEH_BEYBLADE,
             _EFFECT_ENUM_MAX
         }
 
@@ -461,6 +462,7 @@ namespace ConfigApp
             {EffectType.EFFECT_MISC_FLAMETHROWER, new EffectInfo("Flamethrowers", EffectCategory.MISC, "misc_flamethrower", true)},
             {EffectType.EFFECT_PLAYER_FAKEDEATH, new EffectInfo("Fake Death", EffectCategory.PLAYER, "player_fakedeath")},
             {EffectType.EFFECT_GAMESPEED_SUPERHOT, new EffectInfo("Superhot", EffectCategory.TIME, "time_superhot", true)},
+            {EffectType.EFFECT_VEH_BEYBLADE, new EffectInfo("Beyblade", EffectCategory.VEHICLE, "vehs_beyblade", true)},
         };
     }
 }
