ff86bf8114ca84b74053e744fda52dadc3a6f776
ProfessorBiddle <probiddle@gmail.com>
2020-09-02 17:48:10 -0600

    Update VehsBusRain.cpp

--- ChaosMod/Effects/db/Vehs/VehsBusRain.cpp
+++ ChaosMod/Effects/db/Vehs/VehsBusRain.cpp
@@ -6,7 +6,6 @@
 */
 static void OnTick()
 {
-
 	Vector3 playerPos = GET_ENTITY_COORDS(PLAYER_PED_ID(), false);
 
 	static DWORD64 lastTick = 0;
@@ -21,14 +20,8 @@ static void OnTick()
 		spawnPos.y = playerPos.y + g_random.GetRandomInt(-100, 100);
 		spawnPos.z = playerPos.z + g_random.GetRandomInt(25, 50);
 
-
 		CreatePoolVehicle(GET_HASH_KEY("BUS"), spawnPos.x, spawnPos.y, spawnPos.z, GET_ENTITY_HEADING(PLAYER_PED_ID()));
-
-
-
 	}
-
-
 }
 
 static RegisterEffect registerEffect(EFFECT_VEH_BUSRAIN, nullptr, nullptr, OnTick);
