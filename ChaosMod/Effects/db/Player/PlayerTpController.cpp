de1a15e42a838b2c72547847e1bc6fd4302917ab
Michael B <hansebanger24@yahoo.de>
2020-10-01 21:05:19 +0200

    Filter out other players missions

--- ChaosMod/Effects/db/Player/PlayerTpController.cpp
+++ ChaosMod/Effects/db/Player/PlayerTpController.cpp
@@ -173,26 +173,43 @@ static RegisterEffect registerEffect8(EFFECT_TP_RANDOM, OnStartRandom);
 
 static void OnStartMission()
 {
+	std::vector<Hash> excludedColors;
+	switch (GET_ENTITY_MODEL(PLAYER_PED_ID()))
+	{
+	case 225514697: // Michael 
+		excludedColors = { 43, 44 };
+		break;
+	case 2602752943: // Franklin
+		excludedColors = { 42, 44 };
+		break;
+	case 2608926626: // Trevor
+		excludedColors = { 42, 43 };
+		break;
+	default: // default
+		break;
+	}
 	const int possibleBlipIds[] = { 66, 76, 77, 78, 79, 80, 86, 88 , 89, 96, 104, 105, 106, 107, 112, 113, 118, 120, 123, 124, 208, 209, 210, 211, 214, 267, 355, 363, 381, 382, 383, 384, 385, 386, 387, 388, 389, 428, 445, 447, 448, 449, 450, 451, 452, 453, 454 };
 	std::vector<Blip> validBlips;
 	for (int i : possibleBlipIds)
 	{
-		Blip firstBlip = GET_FIRST_BLIP_INFO_ID(i);
-		if (firstBlip != 0)
+		std::vector<Blip> checkedBlips;
+		Blip nextBlip = GET_FIRST_BLIP_INFO_ID(i);
+		while (DOES_BLIP_EXIST(nextBlip))
 		{
-			validBlips.push_back(firstBlip);
+			int color = GET_BLIP_COLOUR(nextBlip);
+			// Filter out missions for other players (ignore Trevor- and Franklin-Blips for Michael)
+			if (std::find(excludedColors.begin(), excludedColors.end(), color) == excludedColors.end())
+			{
+				validBlips.push_back(nextBlip);
+			}
+			nextBlip = GET_NEXT_BLIP_INFO_ID(i);
 		}
 	}
 	if (validBlips.size() >= 1)
 	{
 		Blip randomBlip = validBlips[g_random.GetRandomInt(0, validBlips.size() - 1)];
 		Vector3 blipCoord = GET_BLIP_COORDS(randomBlip);
-		float groundZ;
-		if (!GET_GROUND_Z_FOR_3D_COORD(blipCoord.x, blipCoord.y, blipCoord.z, &groundZ, false, false))
-		{
-			groundZ = blipCoord.z;
-		}
-		TeleportPlayer(blipCoord.x, blipCoord.y, groundZ, true);
+		TeleportPlayer(blipCoord.x, blipCoord.y, blipCoord.z, true);
 	}
 }
 
