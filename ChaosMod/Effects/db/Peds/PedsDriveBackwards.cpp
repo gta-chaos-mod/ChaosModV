a246148808b5debcfa9a17b2fbd2f78913c68177
James Puleo <electricman226@gmail.com>
2020-10-16 01:39:56 -0400

    Implement Peds Drive Backwards
    When activated, all peds in vehicles will have their task driving style set to 1024,
    which will make them perform their vehicle task in reverse gear.

---
+++ ChaosMod/Effects/db/Peds/PedsDriveBackwards.cpp
@@ -0,0 +1,16 @@
+#include <stdafx.h>
+
+static void OnTick()
+{
+	static unsigned int constexpr DRIVING_STYLE = 1024; // "Drive in reverse gear" bit
+
+	for (auto ped : GetAllPeds())
+	{
+		if(ped == PLAYER::PLAYER_PED_ID() || !PED::IS_PED_IN_ANY_VEHICLE(ped, false))
+			continue;
+
+		TASK::SET_DRIVE_TASK_DRIVING_STYLE(ped, 1024);
+	}
+}
+
+static RegisterEffect registerEffect(EFFECT_PEDS_DRIVE_BACKWARDS, nullptr, nullptr, OnTick);
