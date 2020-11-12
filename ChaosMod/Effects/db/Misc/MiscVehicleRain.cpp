f451c8e5d27cb641da44d2ecf121e97b1d15caf1
ProfessorBiddle <probiddle@gmail.com>
2020-11-12 15:16:55 -0700

    Added Effect Vehicle Rain
    Rains random vehicles. Quite explodey,.

---
+++ ChaosMod/Effects/db/Misc/MiscVehicleRain.cpp
@@ -0,0 +1,44 @@
+#include <stdafx.h>
+
+	//Effect by ProfessorBiddle
+
+static void OnTick()
+{
+	Vector3 playerPos = GET_ENTITY_COORDS(PLAYER_PED_ID(), false);
+
+	static DWORD64 lastTick = 0;
+	DWORD64 curTick = GetTickCount64();
+
+	if (curTick > lastTick + 500)
+	{
+		lastTick = curTick;
+
+		Vector3 spawnPos;
+		spawnPos.x = playerPos.x + g_random.GetRandomInt(-100, 100);
+		spawnPos.y = playerPos.y + g_random.GetRandomInt(-100, 100);
+		spawnPos.z = playerPos.z + g_random.GetRandomInt(25, 50);
+
+
+		static std::vector<Hash> vehModels = Memory::GetAllVehModels();
+
+		if (!vehModels.empty())
+		{
+			Vehicle veh = CreatePoolVehicle(vehModels[g_random.GetRandomInt(0, vehModels.size() - 1)], spawnPos.x, spawnPos.y, spawnPos.z, GET_ENTITY_HEADING(PLAYER_PED_ID()));
+
+			// Also apply random upgrades
+			SET_VEHICLE_MOD_KIT(veh, 0);
+			for (int i = 0; i < 50; i++)
+			{
+				int max = GET_NUM_VEHICLE_MODS(veh, i);
+				SET_VEHICLE_MOD(veh, i, max > 0 ? g_random.GetRandomInt(0, max - 1) : 0, g_random.GetRandomInt(0, 1));
+
+				TOGGLE_VEHICLE_MOD(veh, i, g_random.GetRandomInt(0, 1));
+			}
+
+			SET_VEHICLE_TYRES_CAN_BURST(veh, g_random.GetRandomInt(0, 1));
+			SET_VEHICLE_WINDOW_TINT(veh, g_random.GetRandomInt(0, 6));
+		}
+	}
+}
+
+static RegisterEffect registerEffect(EFFECT_MISC_VEHICLE_RAIN, nullptr, nullptr, OnTick);
