d4be3408e745eec8640721c6805e7a34345c91c0
ProfessorBiddle <probiddle@gmail.com>
2020-11-13 17:23:22 -0700

    Implemented error message idea from Moneywasted
    Can't get it to display error message, but it does say Alert and makes the screen black.

--- ChaosMod/Effects/db/Misc/MiscFakeCrash.cpp
+++ ChaosMod/Effects/db/Misc/MiscFakeCrash.cpp
@@ -5,6 +5,7 @@
 static int currentMode = 0;
 static int lastModeTime = 0;
 static int nextModeTime = 0;
+static bool message = false;
 
 static void OnStart()
 {
@@ -12,9 +13,16 @@ static void OnStart()
 	lastModeTime = 0;
 	nextModeTime = 0;
 
-	while (currentMode < 4)
+	while (currentMode < 5)
 	{
 		WAIT(0);
+
+		if (message == true) 
+		{
+			DRAW_RECT(.5f, .5f, 1.f, 1.f, 0, 0, 0, 255, false);
+			//Error, composed of meme and random garbage. Button says no, but doesn't do anything.
+			SET_WARNING_MESSAGE("LIBCMT LNK", 32, "Error code 420", 0, -1, 0, 0, 0, 0);
+		}
 
 		int current_time = GetTickCount64();
 		if (current_time - lastModeTime > nextModeTime)
@@ -46,15 +54,20 @@ static void OnStart()
 			SET_TIME_SCALE(1);
 
 			break;
-		case 3: // Pause again 4s
+		case 3: // Pause again .5s
 			lastModeTime = GetTickCount64();
-			nextModeTime = 4000;
-
+			nextModeTime = 500;
 			SET_TIME_SCALE(0);
+			
 
 			break;
-		case 4: // Done
+		case 4: // Display message 6s
+			nextModeTime = 6000;
+			message = true;
 
+			break;
+		case 5: // Done
+			message = false;
 			SET_TIME_SCALE(1);
 
 			break;
