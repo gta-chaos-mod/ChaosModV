bf555dc715410be4c8232746d69bad4b974e9a98
ProfessorBiddle <probiddle@gmail.com>
2020-11-14 21:15:12 -0700

    Added Effect Easy Aim
    Slows down time while the player is aiming or shooting

---
+++ ChaosMod/Effects/db/Player/PlayerEasyAim.cpp
@@ -0,0 +1,21 @@
+#include <stdafx.h>
+
+// Effect by ProfessorBiddle
+
+static void OnTick()
+{
+	static float verticalRecoil = 4;
+	Ped player = PLAYER_PED_ID();
+	//detect player aiming
+	if (GET_PED_CONFIG_FLAG(player, 78, 1))
+	{
+		//slow time
+		SET_TIME_SCALE(.1);
+	}
+	else
+	{
+		SET_TIME_SCALE(1);
+	}
+}
+
+static RegisterEffect registerEffect(EFFECT_PLAYER_EASY_AIM, nullptr, nullptr, OnTick);
