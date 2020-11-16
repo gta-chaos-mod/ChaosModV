860442b1a0e9a9d4ceae120643989298de1acd2a
ProfessorBiddle <probiddle@gmail.com>
2020-11-16 12:30:04 -0700

    Added Effect Stop And Stare
    Makes everyone stop immediately, exit vehicles, and stare at the player

---
+++ ChaosMod/Effects/db/Peds/PedsStopAndStare.cpp
@@ -0,0 +1,21 @@
+#include <stdafx.h>
+static void OnStart()
+{
+	Ped playerPed = PLAYER_PED_ID();
+
+	for (auto ped : GetAllPeds())
+	{
+		if (IS_PED_IN_ANY_VEHICLE(ped, true))
+		{
+			Vehicle pedVeh = GET_VEHICLE_PED_IS_IN(ped, true);
+			TASK_LEAVE_VEHICLE(ped, pedVeh, 256);
+			BRING_VEHICLE_TO_HALT(pedVeh, 0.1f, 10, 0);	
+		}
+		if(ped != playerPed)
+		{
+			TASK_TURN_PED_TO_FACE_ENTITY(ped, playerPed, -1);
+			TASK_LOOK_AT_ENTITY(ped, playerPed, -1, 2048, 3);
+		}
+	}
+}
+static RegisterEffect registerEffect(EFFECT_PEDS_STOP_AND_STARE, OnStart, nullptr, nullptr);
