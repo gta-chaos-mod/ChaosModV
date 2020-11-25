467600aa8aeb1c768770fc894a8917208a661b23
Michael B <41595552+Last0xygen@users.noreply.github.com>
2020-11-25 19:42:15 +0100

    Porting the player (including vehicle if needed) to the surface

--- ChaosMod/Effects/db/Player/PlayerWalkOnWater.cpp
+++ ChaosMod/Effects/db/Player/PlayerWalkOnWater.cpp
@@ -15,23 +15,34 @@ static void OnStart()
 static void OnTick()
 {
 	Ped player = PLAYER_PED_ID();
-	Vector3 PlayerCoord = GET_ENTITY_COORDS(player, 1);
+	Vector3 playerCoord = GET_ENTITY_COORDS(player, 1);
 	LoadModel(displayHash);
-	waterObj = GET_CLOSEST_OBJECT_OF_TYPE(PlayerCoord.x, PlayerCoord.y, PlayerCoord.z, 300, displayHash, 1, 0, 1);
+	waterObj = GET_CLOSEST_OBJECT_OF_TYPE(playerCoord.x, playerCoord.y, playerCoord.z, 300, displayHash, 1, 0, 1);
 	float waterZ;
-	GET_WATER_HEIGHT(PlayerCoord.x, PlayerCoord.y, PlayerCoord.z, &waterZ);
+	GET_WATER_HEIGHT(playerCoord.x, playerCoord.y, playerCoord.z, &waterZ);
 	if (waterZ > -1000)
 	{
 		if (!DOES_ENTITY_EXIST(waterObj))
 		{
-			waterObj = CREATE_OBJECT(displayHash, PlayerCoord.x, PlayerCoord.y, PlayerCoord.z, true, true, true);
+			waterObj = CREATE_OBJECT(displayHash, playerCoord.x, playerCoord.y, playerCoord.z, true, true, true);
 			SET_ENTITY_ROTATION(waterObj, 90, 0, 0, 2, true);
 			FREEZE_ENTITY_POSITION(waterObj, 1);
 			SET_ENTITY_VISIBLE(waterObj, false, false);
 		}
 		else
 		{
-			SET_ENTITY_COORDS(waterObj, PlayerCoord.x, PlayerCoord.y, waterZ, 1, 0, 0, false);
+			SET_ENTITY_COORDS(waterObj, playerCoord.x, playerCoord.y, waterZ, 1, 0, 0, false);
+			if (playerCoord.z < waterZ) 
+			{
+				Entity entityToPort = player;
+				Vector3 coordsToPort = playerCoord;
+				if (IS_PED_IN_ANY_VEHICLE(player, false))
+				{
+					entityToPort = GET_VEHICLE_PED_IS_IN(player, false);
+					coordsToPort = GET_ENTITY_COORDS(entityToPort, false);
+				}
+				SET_ENTITY_COORDS(entityToPort, coordsToPort.x, coordsToPort.y, waterZ + 1, false, false, false, false);
+			}
 		}
 	}
 }
