cd3d39669d806ab40ac2f9f99d41245b103c2328
Michael B <41595552+Last0xygen@users.noreply.github.com>
2020-11-26 18:31:14 +0100

    Changed the effect

--- ChaosMod/Effects/db/Player/PlayerDeadEye.cpp
+++ ChaosMod/Effects/db/Player/PlayerDeadEye.cpp
@@ -4,53 +4,37 @@
 
 #include <stdafx.h>
 
-static void OnStop()
-{
-
-}
-
 struct PedHitInfo
 {
 	Ped pedHandle;
 	int boneIdx;
-	Vector3 offset;
 };
 
 static bool didSelect = false;
 static std::vector<PedHitInfo> selectedOffsets;
 static bool isBlocked = false;
 
-static void PointAndShoot(PedHitInfo hitInfo, Hash weaponHash)
+static void OnStop()
 {
-	if (!DOES_ENTITY_EXIST(hitInfo.pedHandle))
-	{
-		return;
-	}
-	// TODO Fix Camera
-	WAIT(200);
-	Ped player = PLAYER_PED_ID();
-	Entity weapon = GET_CURRENT_PED_WEAPON_ENTITY_INDEX(player);
-	Vector3 weaponCoord = GET_ENTITY_COORDS(weapon, false);
-
-	Vector3 targPos = GET_PED_BONE_COORDS(hitInfo.pedHandle, hitInfo.boneIdx, hitInfo.offset.x, hitInfo.offset.y, hitInfo.offset.z);
-	SHOOT_SINGLE_BULLET_BETWEEN_COORDS(weaponCoord.x, weaponCoord.y, weaponCoord.z, targPos.x, targPos.y, targPos.z, 5, true, weaponHash, player, true, false, 24000);
-	WAIT(200);
+	didSelect = false;
+	selectedOffsets.clear();
+	isBlocked = false;
+	SET_TIME_SCALE(1);
 }
 
 static void OnTick()
 {
 	Ped player = PLAYER_PED_ID();
 	Hash weaponHash;
-	if (!GET_CURRENT_PED_WEAPON(player, &weaponHash, true) || GET_WEAPONTYPE_GROUP(weaponHash) == 0xD49321D4)
+	if (isBlocked || !GET_CURRENT_PED_WEAPON(player, &weaponHash, true) || GET_WEAPONTYPE_GROUP(weaponHash) == 0xD49321D4)
 	{
 		return;
 	}
-
-
-	static std::ofstream log("ChaosLog.txt");
+	// Check if player is aiming
 	if (IS_CONTROL_PRESSED(0, 25))
 	{
 		SET_TIME_SCALE(0.2f);
+		// Disable Fire-Buttons
 		DISABLE_CONTROL_ACTION(0, 24, true);
 		DISABLE_CONTROL_ACTION(2, 257, true);
 
@@ -58,9 +42,8 @@ static void OnTick()
 		{
 			if (!didSelect)
 			{
-				didSelect = true;
 				Vector3 launchPos = GetGameplayCamOffsetInWorldCoords(Vector3::Init(0, 0, 0));
-				Vector3 targPos = GetGameplayCamOffsetInWorldCoords(Vector3::Init(0, 1000, 0));
+				Vector3 targPos = GetGameplayCamOffsetInWorldCoords(Vector3::Init(0, 10000, 0));
 
 				int rayHandle = _START_SHAPE_TEST_RAY(launchPos.x, launchPos.y, launchPos.z, targPos.x, targPos.y, targPos.z, 12, player, 7);
 				if (rayHandle != 0)
@@ -74,11 +57,36 @@ static void OnTick()
 					{
 						if (IS_ENTITY_A_PED(entityHandle))
 						{
-							Vector3 coord = GET_ENTITY_COORDS(entityHandle, false);
-							Vector3 offset = coord - hitCoords;
+							didSelect = true;
+							/*
+								SKEL_ROOT: 0x0
+								SKEL_Pelvis: 0x2e28
+								SKEL_L_Thigh: 0xe39f
+								SKEL_L_Calf: 0xf9bb
+								SKEL_L_Foot: 0x3779
+								SKEL_R_Thigh: 0xca72
+								SKEL_R_Calf: 0x9000
+								SKEL_R_Foot: 0xcc4d
+								SKEL_Spine_Root: 0xe0fd
+								SKEL_Spine0: 0x5c01
+								SKEL_Spine1: 0x60f0
+								SKEL_Spine2: 0x60f1
+								SKEL_Spine3: 0x60f2
+								SKEL_L_Clavicle: 0xfcd9
+								SKEL_L_UpperArm: 0xb1c5
+								SKEL_L_Forearm: 0xeeeb
+								SKEL_L_Hand: 0x49d9
+								SKEL_R_Clavicle: 0x29d2
+								SKEL_R_UpperArm: 0x9d4d
+								SKEL_R_Forearm: 0x6e5c
+								SKEL_R_Hand: 0xdead
+								SKEL_Neck_1: 0x9995
+								SKEL_Head: 0x796e
+							*/
 							int bestBone = -1;
 							float bestDistance = -1;
-							static const int boneIds[] = { 0x0, 0x2e28, 0xe39f, 0xf9bb, 0x3779, 0x83c, 0xfedd, 0xe175, 0xb3fe, 0xca72, 0x9000, 0xcc4d, 0x512d, 0x8aae, 0x60e6, 0x3fcf, 0x5c57, 0x192a, 0xe0fd, 0x5c01, 0x60f0, 0x60f1, 0x60f2, 0xfcd9, 0xb1c5, 0xeeeb, 0x49d9, 0x67f2, 0xff9, 0xffa, 0x67f3, 0x1049, 0x104a, 0x67f4, 0x1059, 0x105a, 0x67f5, 0x1029, 0x102a, 0x67f6, 0x1039, 0x103a, 0xeb95, 0x8cbd, 0xee4f, 0x1470, 0x58b7, 0x29d2, 0x9d4d, 0x6e5c, 0xdead, 0xe5f2, 0xfa10, 0xfa11, 0xe5f3, 0xfa60, 0xfa61, 0xe5f4, 0xfa70, 0xfa71, 0xe5f5, 0xfa40, 0xfa41, 0xe5f6, 0xfa50, 0xfa51, 0x6f06, 0x188e, 0xab22, 0x90ff, 0xbb0, 0x9995, 0x796e, 0x322c, 0xfe2c, 0xe3db, 0xb2b6, 0x62ac, 0x542e, 0x74ac, 0xaa10, 0x6b52, 0x4b88, 0x54c, 0x2ba6, 0x9149, 0x4ed2, 0xf18f, 0x4f37, 0x4537, 0xb4a0, 0x4324, 0x508f, 0xb93b, 0xc33b, 0xb987, 0x8b93, 0xdd1c };
+
+							static const int boneIds[] = { 0x0, 0x2e28, 0xe39f, 0xf9bb, 0x3779, 0xca72, 0x9000, 0xcc4d, 0xe0fd, 0x5c01, 0x60f0, 0x60f1, 0x60f2, 0xfcd9, 0xb1c5, 0xeeeb, 0x49d9, 0x29d2, 0x9d4d, 0x6e5c, 0xdead, 0x9995, 0x796e };
 							for (int id : boneIds)
 							{
 								Vector3 boneCoord = GET_PED_BONE_COORDS(entityHandle, id, 0, 0, 0);
@@ -92,7 +100,6 @@ static void OnTick()
 							PedHitInfo hitInfo;
 							hitInfo.pedHandle = entityHandle;
 							hitInfo.boneIdx = bestBone;
-							hitInfo.offset = Vector3::Init(0, 0, 0);
 							selectedOffsets.push_back(hitInfo);
 						}
 					}
@@ -108,25 +115,34 @@ static void OnTick()
 		{
 			if (DOES_ENTITY_EXIST(info.pedHandle))
 			{
-				Vector3 destOffset = GET_PED_BONE_COORDS(info.pedHandle, info.boneIdx, info.offset.x, info.offset.y, info.offset.z);
-
-				DRAW_BOX(destOffset.x - 0.2, destOffset.y - 0.2, destOffset.z - 0.2, destOffset.x + 0.2, destOffset.y + 0.2, destOffset.z + 0.2, 255, 0, 0, 255);
+				Vector3 destOffset = GET_PED_BONE_COORDS(info.pedHandle, info.boneIdx, 0, 0, 0);
+				float boxSize = 0.1;
+				DRAW_BOX(destOffset.x - boxSize, destOffset.y - boxSize, destOffset.z - boxSize, destOffset.x + boxSize, destOffset.y + boxSize, destOffset.z + boxSize, 255, 0, 0, 100);
 			}
 		}
 	}
 	else
 	{
+		isBlocked = true;
 		if (selectedOffsets.size() > 0)
 		{
+			Ped player = PLAYER_PED_ID();
+			Entity weapon = GET_CURRENT_PED_WEAPON_ENTITY_INDEX(player);
 			for (PedHitInfo info : selectedOffsets)
 			{
-				PointAndShoot(info, weaponHash);
-				WAIT(100);
+				if (DOES_ENTITY_EXIST(info.pedHandle))
+				{
+					Vector3 weaponCoord = GET_ENTITY_COORDS(weapon, false);
+					Vector3 targPos = GET_PED_BONE_COORDS(info.pedHandle, info.boneIdx, 0, 0, 0);
+					SHOOT_SINGLE_BULLET_BETWEEN_COORDS(weaponCoord.x, weaponCoord.y, weaponCoord.z, targPos.x, targPos.y, targPos.z, 5, true, weaponHash, player, true, false, 24000);
+					WAIT(100);
+				}
 			}
 		}
 		SET_TIME_SCALE(1);
 		selectedOffsets.clear();
+		isBlocked = false;
 	}
 }
 
-static RegisterEffect registerEffect(EFFECT_IN_THE_HOOD, nullptr, OnStop, OnTick);
+static RegisterEffect registerEffect(EFFECT_PLAYER_DEAD_EYE, nullptr, OnStop, OnTick);
