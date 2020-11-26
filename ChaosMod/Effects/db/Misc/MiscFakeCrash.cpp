877b073bdfbb5f666e7b44d2ee6dee6726baca90
pongo1231 <pongo1999712@gmail.com>
2020-11-26 13:53:53 +0100

    ChaosMod: Adjust "Fake Crash"

--- ChaosMod/Effects/db/Misc/MiscFakeCrash.cpp
+++ ChaosMod/Effects/db/Misc/MiscFakeCrash.cpp
@@ -1,77 +1,18 @@
 #include <stdafx.h>
 
-//Effect by ProfessorBiddle
-
-static int currentMode = 0;
-static int lastModeTime = 0;
-static int nextModeTime = 0;
-static bool message = false;
+//Effect by ProfessorBiddle, modified
 
 static void OnStart()
 {
-	currentMode = 0;
-	lastModeTime = 0;
-	nextModeTime = 0;
+	PlaySound(reinterpret_cast<LPCSTR>(SND_ALIAS_SYSTEMHAND), NULL, SND_ALIAS_ID);
 
-	while (currentMode < 5)
+	DWORD64 lastTimestamp = GetTickCount64();
+
+	int time = g_random.GetRandomInt(3000, 5000);
+
+	while (lastTimestamp < GetTickCount64() - time)
 	{
-		WAIT(0);
-
-		if (message == true) 
-		{
-			DRAW_RECT(.5f, .5f, 1.f, 1.f, 0, 0, 0, 255, false);
-			//Error, composed of meme and random garbage. Button says no, but doesn't do anything.
-			SET_WARNING_MESSAGE("PM_WAIT", 32, "HUD_QUITTING", 0, -1, 0, 0, 0, 0);
-		}
-
-		int current_time = GetTickCount64();
-		if (current_time - lastModeTime > nextModeTime)
-		{
-			nextModeTime = 999999;
-			lastModeTime = current_time;
-			currentMode++;
-		}
-		else
-		{
-			continue;
-		}
-
-		switch (currentMode)
-		{
-		case 1: // Pause 1s
-			lastModeTime = GetTickCount64();
-			nextModeTime = 1000;
-			
-			SET_TIME_SCALE(0);
-
-			break;
-		case 2: //Quick Skip Forward and play sound
-
-			lastModeTime = GetTickCount64();
-			nextModeTime = 50;
-
-			PLAY_SOUND_FRONTEND(-1, "Failure", "DLC_HEIST_HACKING_SNAKE_SOUNDS", 1);
-			SET_TIME_SCALE(1);
-
-			break;
-		case 3: // Pause again .5s
-			lastModeTime = GetTickCount64();
-			nextModeTime = 500;
-			SET_TIME_SCALE(0);
-			
-
-			break;
-		case 4: // Display message 6s
-			nextModeTime = 6000;
-			message = true;
-
-			break;
-		case 5: // Done
-			message = false;
-			SET_TIME_SCALE(1);
-
-			break;
-		}
+		
 	}
 }
 
