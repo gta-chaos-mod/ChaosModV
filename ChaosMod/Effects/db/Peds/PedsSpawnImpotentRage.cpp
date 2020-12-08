afe68b17f898900c47bb3d7540e6adcc15e225f1
Tommy <45946092+MoneyWasted@users.noreply.github.com>
2020-12-08 17:46:40 -0500

    Update PedsSpawnImpotentRage.cpp

--- ChaosMod/Effects/db/Peds/PedsSpawnImpotentRage.cpp
+++ ChaosMod/Effects/db/Peds/PedsSpawnImpotentRage.cpp
@@ -33,6 +33,10 @@ static void OnStart()
 	TASK_COMBAT_PED(ped, playerPed, 0, 16);
 
 	SET_PED_FIRING_PATTERN(ped, 0xC6EE6B4C);
+	
+	Blip blip = ADD_BLIP_FOR_ENTITY(ped);
+	SET_BLIP_COLOUR(blip, 1);
+	SET_BLIP_AS_FRIENDLY(blip, false);
 
 	if (IS_PED_IN_ANY_VEHICLE(playerPed, false))
 	{
