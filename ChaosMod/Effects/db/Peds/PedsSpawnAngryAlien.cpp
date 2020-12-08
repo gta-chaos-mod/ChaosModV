2de0b71b229dbb0cdfbc69412a7fb8c0b829623f
Tommy <45946092+MoneyWasted@users.noreply.github.com>
2020-12-08 03:12:02 +0000

    Update PedsSpawnAngryAlien.cpp
    Adding blips to spawned peds.

--- ChaosMod/Effects/db/Peds/PedsSpawnAngryAlien.cpp
+++ ChaosMod/Effects/db/Peds/PedsSpawnAngryAlien.cpp
@@ -54,6 +54,10 @@ static void OnStart()
 	TASK_COMBAT_PED(ped, playerPed, 0, 16);
 
 	SET_PED_FIRING_PATTERN(ped, 0xC6EE6B4C);
+	
+	Blip blip = ADD_BLIP_FOR_ENTITY(ped);
+	SET_BLIP_COLOUR(blip, 1);
+	SET_BLIP_AS_FRIENDLY(blip, false);
 }
 
 static RegisterEffect registerEffect(EFFECT_ANGRY_ALIEN, OnStart);
