1637656372c1def932bee08ef496406137c0d102
Kolya Venturi <kolya.venturi@enx3s.com>
2020-12-09 17:23:46 -0700

    Framework EFFECT_PLAYER_BEES

--- ChaosMod/Effects/EffectsInfo.h
+++ ChaosMod/Effects/EffectsInfo.h
@@ -247,6 +247,7 @@ enum EffectType
 	EFFECT_PLAYER_PACIFIST,
 	EFFECT_VEH_TURN_RIGHT,
 	EFFECT_PEDS_BUSBOIS,
+	EFFECT_PLAYER_BEES,
 	_EFFECT_ENUM_MAX
 };
 
@@ -508,4 +509,5 @@ const std::map<EffectType, EffectInfo> g_effectsMap =
 	{EFFECT_PLAYER_PACIFIST, {"Pacifist", "player_pacifist", true, {}, false}},
 	{EFFECT_VEH_TURN_RIGHT, {"Everyone Turn Right", "veh_turnright", true, {}, true}},
 	{EFFECT_PEDS_BUSBOIS, {"Bus Bois", "peds_busbois"}},
+	{EFFECT_PLAYER_BEES, {"Bees", "player_bees", true}},
 };
---
+++ ChaosMod/Effects/db/Player/PlayerBees.cpp
@@ -0,0 +1,11 @@
+#include <stdafx.h>
+
+// Effect by kolyaventuri
+
+static void OnStart() {}
+
+static void OnTick() {}
+
+static void OnStop() {}
+
+static RegisterEffect registerEffect(EFFECT_PLAYER_BEES, OnStart, OnStop, OnTick);
