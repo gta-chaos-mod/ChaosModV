5873b14c8998153526c184ac9172afb320b26166
pongo1231 <pongo1999712@gmail.com>
2020-12-28 19:06:57 +0100

    ChaosMod: Removed unused list in 'Peds Drive-By Player'

--- ChaosMod/Effects/db/Peds/PedsDrivebyPlayer.cpp
+++ ChaosMod/Effects/db/Peds/PedsDrivebyPlayer.cpp
@@ -1,7 +1,5 @@
 #include <stdafx.h>
 
-static std::vector<Ped> m_ranThroughPeds;
-
 static void OnTick()
 {
 	Ped playerPed = PLAYER_PED_ID();
@@ -11,8 +9,6 @@ static void OnTick()
 	{
 		if (!IS_PED_A_PLAYER(ped) && IS_PED_IN_ANY_VEHICLE(ped, false))
 		{
-			m_ranThroughPeds.push_back(ped);
-
 			SET_BLOCKING_OF_NON_TEMPORARY_EVENTS(ped, true);
 
 			GIVE_WEAPON_TO_PED(ped, weaponHash, 9999, true, true);
@@ -21,9 +17,4 @@ static void OnTick()
 	}
 }
 
-static void OnStop()
-{
-	m_ranThroughPeds.clear();
-}
-
-static RegisterEffect registerEffect(EFFECT_PEDS_DRIVEBY, nullptr, OnStop, OnTick);
+static RegisterEffect registerEffect(EFFECT_PEDS_DRIVEBY, nullptr, nullptr, OnTick);
