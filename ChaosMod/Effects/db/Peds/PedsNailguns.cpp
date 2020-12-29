62da373efe9d1c7f141499f7cd5253bf12a7630d
Kolya Venturi <kolya.venturi@enx3s.com>
2020-12-28 22:56:16 -0700

    Attach nailgun object to players hand

--- ChaosMod/Effects/db/Peds/PedsNailguns.cpp
+++ ChaosMod/Effects/db/Peds/PedsNailguns.cpp
@@ -1,9 +1,18 @@
 #include <stdafx.h>
 
 static std::vector<Ped> frozenPeds;
+Entity nailgun;
 
 static bool isFrozen(Ped ped) {
 	return std::find(frozenPeds.begin(), frozenPeds.end(), ped) != frozenPeds.end();
+}
+
+static void OnStart() {
+	Ped player = PLAYER_PED_ID();
+	int bone = GET_PED_BONE_INDEX(player, 0xDEAD); // (kolyaventuri): Right Hand
+
+	nailgun = CREATE_OBJECT(1854391800, 0, 0, 0, false, false, false); // (kolyaventuri): 1854391800 == nailgun hash
+	ATTACH_ENTITY_TO_ENTITY(nailgun, player, bone, 0.15f, 0.05f, 0.01f, 70.0f, 0.0f, 180.f, true, true, false, false, 2, true);
 }
 
 static void OnTick() {
@@ -24,7 +33,8 @@ static void OnStop() {
 		FREEZE_ENTITY_POSITION(ped, false);
 	}
 
+	DELETE_OBJECT(&nailgun);
 	std::vector<Ped>().swap(frozenPeds); // Clear frozenPeds and reallocate
 }
 
-static RegisterEffect registerEffect(EFFECT_PEDS_NAILGUNS, nullptr, OnStop, OnTick);
+static RegisterEffect registerEffect(EFFECT_PEDS_NAILGUNS, OnStart, OnStop, OnTick);
