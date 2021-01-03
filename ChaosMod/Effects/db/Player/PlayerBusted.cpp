33f2bfd9a1e3e3d972aa0217ca3aa8061faeb2b7
Kolya Venturi <kolya.venturi@enx3s.com>
2021-01-02 17:11:58 -0700

    Gracefully handle deaths, and hard-to-bust vehicles

--- ChaosMod/Effects/db/Player/PlayerBusted.cpp
+++ ChaosMod/Effects/db/Player/PlayerBusted.cpp
@@ -1,4 +1,9 @@
 #include <stdafx.h>
+#include "Memory/Hooks/EntityCoordsHook.h"
+
+/*
+* Effect by kolyaventuri
+*/
 
 enum ArrestState {
 	start = 0,
@@ -16,6 +21,7 @@ static int nextModeTime = 0;
 static void OnStart() {
 	Player playerId = PLAYER_ID();
 	Ped player = PLAYER_PED_ID();
+	Vector3 pos = GET_ENTITY_COORDS(player, true);
 	const char* hashKey = cops[g_random.GetRandomInt(0, 2)];
 
 	currentMode = ArrestState::start;
@@ -31,18 +37,34 @@ static void OnStart() {
 	Vehicle veh;
 	boolean inVeh = false;
 	if (IS_PED_IN_ANY_VEHICLE(player, false)) {
-		inVeh = true;
 		veh = GET_VEHICLE_PED_IS_IN(player, false);
-		BRING_VEHICLE_TO_HALT(veh, 3.f, stopTime, 0);
-		TASK_LEAVE_VEHICLE(player, veh, 256);
-		WAIT(stopTime);
+		if (IS_PED_IN_ANY_BOAT(player) || IS_PED_IN_ANY_HELI(player) || IS_PED_IN_ANY_PLANE(player) || IS_PED_IN_ANY_TRAIN(player)) {
+			/*
+			* (kolyaventuri): For now, to avoid complications, we're teleporting the player to a location (police station)
+			* and busting them there if they are in a boat, plane, helicopter, or train. A better solution would be to find the nearest
+			* spot of solid ground, and teleport them there, or find a way to hook into the arrest directly so we can avoid a TP
+			* all together.
+			*/
+			TASK_LEAVE_VEHICLE(player, veh, 16); // Leave by TP
+			WAIT(stopTime);
+
+			Vector3 policeStation = Vector3(491.14f, -963.71f, 27.f);
+			Hooks::EnableFakeTpHook(pos); // Ensure that missions don't fail due to the TP
+			TeleportPlayer(policeStation);
+		} else {
+			// Standard vehicle (car) handling
+			inVeh = true;
+			BRING_VEHICLE_TO_HALT(veh, 3.f, stopTime, 0);
+			TASK_LEAVE_VEHICLE(player, veh, 256);
+			WAIT(stopTime);
+		}
 	}
 
 	/*
 	* (kolyaventuri): Ensure that the police cannot spawn inside of the car model.
 	* It was causing issues where the stars would immediately bump to 2, given the cop was injured
 	*/
-	Vector3 pos = GET_ENTITY_COORDS(player, true);
+	pos = GET_ENTITY_COORDS(player, true);
 	Hash model = GET_ENTITY_MODEL(inVeh ? veh : player);
 	GET_MODEL_DIMENSIONS(model, &minSize, &maxSize);
 	Vector3 modelSize = (maxSize - minSize) * 1.15f;
@@ -50,8 +72,15 @@ static void OnStart() {
 	Vector3 newPos = GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(inVeh ? veh : player, modelSize.x, modelSize.y, 0.0);
 	SET_ENTITY_COORDS(cop, newPos.x, newPos.y, newPos.z, 1, 0, 0, 1);
 
-
 	while (currentMode < ArrestState::cleanup) {
+		if (IS_PED_DEAD_OR_DYING(player, 1)) {
+			// (kolyaventuri): If the player dies, give up
+			SET_ENTITY_AS_MISSION_ENTITY(cop, true, true);
+			DELETE_PED(&cop);
+			Hooks::DisableFakeTpHook();
+			break;
+		}
+
 		// (kolyaventuri): Ensure player cannot move
 		DISABLE_ALL_CONTROL_ACTIONS(0);
 		WAIT(0);
@@ -83,6 +112,7 @@ static void OnStart() {
 			case ArrestState::cleanup:
 				SET_ENTITY_AS_MISSION_ENTITY(cop, true, true);
 				DELETE_PED(&cop);
+				Hooks::DisableFakeTpHook();
 				break;
 		}
 	}
