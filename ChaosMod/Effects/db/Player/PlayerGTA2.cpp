bb58b7bba255fb88a5ac2d5b9066165c7945577b
Kolya Venturi <kolya.venturi@enx3s.com>
2021-01-06 23:14:38 -0700

    Implement basic version

--- ChaosMod/Effects/db/Player/PlayerGTA2.cpp
+++ ChaosMod/Effects/db/Player/PlayerGTA2.cpp
@@ -4,8 +4,28 @@
 * Effect by kolyaventuri
 */
 
-static void OnStart() {}
+static const float height = 40.f;
+static Cam camera;
 
-static void OnStop() {}
+static void OnStart() {
+	Ped player = PLAYER_PED_ID();
+	Vector3 rot = GET_ENTITY_ROTATION(player, 2);
 
-static RegisterEffect registerEffect(EFFECT_PLAYER_GTA_2, OnStart, OnStop);
+	camera = CREATE_CAM("DEFAULT_SCRIPTED_FLY_CAMERA", 1);
+	ATTACH_CAM_TO_ENTITY(camera, player, 0.f, 0.f, height, true);
+	SET_CAM_ROT(camera, -90.f, 0.f, rot.z, 2);
+	RENDER_SCRIPT_CAMS(true, true, 500, 0, 1, 0);
+}
+
+static void OnTick() {
+	Ped player = PLAYER_PED_ID();
+	Vector3 rot = GET_ENTITY_ROTATION(player, 2);
+
+	SET_CAM_ROT(camera, -90.f, 0.f, rot.z, 2);
+}
+
+static void OnStop() {
+	RENDER_SCRIPT_CAMS(false, true, 500, 0, 1, 0);
+}
+
+static RegisterEffect registerEffect(EFFECT_PLAYER_GTA_2, OnStart, OnStop, OnTick);
