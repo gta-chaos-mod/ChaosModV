cecbd19d0a12802cb71ea564e464f2f21b321211
Kolya Venturi <kolya.venturi@enx3s.com>
2021-01-08 17:05:05 -0700

    Maintain camera position w/o being relative to ped

--- ChaosMod/Effects/db/Player/PlayerGTA2.cpp
+++ ChaosMod/Effects/db/Player/PlayerGTA2.cpp
@@ -9,13 +9,19 @@ static const float speedFactor = 1.f + (0.5f / (180.f / 2.236936f));
 static float baseFov;
 static Cam camera;
 
-static void OnStart() {
+static void UpdateLocation() {
 	Ped player = PLAYER_PED_ID();
-	Vector3 rot = GET_ENTITY_ROTATION(player, 2);
+	Vector3 pos = GET_ENTITY_COORDS(player, IS_PED_DEAD_OR_DYING(player, 1));
 
+	SET_CAM_COORD(camera, pos.x, pos.y, pos.z + height);
+}
+
+static void OnStart() {
 	camera = CREATE_CAM("DEFAULT_SCRIPTED_FLY_CAMERA", 1);
-	ATTACH_CAM_TO_ENTITY(camera, player, 0.f, 0.f, height, true);
+	//ATTACH_CAM_TO_ENTITY(camera, player, 0.f, 0.f, height, true);
+
 	SET_CAM_ROT(camera, -90.f, 0.f, 0.0, 2);
+	UpdateLocation();
 	RENDER_SCRIPT_CAMS(true, true, 500, 0, 1, 0);
 	baseFov = GET_CAM_FOV(camera);
 
@@ -24,12 +30,11 @@ static void OnStart() {
 
 static void OnTick() {
 	Ped player = PLAYER_PED_ID();
-	Vector3 rot = GET_ENTITY_ROTATION(player, 2);
 	float speed = GET_ENTITY_SPEED(player);
 	float offset = speed * speedFactor;
 
 	SET_CAM_FOV(camera, baseFov + offset);
-	SET_CAM_ROT(camera, -90.f, 0.f, 0.0, 2);
+	UpdateLocation();
 }
 
 static void OnStop() {
