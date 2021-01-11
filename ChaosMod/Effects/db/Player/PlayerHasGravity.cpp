0b0e0d0d2a57a0e355e903be04ac815a5529e5ce
Juhana <git@nitku.net>
2021-01-11 20:27:53 +0200

    Make the Gravity Field effect much weaker

--- ChaosMod/Effects/db/Player/PlayerHasGravity.cpp
+++ ChaosMod/Effects/db/Player/PlayerHasGravity.cpp
@@ -8,8 +8,6 @@ static void OnTick()
 {
 	Ped player = PLAYER_ID();
 	std::vector<Entity> entities;
-	//make player not die instantly
-	SET_PLAYER_INVINCIBLE(player, true);
 
 	Ped playerPed = PLAYER_PED_ID();
 
@@ -36,9 +34,6 @@ static void OnTick()
 	for (Entity entity : entities)
 	{
 		static float startDistance = 50;
-		static float maxForceDistance = 1;
-		static float maxForce = 80;
-
 		Vector3 entityCoord = GET_ENTITY_COORDS(entity, false);
 
 		float distance = GET_DISTANCE_BETWEEN_COORDS(playerCoord.x, playerCoord.y, playerCoord.z, entityCoord.x, entityCoord.y, entityCoord.z, true);
@@ -48,15 +43,9 @@ static void OnTick()
 			{
 				SET_PED_TO_RAGDOLL(entity, 5000, 5000, 0, true, true, false);
 			}
-			float forceDistance = min(max(0.f, (startDistance - distance)), maxForceDistance);
-			float force = (forceDistance / maxForceDistance) * maxForce;
-			APPLY_FORCE_TO_ENTITY(entity, 3, (entityCoord.x - playerCoord.x) * -1.f, (entityCoord.y - playerCoord.y) * -1.f, (entityCoord.z - playerCoord.z) * -1.f, 0, 0, 0, false, false, true, true, false, true);
+			float force = -0.012f;
+			APPLY_FORCE_TO_ENTITY(entity, 3, (entityCoord.x - playerCoord.x) * force, (entityCoord.y - playerCoord.y) * force, (entityCoord.z - playerCoord.z) * force, 0, 0, 0, false, false, true, true, false, true);
 		}
 	}
 }
-static void OnStop()
-{
-	Ped player = PLAYER_ID();
-	SET_PLAYER_INVINCIBLE(player, false);
-}
-static RegisterEffect registerEffect(EFFECT_PLAYER_GRAVITY, nullptr, OnStop, OnTick);
+static RegisterEffect registerEffect(EFFECT_PLAYER_GRAVITY, nullptr, nullptr, OnTick);
