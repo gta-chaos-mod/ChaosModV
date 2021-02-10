3c4769b0178bd47995c387cecc4a48c648091fd9
ProfessorBiddle <34872564+ProfessorBiddle@users.noreply.github.com>
2021-02-10 10:51:30 -0700

    fixed misnamed variable

--- ChaosMod/Effects/db/Player/PlayerSpinCam.cpp
+++ ChaosMod/Effects/db/Player/PlayerSpinCam.cpp
@@ -3,7 +3,7 @@
 */
 
 #include <stdafx.h>
-static Camera flippedCamera = 0;
+static Camera spinningCamera = 0;
 float camRot = 0.f;
 const float camRotRate = 1.2f;
 
@@ -12,28 +12,28 @@ static void UpdateCamera()
     auto coord = CAM::GET_GAMEPLAY_CAM_COORD();
     auto rot = CAM::GET_GAMEPLAY_CAM_ROT(2);
     auto fov = CAM::GET_GAMEPLAY_CAM_FOV();
-    CAM::SET_CAM_PARAMS(flippedCamera, coord.x, coord.y, coord.z, rot.x, camRot, rot.z, fov, 0, 1, 1, 2);
+    CAM::SET_CAM_PARAMS(spinningCamera, coord.x, coord.y, coord.z, rot.x, camRot, rot.z, fov, 0, 1, 1, 2);
 }
 
 static void OnStart()
 {
-    flippedCamera = CAM::CREATE_CAM("DEFAULT_SCRIPTED_CAMERA", 1);
+    spinningCamera = CAM::CREATE_CAM("DEFAULT_SCRIPTED_CAMERA", 1);
     CAM::RENDER_SCRIPT_CAMS(true, true, 10, 1, 1, 1);
 }
 
 static void OnTick()
 {
     camRot += camRotRate;
-    CAM::SET_CAM_ACTIVE(flippedCamera, true);
+    CAM::SET_CAM_ACTIVE(spinningCamera, true);
     UpdateCamera();
 }
 
 static void OnStop()
 {
-    CAM::SET_CAM_ACTIVE(flippedCamera, false);
+    CAM::SET_CAM_ACTIVE(spinningCamera, false);
     CAM::RENDER_SCRIPT_CAMS(false, true, 700, 1, 1, 1);
-    CAM::DESTROY_CAM(flippedCamera, true);
-    flippedCamera = 0;
+    CAM::DESTROY_CAM(spinningCamera, true);
+    spinningCamera = 0;
 }
 
 static RegisterEffect registerEffect(EFFECT_PLAYER_SPIN_CAMERA, OnStart, OnStop, OnTick);
