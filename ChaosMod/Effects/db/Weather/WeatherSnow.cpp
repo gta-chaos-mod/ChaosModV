6b28cda2ae57caa31feaca35f1c3c1e7d2ed902b
ProfessorBiddle <34872564+ProfessorBiddle@users.noreply.github.com>
2021-02-16 15:21:58 -0700

    Make effect 'Snow' reduce vehicle grip
    A little more like driving in real snow now.

--- ChaosMod/Effects/db/Weather/WeatherSnow.cpp
+++ ChaosMod/Effects/db/Weather/WeatherSnow.cpp
@@ -5,13 +5,26 @@ static void OnStop()
 	Memory::SetSnow(false);
 
 	SET_WEATHER_TYPE_NOW("EXTRASUNNY");
+
+	for (auto veh : GetAllVehs())
+	{
+		SET_VEHICLE_REDUCE_GRIP(veh, false);
+	}
 }
 
 static void OnTick()
 {
 	Memory::SetSnow(true);
-
 	SET_WEATHER_TYPE_NOW("XMAS");
+
+	for (auto veh : GetAllVehs())
+	{
+		static bool toggle = true;
+
+		SET_VEHICLE_REDUCE_GRIP(veh, toggle);
+
+		toggle = !toggle;
+	}
 }
 
 static RegisterEffect registerEffect(EFFECT_SNOW, nullptr, OnStop, OnTick);
