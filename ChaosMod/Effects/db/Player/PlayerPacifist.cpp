d84faa97ef3c220154901738ebd4ff71c2ee59cb
ProfessorBiddle <34872564+ProfessorBiddle@users.noreply.github.com>
2021-02-17 15:47:22 -0700

    Improve Pacifist effect to make sense.
    With the help of Pongo, finally found a way to detect when the player hits someone. This works way better and makes much more sense than the old effect.

--- ChaosMod/Effects/db/Player/PlayerPacifist.cpp
+++ ChaosMod/Effects/db/Player/PlayerPacifist.cpp
@@ -2,19 +2,40 @@
 //Effect by ProfessorBiddle, requested thousands of times on discord
 static void OnTick()
 {
-	Player player = PLAYER_ID();
+	int hits0, hits1, hits2;
+	STAT_GET_INT(GET_HASH_KEY("SP0_HITS"), &hits0, -1);
+	STAT_GET_INT(GET_HASH_KEY("SP1_HITS"), &hits1, -1);
+	STAT_GET_INT(GET_HASH_KEY("SP2_HITS"), &hits2, -1);
+	
+	WAIT(0);
 
-	Entity target;
+	Ped playerPed = PLAYER_PED_ID();
 
-	STOP_GAMEPLAY_CAM_SHAKING(true);
+	int hits0Post, hits1Post, hits2Post;
 
-	if (GET_ENTITY_PLAYER_IS_FREE_AIMING_AT(player, &target))
+	switch (GET_ENTITY_MODEL(playerPed))
 	{
-		if ((IS_ENTITY_A_PED(target) || IS_ENTITY_A_VEHICLE(target)) && !IS_ENTITY_DEAD(target, false))
+	case 225514697: // Michael 
+		STAT_GET_INT(GET_HASH_KEY("SP0_HITS"), &hits0Post, -1);
+		if (hits0Post > hits0)
 		{
-			Ped playerPed = PLAYER_PED_ID();
 			SET_ENTITY_HEALTH(playerPed, 0, 0);
 		}
+		break;
+	case 2602752943: // Franklin
+		STAT_GET_INT(GET_HASH_KEY("SP1_HITS"), &hits1Post, -1);
+		if (hits1Post > hits1)
+		{
+			SET_ENTITY_HEALTH(playerPed, 0, 0);
+		}
+		break;
+	case 2608926626: // Trevor
+		STAT_GET_INT(GET_HASH_KEY("SP2_HITS"), &hits2Post, -1);
+		if (hits2Post > hits2)
+		{
+			SET_ENTITY_HEALTH(playerPed, 0, 0);
+		}
+		break;
 	}
 }
 static RegisterEffect registerEffect(EFFECT_PLAYER_PACIFIST, nullptr, nullptr, OnTick);
