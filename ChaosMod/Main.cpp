ee35e0194696746a099d3a62cd4d6c8e3a12d574
pongo1231 <pongo1999712@gmail.com>
2021-02-18 22:35:00 +0100

    Bump version to 1.9.0.4

--- ChaosMod/Main.cpp
+++ ChaosMod/Main.cpp
@@ -328,7 +328,7 @@ void Main::Loop()
 
 		if (splashTextTime > 0)
 		{
-			DrawScreenText("Chaos Mod v1.9.0.3 by pongo1231\n\nSee credits.txt for list of contributors",
+			DrawScreenText("Chaos Mod v1.9.0.4 by pongo1231\n\nSee credits.txt for list of contributors",
 				{ .2f, .3f }, .65f, { 60, 245, 190 }, true);
 
 #ifdef _DEBUG
