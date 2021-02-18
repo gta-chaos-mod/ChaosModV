7466eeb0460fd92b8a65e5bc536a7a209cbb5d8c
pongo1231 <pongo1999712@gmail.com>
2021-02-18 21:29:01 +0100

    ChaosMod: Disable TestHook

--- ChaosMod/Memory/Hooks/TestHook.cpp
+++ ChaosMod/Memory/Hooks/TestHook.cpp
@@ -14,12 +14,12 @@ static bool OnHook()
 
 	//
 
-	handle = Memory::FindPattern("E8 ? ? ? ? 4C 8B 4F 08 45 33 C0 66 45 39 41 ? 74 27 41 0F B7 49 ? 33 D2 8B C3 F7 F1 49 8B 01 48 8B 14 D0 EB 08 3B 1A 74 0B 48 8B 52 20 48 85 D2 75 F3 EB 04 4C 8D 42 08 49 8B C0 48 8B 5C 24 ? 48 83 C4 20 5F C3 33 C9");
+	/*handle = Memory::FindPattern("E8 ? ? ? ? 4C 8B 4F 08 45 33 C0 66 45 39 41 ? 74 27 41 0F B7 49 ? 33 D2 8B C3 F7 F1 49 8B 01 48 8B 14 D0 EB 08 3B 1A 74 0B 48 8B 52 20 48 85 D2 75 F3 EB 04 4C 8D 42 08 49 8B C0 48 8B 5C 24 ? 48 83 C4 20 5F C3 33 C9");
 	if (!handle.IsValid())
 	{
 		return false;
 	}
-	Memory::AddHook(handle.Into().Get<void>(), HK_sub_7FF788ADE188, &OG_sub_7FF788ADE188);
+	Memory::AddHook(handle.Into().Get<void>(), HK_sub_7FF788ADE188, &OG_sub_7FF788ADE188);*/
 
 	//
 
