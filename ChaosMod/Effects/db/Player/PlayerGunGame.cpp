75bd0fe4f9c26198a1faa57b3b22bdd6e11d6b60
ProfessorBiddle <34872564+ProfessorBiddle@users.noreply.github.com>
2021-02-17 18:04:13 -0700

    wip

--- ChaosMod/Effects/db/Player/PlayerGunGame.cpp
+++ ChaosMod/Effects/db/Player/PlayerGunGame.cpp
@@ -2,12 +2,26 @@
 #include <stdafx.h>
 
 //Effect by ProfessorBiddle
-
+int kills;
+Hash killsHash = GET_HASH_KEY("SP0_KILLS");
 static void OnStart()
 {
 	Ped playerPed = PLAYER_PED_ID();
 	REMOVE_ALL_PED_WEAPONS(playerPed, 0);
 	GIVE_WEAPON_TO_PED(playerPed, GET_HASH_KEY("WEAPON_PISTOL"), 9999, false, true);
+	switch (GET_ENTITY_MODEL(playerPed))
+	{
+	case 225514697: // Michael 
+		kills = STAT_GET_INT(GET_HASH_KEY())
+		break;
+	case 2602752943: // Franklin
+
+		break;
+	case 2608926626: // Trevor
+
+		break;
+	}
+
 }
 
 static void OnTick()
