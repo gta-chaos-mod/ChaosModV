8a2f6a3f24025ab4437561b9f0416073a967f786
ProfessorBiddle <34872564+ProfessorBiddle@users.noreply.github.com>
2021-02-17 18:38:48 -0700

    Add effect Gun Game
    Starts you with a pistol, and gives you a new weapon every time you kill someone.
    
    This script works, but it doesn't always detect when someone is killed. If anyone has a solution to that it would be awesome.

--- ChaosMod/Effects/db/Player/PlayerGunGame.cpp
+++ ChaosMod/Effects/db/Player/PlayerGunGame.cpp
@@ -9,57 +9,67 @@ static void OnStart()
 	Ped playerPed = PLAYER_PED_ID();
 	REMOVE_ALL_PED_WEAPONS(playerPed, 0);
 	GIVE_WEAPON_TO_PED(playerPed, GET_HASH_KEY("WEAPON_PISTOL"), 9999, false, true);
-	switch (GET_ENTITY_MODEL(playerPed))
-	{
-	case 225514697: // Michael 
-		kills = STAT_GET_INT(GET_HASH_KEY())
-		break;
-	case 2602752943: // Franklin
-
-		break;
-	case 2608926626: // Trevor
-
-		break;
-	}
-
 }
 
 static void OnTick()
 {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	Ped playerPed = PLAYER_PED_ID();
+	Hash playerHash = GET_ENTITY_MODEL(playerPed);
+	Hash killHash;
+	int playerKills, playerKillsNew;
 
 	Weapon wepGive;
-	Ped playerPed = PLAYER_PED_ID();
-	if (IS_PED_SHOOTING(playerPed))
+
+	//get current kills
+	switch (playerHash)
 	{
-		REMOVE_ALL_PED_WEAPONS(playerPed, 0);	
-		static const std::vector<Hash>& weps = Memory::GetAllWeapons();
-		wepGive = weps[g_random.GetRandomInt(0, weps.size() - 1)];
-		//make sure it's actually a gun, not a broken bottle etc.
-		while (GET_WEAPON_CLIP_SIZE(wepGive) < 2)
-		{
-			wepGive = weps[g_random.GetRandomInt(0, weps.size() - 1)];
-		}
-		//player always has pistol because sometimes the other weapon doesn't work
-		GIVE_WEAPON_TO_PED(playerPed, GET_HASH_KEY("WEAPON_PISTOL"), 9999, false, true);
-		GIVE_WEAPON_TO_PED(playerPed, wepGive, 9999, false, true);
+	case 225514697: // Michael 
+		killHash = GET_HASH_KEY("SP0_KILLS");
+		break;
+	case 2602752943: // Franklin
+		killHash = GET_HASH_KEY("SP0_KILLS");
+		break;
+	case 2608926626: // Trevor
+		killHash = GET_HASH_KEY("SP0_KILLS");
+		break;
 	}
+	STAT_GET_INT(killHash, &playerKills, -1);
 
+	WAIT(0);
 
+	//check if player changed
+	playerPed = PLAYER_PED_ID();
+	Hash newPlayerHash = GET_ENTITY_MODEL(playerPed);
+	if (newPlayerHash = playerHash)
+	{
+		switch (playerHash)
+		{
+		case 225514697: // Michael 
+			killHash = GET_HASH_KEY("SP0_KILLS");
+			break;
+		case 2602752943: // Franklin
+			killHash = GET_HASH_KEY("SP0_KILLS");
+			break;
+		case 2608926626: // Trevor
+			killHash = GET_HASH_KEY("SP0_KILLS");
+			break;
+		}
+		STAT_GET_INT(killHash, &playerKillsNew, -1);
+		if (playerKillsNew > playerKills)
+		{
+			REMOVE_ALL_PED_WEAPONS(playerPed, 0);
+			static const std::vector<Hash>& weps = Memory::GetAllWeapons();
+			wepGive = weps[g_random.GetRandomInt(0, weps.size() - 1)];
+			//make sure it's actually a gun, not a broken bottle etc.
+			while (GET_WEAPON_CLIP_SIZE(wepGive) < 2)
+			{
+				wepGive = weps[g_random.GetRandomInt(0, weps.size() - 1)];
+			}
+			//player always has pistol because sometimes the other weapon doesn't work
+			GIVE_WEAPON_TO_PED(playerPed, GET_HASH_KEY("WEAPON_PISTOL"), 9999, false, true);
+			GIVE_WEAPON_TO_PED(playerPed, wepGive, 9999, false, true);
+		}
+	}
 }
 static RegisterEffect registerEffect(EFFECT_PLAYER_GUN_GAME, OnStart, nullptr, OnTick);
