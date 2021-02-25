205be816fb1b31698444ab4dd308f7b593ec8fce
Niclas <niclasdriver@gmail.com>
2021-02-25 18:34:20 +0100

    Updated Flashbang effect - thanks for all the input

--- ChaosMod/Effects/db/Misc/MiscFlashbang.cpp
+++ ChaosMod/Effects/db/Misc/MiscFlashbang.cpp
@@ -4,24 +4,18 @@
 */
 
 #include <stdafx.h>
-static int f_alpha = 510;
 
-static void OnTick()
+static void OnStart()
 {
-	DISABLE_CONTROL_ACTION(0, 199, true);
-	DISABLE_CONTROL_ACTION(0, 200, true);
-	HIDE_HUD_AND_RADAR_THIS_FRAME();
+	int f_alpha = 510;
 
-	DRAW_RECT(.5f, .5f, 1.f, 1.f, 255, 255, 255, f_alpha, false);
-	if (f_alpha > 0)
+	while (f_alpha > 0)
 	{
+		DRAW_RECT(.5f, .5f, 1.f, 1.f, 255, 255, 255, f_alpha, false);
 		f_alpha--;
+		HIDE_HUD_AND_RADAR_THIS_FRAME();
+		WAIT(0);
 	}
 }
 
-static void OnStop()
-{
-	f_alpha = 510;
-}
-
-static RegisterEffect registerEffect(EFFECT_MISC_FLASHBANG, nullptr, OnStop, OnTick);
+static RegisterEffect registerEffect(EFFECT_MISC_FLASHBANG, OnStart, nullptr, nullptr);
