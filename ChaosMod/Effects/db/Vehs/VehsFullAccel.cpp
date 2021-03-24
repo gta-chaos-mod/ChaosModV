a031a94abfa51ea9ffdbb487bff9f3d4920a7a89
pongo1231 <pongo1999712@gmail.com>
2021-03-24 18:27:06 +0100

    ChaosMod: 'Full Acceleration' now honors braking

--- ChaosMod/Effects/db/Vehs/VehsFullAccel.cpp
+++ ChaosMod/Effects/db/Vehs/VehsFullAccel.cpp
@@ -6,7 +6,7 @@ static void OnTick()
 	{
 		Hash vehModel = GET_ENTITY_MODEL(veh);
 
-		if (IS_VEHICLE_ON_ALL_WHEELS(veh) || IS_THIS_MODEL_A_PLANE(vehModel) || IS_THIS_MODEL_A_HELI(vehModel))
+		if (!Memory::IsVehicleBraking(veh) && (IS_VEHICLE_ON_ALL_WHEELS(veh) || IS_THIS_MODEL_A_PLANE(vehModel) || IS_THIS_MODEL_A_HELI(vehModel)))
 		{
 			SET_VEHICLE_FORWARD_SPEED(veh, GET_VEHICLE_MODEL_ESTIMATED_MAX_SPEED(GET_ENTITY_MODEL(veh)) * 2);
 		}
