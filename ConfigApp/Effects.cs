22ff285a0cebaa4eda891db78cf8a2c605990b55
Last0xygen <41595552+Last0xygen@users.noreply.github.com>
2021-04-06 23:41:07 +0200

    Update Effects.cs

--- ConfigApp/Effects.cs
+++ ConfigApp/Effects.cs
@@ -589,7 +589,7 @@ namespace ConfigApp
             {EffectType.EFFECT_PLAYER_AIMBOT, new EffectInfo("Aimbot", EffectCategory.PLAYER, "player_aimbot", true)},
             {EffectType.EFFECT_PEDS_SPAWN_BIKER, new EffectInfo("Spawn Biker", EffectCategory.PEDS, "peds_spawn_biker")},
             {EffectType.EFFECT_PEDS_SPAWN_SPACE_RANGER, new EffectInfo("Spawn Space Ranger", EffectCategory.PEDS, "peds_spawn_space_ranger")},
-            {EffectType.EFFECT_PEDS_TANK_BOIS, new EffectInfo("Tank Bois", EffectCategory.PEDS, "peds_tank_bois")},
+            {EffectType.EFFECT_PEDS_TANK_BOIS, new EffectInfo("Tanks A Lot", EffectCategory.PEDS, "peds_tank_bois")},
         };
     }
 }
