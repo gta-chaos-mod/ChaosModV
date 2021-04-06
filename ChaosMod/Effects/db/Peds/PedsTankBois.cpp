d67353d4f110d318d37f39259cc352fdc7251fec
Last0xygen <41595552+Last0xygen@users.noreply.github.com>
2021-04-06 22:21:03 +0200

    Added Effect 'Tank Bois' Same as 'Bus Bois' but with tanks lol

--- ChaosMod/Effects/db/Peds/PedsTankBois.cpp
+++ ChaosMod/Effects/db/Peds/PedsTankBois.cpp
@@ -1,37 +1,38 @@
+/*
+Effect by Last0xygen
+*/
+
 #include <stdafx.h>
 
-//effect by ProfessorBiddle
 
 static void OnStart()
 {
-	//not sure exactly what to make this, 60 seemed to work ok
 	static const float maxDistance = 60.f;
-
-	static const Hash busHash = GET_HASH_KEY("BUS");
+	static const Hash vehHash = GET_HASH_KEY("rhino");
 
 	Ped playerPed = PLAYER_PED_ID();
+	Vector3 playerPos = GET_ENTITY_COORDS(playerPed, true);
 
 	for (Ped ped : GetAllPeds())
 	{
 		if (!IS_PED_A_PLAYER(ped) && !IS_PED_DEAD_OR_DYING(ped, false))
 		{
 			Vector3 pedPos = GET_ENTITY_COORDS(ped, true);
-			Vector3 playerPos = GET_ENTITY_COORDS(playerPed, true);
 			//check if player is far away from entity
-			if (GET_DISTANCE_BETWEEN_COORDS(pedPos.x, pedPos.y, pedPos.z, playerPos.x, playerPos.y, playerPos.z, true) <= maxDistance)
+			if (pedPos.DistanceTo(playerPos) <= maxDistance)
 			{
 				if (IS_PED_IN_ANY_VEHICLE(ped, false))
 				{
 					Vehicle veh = GET_VEHICLE_PED_IS_IN(ped, false);
 
-					if (GET_ENTITY_MODEL(veh) == busHash)
+					if (GET_ENTITY_MODEL(veh) == vehHash)
 					{
 						continue;
 					}
 				}
 
-				Vector3 pedPos = GET_ENTITY_COORDS(ped, false);
 				float pedHeading = GET_ENTITY_HEADING(ped);
+				Vector3 vel = GET_ENTITY_VELOCITY(ped);
 
 				SET_ENTITY_COORDS(ped, pedPos.x, pedPos.y, pedPos.z + 10.f, false, false, false, false);
 
@@ -39,19 +40,21 @@ static void OnStart()
 
 				SET_BLOCKING_OF_NON_TEMPORARY_EVENTS(ped, true);
 
-				Vehicle veh = CreateTempVehicle(busHash, pedPos.x, pedPos.y, pedPos.z, pedHeading);
+				Vehicle veh = CreateTempVehicle(vehHash, pedPos.x, pedPos.y, pedPos.z, pedHeading);
 				SET_PED_INTO_VEHICLE(ped, veh, -1);
+				SET_VEHICLE_ENGINE_ON(veh, true, true, true);
+				SET_ENTITY_VELOCITY(veh, vel.x, vel.y, vel.z);
 
 				TASK_VEHICLE_MISSION_PED_TARGET(ped, veh, playerPed, 13, 9999.f, 4176732, .0f, .0f, false);
-			}	
+			}
 		}
 	}
 }
 
-static RegisterEffect registerEffect(EFFECT_PEDS_BUSBOIS, OnStart, EffectInfo
-	{
-		.Name = "Bus Bois",
-		.Id = "peds_busbois",
+static RegisterEffect registerEffect(EFFECT_PEDS_TANK_BOIS, OnStart, nullptr, nullptr, EffectInfo
+    {
+        .Name = "Tank Bois",
+        .Id = "peds_tank_bois",
 		.EffectGroupType = EffectGroupType::SPAWN_GENERIC
-	}
+    }
 );
