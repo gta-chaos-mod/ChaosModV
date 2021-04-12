f3d66efdba8b259729583bc4249684b41309b146
BuildTools <admin@joellang.ch>
2021-04-12 17:51:52 +0200

    Added Fake Detonate Current Vehicle

---
+++ ChaosMod/Effects/db/Vehs/VehsPlayerFakeVehExplode.cpp
@@ -0,0 +1,47 @@
+#include <stdafx.h>
+
+#define DETONATE_TIMER 5000
+
+static void OnStart()
+{
+	Ped playerPed = PLAYER_PED_ID();
+
+	if (!IS_PED_IN_ANY_VEHICLE(playerPed, false))
+	{
+		return;
+	}
+
+	Vehicle veh = GET_VEHICLE_PED_IS_IN(playerPed, false);
+
+	int lastTimestamp = GET_GAME_TIMER();
+
+	int detonateTimer = DETONATE_TIMER;
+	int beepTimer = DETONATE_TIMER;
+	while (DOES_ENTITY_EXIST(veh))
+	{
+		WAIT(0);
+
+		int curTimestamp = GET_GAME_TIMER();
+
+		if ((detonateTimer -= curTimestamp - lastTimestamp) < beepTimer)
+		{
+			beepTimer *= .8f;
+
+			PLAY_SOUND_FROM_ENTITY(-1, "Beep_Red", veh, "DLC_HEIST_HACKING_SNAKE_SOUNDS", true, false);
+		}
+
+		if (detonateTimer <= 0)
+		{
+			break;
+		}
+
+		lastTimestamp = curTimestamp;
+	}
+}
+
+static RegisterEffect registerEffect(EFFECT_EXPLODE_CUR_VEH, OnStart, EffectInfo
+	{
+		.Name = "Fake Detonate Current Vehicle",
+		.Id = "playerveh_fake_explode"
+	}
+);
