725211e7f126e5091fb46fd37f04896d3b0ff0ff
ubscal <ubscal99@gmail.com>
2021-04-30 12:58:22 -0500

    Add files via upload

---
+++ ChaosMod/Effects/db/Peds/PedsMowerMates.cpp
@@ -0,0 +1,48 @@
+#include <stdafx.h>
+
+//effect by ubscal, modified from Scooter Brotehrs
+
+static void OnStart()
+{
+	static const Hash mowerHash = GET_HASH_KEY("MOWER");
+
+	Ped playerPed = PLAYER_PED_ID();
+
+	for (Ped ped : GetAllPeds())
+	{
+		if (!IS_PED_A_PLAYER(ped) && !IS_PED_DEAD_OR_DYING(ped, false))
+		{
+			if (IS_PED_IN_ANY_VEHICLE(ped, false))
+			{
+				Vehicle veh = GET_VEHICLE_PED_IS_IN(ped, false);
+
+				if (GET_ENTITY_MODEL(veh) == mowerHash)
+				{
+					continue;
+				}
+			}
+
+				Vector3 pedPos = GET_ENTITY_COORDS(ped, false);
+				float pedHeading = GET_ENTITY_HEADING(ped);
+
+				SET_ENTITY_COORDS(ped, pedPos.x, pedPos.y, pedPos.z + 10.f, false, false, false, false);
+
+				SET_PED_COMBAT_ATTRIBUTES(ped, 3, false); // Don't allow them to leave vehicle by themselves
+
+				SET_BLOCKING_OF_NON_TEMPORARY_EVENTS(ped, true);
+
+				Vehicle veh = CreateTempVehicle(mowerHash, pedPos.x, pedPos.y, pedPos.z, pedHeading);
+				SET_PED_INTO_VEHICLE(ped, veh, -1);
+
+				TASK_VEHICLE_MISSION_PED_TARGET(ped, veh, playerPed, 13, 9999.f, 4176732, .0f, .0f, false);
+			}
+		}
+	}
+}
+
+static RegisterEffect registerEffect(EFFECT_PEDS_MOWERMATES, OnStart, EffectInfo
+	{
+		.Name = "Mower Mates",
+		.Id = "peds_mowermates"
+	}
+);
