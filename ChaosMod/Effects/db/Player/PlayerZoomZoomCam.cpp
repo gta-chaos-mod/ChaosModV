c5d9d496d92539d787890c89be7f598c82a0b842
Moxi <83367710+Mo-xii@users.noreply.github.com>
2021-05-01 13:06:50 +0200

    Update PlayerZoomZoomCam.cpp

--- ChaosMod/Effects/db/Player/PlayerZoomZoomCam.cpp
+++ ChaosMod/Effects/db/Player/PlayerZoomZoomCam.cpp
@@ -1,24 +1,24 @@
 /*
-    Effect by Moxi, based on code from DrUnderscore & ProfessorBiddle
+    Effect by Moxi, based on code from DrUnderscore & ProfessorBiddle, cleaned by Last0xygen.
 */
 
 #include <stdafx.h>
 static Camera zoomCamera = 0;
-float camZoom = 80.f;
-const float camZoomRate = 0.6f;
-int n = 0;
+static float camZoom = 80.f;
+static float camZoomRate = 0.6f;
 
 static void UpdateCamera()
 {
-    auto coord = CAM::GET_GAMEPLAY_CAM_COORD();
-    auto rot = CAM::GET_GAMEPLAY_CAM_ROT(2);
-    CAM::SET_CAM_PARAMS(zoomCamera, coord.x, coord.y, coord.z, rot.x, rot.y, rot.z, camZoom, 0, 1, 1, 2);
+    Vector3 coord = GET_GAMEPLAY_CAM_COORD();
+    Vector3 rot = GET_GAMEPLAY_CAM_ROT(2);
+    SET_CAM_PARAMS(zoomCamera, coord.x, coord.y, coord.z, rot.x, rot.y, rot.z, camZoom, 0, 1, 1, 2);
 }
 
 static void OnStart()
 {
-    zoomCamera = CAM::CREATE_CAM("DEFAULT_SCRIPTED_CAMERA", 1);
-    CAM::RENDER_SCRIPT_CAMS(true, true, 10, 1, 1, 1);
+    zoomCamera = CREATE_CAM("DEFAULT_SCRIPTED_CAMERA", 1);
+    RENDER_SCRIPT_CAMS(true, true, 10, 1, 1, 1);
+    camZoom = 80.f;
 }
 
 static void OnTick()
@@ -30,34 +30,22 @@ static void OnTick()
     {
         lastTick = curTick;
 
-        if (camZoom < 11)
+        if (camZoom < 11 || camZoom > 119)
         {
-            n = 0;
+            camZoomRate = camZoomRate*-1;
         }
-        if (camZoom > 119)
-        {
-            n = 1;
-        }
-        if (n == 0)
-        {
-            camZoom += camZoomRate;
-            CAM::SET_CAM_ACTIVE(zoomCamera, true);
-            UpdateCamera();
-        }
-        if (n == 1)
-        {
-            camZoom -= camZoomRate;
-            CAM::SET_CAM_ACTIVE(zoomCamera, true);
-            UpdateCamera();
-        }
+        camZoom += camZoomRate;
     }
+
+    SET_CAM_ACTIVE(zoomCamera, true);
+    UpdateCamera();
 }
 
 static void OnStop()
 {
-    CAM::SET_CAM_ACTIVE(zoomCamera, false);
-    CAM::RENDER_SCRIPT_CAMS(false, true, 700, 1, 1, 1);
-    CAM::DESTROY_CAM(zoomCamera, true);
+    SET_CAM_ACTIVE(zoomCamera, false);
+    RENDER_SCRIPT_CAMS(false, true, 700, 1, 1, 1);
+    DESTROY_CAM(zoomCamera, true);
     zoomCamera = 0;
 }
 
