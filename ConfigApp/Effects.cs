f94519b9ff9db778149dc264bd68b1c3499de939
ubscal <ubscal99@gmail.com>
2021-06-02 22:08:41 -0500

    Update Effects.cs

--- ConfigApp/Effects.cs
+++ ConfigApp/Effects.cs
@@ -589,7 +589,7 @@ namespace ConfigApp
             {EffectType.EFFECT_PLAYER_AIMBOT, new EffectInfo("Aimbot", EffectCategory.PLAYER, "player_aimbot", true)},
             {EffectType.EFFECT_PEDS_SPAWN_BIKER, new EffectInfo("Spawn Biker", EffectCategory.PEDS, "peds_spawn_biker")},
             {EffectType.EFFECT_PEDS_SPAWN_SPACE_RANGER, new EffectInfo("Spawn Space Ranger", EffectCategory.PEDS, "peds_spawn_space_ranger")},
-            {EffectType.EFFECT_REPOSSESSION, new EffectInfo("Repossession", EffectCategory.VEHICLE, "veh_repossession", true, true) },
+            {EffectType.EFFECT_REPOSSESSION, new EffectInfo("Repossession", EffectCategory.VEHICLE, "veh_repossession")},
         };
     }
 }
