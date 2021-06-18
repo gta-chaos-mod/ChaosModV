754f36536219d37dce776a6ed4a6108fb28fa8bc
Moxi <83367710+Mo-xii@users.noreply.github.com>
2021-06-18 18:50:04 +0200

    Update PedsSeriousKamikazes.cpp
    . Using std::map instead of std::list to better keep track of ped + bomb and removing the bombs on peds death
    
    . Attaching the bomb to the head instead of the body, thus always in the correct position even during animation. Also rotating the bomb to face forward
    
    . Kamikazes are now using the upper body "hands up" animation to mimic Serious Sam's enemies

--- ChaosMod/Effects/db/Peds/PedsSeriousKamikazes.cpp
+++ ChaosMod/Effects/db/Peds/PedsSeriousKamikazes.cpp
@@ -1,11 +1,10 @@
 /*
-	Effect by Moxi based on Killer Clowns by Last0xygen
+	Effect by Moxi based on Killer Clowns by Last0xygen, and a lot of help and suggestions!
 */
 
 #include <stdafx.h>
 
-static std::list<Ped> kamikazeEnemies;
-static std::list<Object> bombObject;
+static std::map<Ped, Entity> kamiBomb;
 static int spawnTimer = -1;
 static Hash relationshipGroup;
 static int maxKamikazesToSpawn = 5;
@@ -13,19 +12,18 @@ static int maxKamikazesToSpawn = 5;
 static Hash kamikazeHash = GET_HASH_KEY("a_m_y_musclbeac_01");
 static Hash bombHash = GET_HASH_KEY("imp_prop_bomb_ball");
 
+static const char* animDict = "missminuteman_1ig_2";
+
 static void OnStop()
 {
-	for (Ped ped : kamikazeEnemies)
+	for (std::map<Ped, Entity>::iterator it = kamiBomb.begin(); it != kamiBomb.end(); ++it)
 	{
-		SET_PED_AS_NO_LONGER_NEEDED(&ped);
-	}
-	kamikazeEnemies.clear();
+		Ped kamikaze = it->first;
+		Entity bomb = it->second;
 
-	for (Object bomb : bombObject)
-	{
-		SET_OBJECT_AS_NO_LONGER_NEEDED(&bomb);
+		SET_PED_AS_NO_LONGER_NEEDED(&kamikaze);
+		SET_ENTITY_AS_NO_LONGER_NEEDED(&bomb);
 	}
-	bombObject.clear();
 }
 
 static void OnStart()
@@ -43,9 +41,10 @@ static void OnTick()
 	Vector3 playerPos = GET_ENTITY_COORDS(playerPed, false);
 	int current_time = GET_GAME_TIMER();
 
-	for (std::list<Entity>::iterator it = kamikazeEnemies.begin(); it != kamikazeEnemies.end(); )
+	for (std::map<Ped, Entity>::iterator it = kamiBomb.begin(); it != kamiBomb.end(); )
 	{
-		Ped kamikaze = *it;
+		Ped kamikaze = it->first;
+		Entity bomb = it->second;
 		Vector3 kamikazePos = GET_ENTITY_COORDS(kamikaze, false);
 		if (IS_PED_DEAD_OR_DYING(kamikaze, false) || IS_PED_INJURED(kamikaze) || GET_DISTANCE_BETWEEN_COORDS(playerPos.x, playerPos.y, playerPos.z, kamikazePos.x, kamikazePos.y, kamikazePos.z, false) > 100.f)
 		{
@@ -53,7 +52,8 @@ static void OnTick()
 			SET_ENTITY_ALPHA(kamikaze, 0, true);
 			SET_PED_AS_NO_LONGER_NEEDED(&kamikaze);
 			DELETE_PED(&kamikaze);
-			it = kamikazeEnemies.erase(it);
+			DELETE_ENTITY(&bomb);
+			it = kamiBomb.erase(it);
 			WAIT(0);
 		}
 		else
@@ -62,7 +62,7 @@ static void OnTick()
 		}
 	}
 
-	if (kamikazeEnemies.size() < maxKamikazesToSpawn && current_time > spawnTimer + 2000)
+	if (kamiBomb.size() < maxKamikazesToSpawn && current_time > spawnTimer + 2000)
 	{
 		spawnTimer = current_time;
 		Vector3 spawnPos = GetCoordAround(playerPed, g_Random.GetRandomInt(0, 360), 15, 0, true);
@@ -74,37 +74,47 @@ static void OnTick()
 		SET_BLOCKING_OF_NON_TEMPORARY_EVENTS(ped, true);
 		SET_PED_RELATIONSHIP_GROUP_HASH(ped, relationshipGroup);
 		SET_PED_HEARING_RANGE(ped, 9999.f);
-		kamikazeEnemies.push_back(ped);
+
+		int bone = GET_PED_BONE_INDEX(ped, 0x796E);
 
 		Object bomb = CREATE_OBJECT(bombHash, playerPos.x, playerPos.y + 2, playerPos.z, true, false, false);
-		ATTACH_ENTITY_TO_ENTITY(bomb, ped, 0, 0, 0, 1.1, 0, 0, 115, true, false, false, false, 0, true);
-		bombObject.push_back(bomb);
+		ATTACH_ENTITY_TO_ENTITY(bomb, ped, bone, 0.58, 0, 0, 90, -30, 90, true, false, false, false, 0, true);
 
-		TASK_FOLLOW_TO_OFFSET_OF_ENTITY(ped, playerPed, .0f, .0f, .0f, 9999.f, -1, .0f, true);
+		kamiBomb[ped] = bomb;
+
+		TASK_FOLLOW_TO_OFFSET_OF_ENTITY(ped, playerPed, .0f, .0f, .0f, 10.f, -1, .0f, true);
 		PLAY_PAIN(ped, 8, 0, 0);
+
+		REQUEST_ANIM_DICT(animDict);
+		while (!HAS_ANIM_DICT_LOADED(animDict))
+		{
+			WAIT(0);
+		}
+		TASK_PLAY_ANIM(ped, animDict, "handsup_base", 8, 8, -1, 49, 0, false, false, false);
 	}
 
-	for (Ped ped : kamikazeEnemies)
+	for (std::map<Ped, Entity>::iterator it = kamiBomb.begin(); it != kamiBomb.end(); ++it)
 	{
-		int maxHealth = GET_ENTITY_MAX_HEALTH(ped);
+		Ped kamikaze = it->first;
+		int maxHealth = GET_ENTITY_MAX_HEALTH(kamikaze);
 
-		Vehicle playerVeh = GET_VEHICLE_PED_IS_IN(ped, false);
+		Vehicle playerVeh = GET_VEHICLE_PED_IS_IN(playerPed, false);
 
-		if (maxHealth > 0 && (IS_PED_INJURED(ped) || IS_PED_RAGDOLL(ped) || IS_ENTITY_TOUCHING_ENTITY(ped, playerPed) || IS_ENTITY_TOUCHING_ENTITY(ped, playerVeh)))
+		if (maxHealth > 0 && (IS_PED_INJURED(kamikaze) || IS_PED_RAGDOLL(kamikaze) || IS_ENTITY_TOUCHING_ENTITY(kamikaze, playerPed) || IS_ENTITY_TOUCHING_ENTITY(kamikaze, playerVeh)))
 		{
-			Vector3 kamikazePos = GET_ENTITY_COORDS(ped, false);
+			Vector3 kamikazePos = GET_ENTITY_COORDS(kamikaze, false);
 
 			ADD_EXPLOSION(kamikazePos.x, kamikazePos.y, kamikazePos.z, 4, 9999.f, true, false, 1.f, false);
 
-			SET_ENTITY_HEALTH(ped, 0, false);
-			SET_ENTITY_MAX_HEALTH(ped, 0);
+			SET_ENTITY_HEALTH(kamikaze, 0, false);
+			SET_ENTITY_MAX_HEALTH(kamikaze, 0);
 		}
 	}
 }
 
 static RegisterEffect registerEffect(EFFECT_PEDS_SERIOUS_KAMIKAZES, OnStart, OnStop, OnTick, EffectInfo
 	{
-		.Name = "0 Serious Kamikazes",
+		.Name = "Serious Kamikazes",
 		.Id = "peds_seriouskamikazes",
 		.IsTimed = true,
 		.IsShortDuration = true
