d67ee5a3588d2ebafef7887d60be2d2ee1ff1b09
petter.ohlen@gmail.com <petter.ohlen@gmail.com>
2021-09-05 12:53:40 +0200

    Added effect 'Spin World'

---
+++ ChaosMod/Effects/db/Misc/MiscSpinningProps.cpp
@@ -0,0 +1,22 @@
+/*
+	Effect by Gorakh
+*/
+
+#include <stdafx.h>
+
+static void OnTick()
+{
+	for (Entity prop : GetAllProps())
+	{
+		Vector3 rotation = GET_ENTITY_ROTATION(prop, 2);
+		SET_ENTITY_ROTATION(prop, rotation.x, rotation.y, rotation.z + 5.f, 2, true);
+	}
+}
+
+static RegisterEffect registerEffect(EFFECT_SPINNING_PROPS, nullptr, nullptr, OnTick, EffectInfo
+	{
+		.Name = "Spin World",
+		.Id = "misc_spinning_props",
+		.IsTimed = true
+	}
+);
