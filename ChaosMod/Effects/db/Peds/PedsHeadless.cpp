760107d3d1f7bc0068dc85ec668aaf88db361f47
petter.ohlen@gmail.com <petter.ohlen@gmail.com>
2021-09-10 06:56:09 +0200

    Added effect 'Mannequins'

---
+++ ChaosMod/Effects/db/Peds/PedsHeadless.cpp
@@ -0,0 +1,21 @@
+/*
+	Effect by Gorakh
+*/
+
+#include <stdafx.h>
+
+static void OnTick()
+{
+	for (Ped ped : GetAllPeds())
+	{
+		SET_PED_RESET_FLAG(ped, 166, true);
+	}
+}
+
+static RegisterEffect registerEffect(EFFECT_PEDS_HEADLESS, nullptr, nullptr, OnTick, EffectInfo
+	{
+		.Name = "Mannequins",
+		.Id = "peds_headless",
+		.IsTimed = true
+	}
+);
