79824b7d44879d398eda5ba411f2e48a09853b3f
petter.ohlen@gmail.com <petter.ohlen@gmail.com>
2021-09-29 17:48:18 +0200

    Make PedsPropHunt use std::map instead of vector to keep track of peds and objects. Make effect use the attach natives instead of setting position every tick. Apply offset to align the prop's center with the ped's center

--- ChaosMod/Effects/db/Peds/PedsPropHunt.cpp
+++ ChaosMod/Effects/db/Peds/PedsPropHunt.cpp
@@ -4,22 +4,25 @@
 
 #include <stdafx.h>
 
-struct PropPed
-{
-	Ped ped;
-	Object prop;
-};
-std::vector<PropPed> propPeds;
-
+std::map<Ped, Object> pedPropsMap;
 std::vector<Hash> availablePropModels;
+
+static Vector3 GetPropOffset(Hash propModel)
+{
+	Vector3 propMin, propMax;
+	GET_MODEL_DIMENSIONS(propModel, &propMin, &propMax);
+
+	// Lines up prop model center with ped model center
+	return (((propMax - propMin) / 2.f) + propMin) * -1.f;
+}
+
 static void OnTick()
 {
-	static int lastTick = 0;
+	static int lastModelsUpdateTick = 0;
 	int currentTick = GET_GAME_TIMER();
-
-	if (currentTick - lastTick > 1000)
+	if (currentTick - lastModelsUpdateTick > 1000 || availablePropModels.empty())
 	{
-		lastTick = GET_GAME_TIMER();
+		lastModelsUpdateTick = currentTick;
 
 		availablePropModels.clear();
 
@@ -39,102 +42,119 @@ static void OnTick()
 		}
 	}
 
-	if (availablePropModels.size() > 0)
+	if (!availablePropModels.empty())
 	{
+		int count = 10;
 		for (Ped ped : GetAllPeds())
 		{
-			if (!IS_PED_A_PLAYER(ped))
+			if (DOES_ENTITY_EXIST(ped) && !IS_PED_A_PLAYER(ped) && pedPropsMap.count(ped) == 0)
 			{
-				bool alreadyHasProp = false;
-				for (PropPed existingPropPed : propPeds)
+				// Rather than using a predefined list of prop hashes, get a random existing prop and use that as the model
+				Hash propModel = availablePropModels[g_Random.GetRandomInt(0, availablePropModels.size() - 1)];
+				LoadModel(propModel);
+
+				Object prop = CREATE_OBJECT(propModel, 0.f, 0.f, 0.f, true, false, false);
+
+				SET_ENTITY_COLLISION(prop, false, false);
+				SET_ENTITY_COMPLETELY_DISABLE_COLLISION(prop, false, false);
+
+				SET_ENTITY_VISIBLE(ped, false, 0);
+
+				Vector3 offset = GetPropOffset(propModel);
+				ATTACH_ENTITY_TO_ENTITY(prop, ped, GET_PED_BONE_INDEX(ped, 0x0), offset.x, offset.y, offset.z, 0.f, 0.f, 0.f, false, false, false, false, 0, true);
+				SET_ENTITY_VISIBLE(prop, true, 0);
+
+				pedPropsMap[ped] = prop;
+
+				if (--count == 0)
 				{
-					if (existingPropPed.ped == ped)
-					{
-						alreadyHasProp = DOES_ENTITY_EXIST(existingPropPed.prop);
-						break;
-					}
-				}
-
-				if (!alreadyHasProp)
-				{
-					// Rather than using a predefined list of prop hashes, get a random existing prop and use that as the model
-					Hash propModel = availablePropModels[g_Random.GetRandomInt(0, availablePropModels.size() - 1)];
-					LoadModel(propModel);
-
-					Object pedProp = CREATE_OBJECT(propModel, 0.f, 0.f, 0.f, true, false, false);
-
-					SET_ENTITY_COLLISION(pedProp, false, false);
-					SET_ENTITY_COMPLETELY_DISABLE_COLLISION(pedProp, false, false);
-
-					SET_ENTITY_VISIBLE(ped, false, 0);
-
-					PropPed propPed = { ped, pedProp };
-					propPeds.push_back(propPed);
+					count = 10;
+					WAIT(0);
 				}
 			}
 		}
 	}
 
-	for (std::vector<PropPed>::iterator it = propPeds.begin(); it != propPeds.end(); )
+	static int lastPropPedsCheckTick = 0;
+	currentTick = GET_GAME_TIMER();
+	if (currentTick - lastPropPedsCheckTick > 500)
 	{
-		PropPed propPed = *it;
-		if (DOES_ENTITY_EXIST(propPed.ped))
-		{
-			if (IS_PED_A_PLAYER(propPed.ped))
-			{
-				SET_ENTITY_VISIBLE(propPed.ped, true, 0);
+		lastPropPedsCheckTick = currentTick;
 
-				if (DOES_ENTITY_EXIST(propPed.prop))
+		int count = 20;
+		for (auto it = pedPropsMap.cbegin(); it != pedPropsMap.cend();)
+		{
+			Ped ped = it->first;
+			Object prop = it->second;
+			if (!DOES_ENTITY_EXIST(ped))
+			{
+				if (DOES_ENTITY_EXIST(prop))
 				{
-					DELETE_OBJECT(&propPed.prop);
+					DELETE_OBJECT(&prop);
 				}
 
-				it = propPeds.erase(it);
-				continue;
+				it = pedPropsMap.erase(it);
 			}
-
-			SET_ENTITY_VISIBLE(propPed.ped, false, 0);
-
-			if (DOES_ENTITY_EXIST(propPed.prop))
+			else if (IS_PED_A_PLAYER(ped)) // If player has a prop attached to them (this can happen after switching characters)
 			{
-				// Get bone coords instead of entity coords to make prop line up with model while ped is in a vehicle
-				Vector3 pedCoords = GET_PED_BONE_COORDS(propPed.ped, 0x0, 0.f, 0.f, 0.f);
+				SET_ENTITY_VISIBLE(ped, true, 0);
 
-				// For some reason ATTACH_ENTITY_TO_ENTITY wasn't working, so we set the position and rotation every tick instead
-				SET_ENTITY_COORDS(propPed.prop, pedCoords.x, pedCoords.y, pedCoords.z, false, false, false, false);
-				SET_ENTITY_ROTATION(propPed.prop, GET_ENTITY_PITCH(propPed.ped), GET_ENTITY_ROLL(propPed.ped), GET_ENTITY_HEADING(propPed.ped), 0, true);
+				if (DOES_ENTITY_EXIST(prop))
+				{
+					DELETE_OBJECT(&prop);
+				}
+
+				it = pedPropsMap.erase(it);
 			}
-
-			it++;
-		}
-		else
-		{
-			if (DOES_ENTITY_EXIST(propPed.prop))
+			else if (!DOES_ENTITY_EXIST(prop)) // The ped exists and is not a player, but there is no prop
 			{
-				DELETE_OBJECT(&propPed.prop);
+				SET_ENTITY_VISIBLE(ped, true, 0);
+				it = pedPropsMap.erase(it);
+			}
+			else // Both the ped and prop exist, and the ped is not the player
+			{
+				// Ensure ped is invisible, prop is attached to the ped, and the prop is visible
+				SET_ENTITY_VISIBLE(ped, false, 0);
+
+				if (!IS_ENTITY_ATTACHED_TO_ENTITY(prop, ped))
+				{
+					Vector3 offset = GetPropOffset(GET_ENTITY_MODEL(prop));
+					ATTACH_ENTITY_TO_ENTITY(prop, ped, GET_PED_BONE_INDEX(ped, 0x0), offset.x, offset.y, offset.z, 0.f, 0.f, 0.f, false, false, false, false, 0, true);
+				}
+
+				SET_ENTITY_VISIBLE(prop, true, 0);
+
+				it++;
 			}
 
-			it = propPeds.erase(it);
+			if (--count == 0)
+			{
+				count = 20;
+				WAIT(0);
+			}
 		}
 	}
 }
 
 static void OnStop()
 {
-	for (PropPed propPed : propPeds)
+	for (auto& it : pedPropsMap)
 	{
-		if (DOES_ENTITY_EXIST(propPed.ped))
+		Ped ped = it.first;
+		Object prop = it.second;
+
+		if (DOES_ENTITY_EXIST(ped))
 		{
-			SET_ENTITY_VISIBLE(propPed.ped, true, 0);
+			SET_ENTITY_VISIBLE(ped, true, 0);
 		}
 
-		if (DOES_ENTITY_EXIST(propPed.prop))
+		if (DOES_ENTITY_EXIST(prop))
 		{
-			DELETE_OBJECT(&propPed.prop);
+			DELETE_OBJECT(&prop);
 		}
 	}
 
-	propPeds.clear();
+	pedPropsMap.clear();
 }
 
 static RegisterEffect registerEffect(EFFECT_PEDS_PROP_HUNT, nullptr, OnStop, OnTick, EffectInfo
