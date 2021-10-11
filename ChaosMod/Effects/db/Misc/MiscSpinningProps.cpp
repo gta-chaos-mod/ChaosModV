375622e279585775e9b730d217f01796c372aa10
Petter Öhlén <petter.ohlen@gmail.com>
2021-10-11 19:32:21 +0200

    Make prop rotation speed during "Spinning Props" independent of framerate

--- ChaosMod/Effects/db/Misc/MiscSpinningProps.cpp
+++ ChaosMod/Effects/db/Misc/MiscSpinningProps.cpp
@@ -4,16 +4,30 @@
 
 #include <stdafx.h>
 
+// Degrees per millisecond, current value means 1.3 full rotations per second
+static constexpr float ROTATION_SPEED = (1.3f * 360.f) / 1000.f;
+
+static int lastTick = 0;
+
+static void OnStart()
+{
+	lastTick = GET_GAME_TIMER();
+}
+
 static void OnTick()
 {
+	int currentTick = GET_GAME_TIMER();
+	int tickDelta = currentTick - lastTick;
+	lastTick = currentTick;
+
 	for (Entity prop : GetAllProps())
 	{
 		Vector3 rotation = GET_ENTITY_ROTATION(prop, 2);
-		SET_ENTITY_ROTATION(prop, rotation.x, rotation.y, rotation.z + 5.f, 2, true);
+		SET_ENTITY_ROTATION(prop, rotation.x, rotation.y, rotation.z + (ROTATION_SPEED * tickDelta), 2, true);
 	}
 }
 
-static RegisterEffect registerEffect(EFFECT_SPINNING_PROPS, nullptr, nullptr, OnTick, EffectInfo
+static RegisterEffect registerEffect(EFFECT_SPINNING_PROPS, OnStart, nullptr, OnTick, EffectInfo
 	{
 		.Name = "Spinning Props",
 		.Id = "misc_spinning_props",
