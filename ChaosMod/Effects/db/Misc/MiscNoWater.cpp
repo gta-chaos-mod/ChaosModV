e550aafb5e3cf1744b4cdfa4afba586f6fb095b2
MoneyWasted <45946092+MoneyWasted@users.noreply.github.com>
2022-03-19 20:23:19 -0400

    Storing Water Height Values
    This is done because not all WaterQuads are at Z-Level 0. For example behind Franklins house in Vinewood Hills the Z-Level for the reservoir is something like 140

--- ChaosMod/Effects/db/Misc/MiscNoWater.cpp
+++ ChaosMod/Effects/db/Misc/MiscNoWater.cpp
@@ -8,12 +8,15 @@
 uint64_t WaterPattern = Memory::FindPattern("? 6B C9 1C ? 03 0D ? ? ? ? 66 ? 03 C5 66 89 05 ? ? ? ?").Addr();
 uint64_t WaterPointer;
 
+std::vector<float> WaterHeights;
+
 static void OnStart()
 {
 	WaterPointer = *(DWORD64*)((WaterPattern + *(DWORD*)(WaterPattern + 0x7)) + 0xB);
 	for (int i = 0; i < 821; i++) // 821 = Max Water Items
 	{
 		uint64_t Water = WaterPointer + (i * 0x1C);
+		WaterHeights.push_back(*(float*)(Water + 0x14));
 		*(float*)(Water + 0x14) = -1000.0f; // Remove Water
 	}
 }
@@ -23,7 +26,7 @@ static void OnStop()
 	for (int i = 0; i < 821; i++) // 821 = Max Water Items
 	{
 		uint64_t Water = WaterPointer + (i * 0x1C);
-		*(float*)(Water + 0x14) = 0.0f; // Restore Water
+		*(float*)(Water + 0x14) = WaterHeights.at(i); // Restore Water
 	}
 }
 
