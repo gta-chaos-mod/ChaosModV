9c294849d4f62b3c380bfb79cebb94afde68f132
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-03-27 16:37:57 -0400

    Made changes to effect
    Removed the OnTuck function, removed unused variable

--- ChaosMod/Effects/db/Misc/MiscGoToJail.cpp
+++ ChaosMod/Effects/db/Misc/MiscGoToJail.cpp
@@ -14,16 +14,13 @@ static void OnStart()
 
 	Vector3 policeStation = {495, -1020, 27};
 
-	//Whether to lock the player in the police car until it reaches its destination
-	bool lockPlayerin = false;
-
 	//Possible peds, (male or female)
 	std::vector<Hash> policePeds = { 0x15F8700D,  0x5E3DA4A4 };
 
 	float heading = GET_ENTITY_HEADING(playerPed);
 	Vector3 pos = GET_ENTITY_COORDS(playerPed, false);
 
-	//Get a random ped
+	//Get a random police ped
 	Hash randomPed = policePeds[g_Random.GetRandomInt(0, 1)];
 
 	//Create the police car
@@ -41,26 +38,10 @@ static void OnStart()
 	SET_PED_KEEP_TASK(policePed, 1);
 }
 
-static void OnTick()
-{
-	if (!GET_IS_TASK_ACTIVE(policePed, 157))
-	{
-		SET_VEHICLE_SIREN(veh, 0);
-		TASK_LEAVE_ANY_VEHICLE(policePed, 0 ,0);
-		SET_ENTITY_AS_NO_LONGER_NEEDED(&veh);
-		SET_ENTITY_AS_NO_LONGER_NEEDED(&policePed);
-	}
-	else if (GET_VEHICLE_PED_IS_IN(PLAYER_PED_ID(), true) != veh)
-	{
-		SET_VEHICLE_SIREN(veh, 0);
-		SET_ENTITY_AS_NO_LONGER_NEEDED(&veh);
-		SET_ENTITY_AS_NO_LONGER_NEEDED(&policePed);
-	}
-}
 
 
 // Any of these functions can be omitted and either replaced with a `nullptr` or completely left out (default parameter values) in the `RegisterEffect` declaration
-static RegisterEffect registerEffect(EFFECT_MISC_GO_TO_JAIL, OnStart, nullptr, OnTick, EffectInfo
+static RegisterEffect registerEffect(EFFECT_MISC_GO_TO_JAIL, OnStart, nullptr, nullptr, EffectInfo
 	{
 		// These are always required, you may have to add more designators depending on your effect
 		.Name = "Go Straight To Jail",
