cf83eabc21bfc3958fae98d98eb9c797c42a2c38
OnlyRealNubs <rywoodsworth@gmail.com>
2022-03-29 12:16:14 -0400

    Added effect

--- ChaosMod/Effects/db/Player/PlayerStun.cpp
+++ ChaosMod/Effects/db/Player/PlayerStun.cpp
@@ -6,12 +6,10 @@
 
 static void OnStart()
 {
-	Vector3 pos1 = GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(PLAYER_PED_ID(), 0, 1, 0);
-	Vector3 pos2 = GET_ENTITY_COORDS(PLAYER_PED_ID(), 1);
+	Vector3 pos2 = GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(PLAYER_PED_ID(), 0, 1, 0);
+	Vector3 pos1 = GET_ENTITY_COORDS(PLAYER_PED_ID(), 1);
 	
-	Ped owner = GET_RANDOM_PED_AT_COORD(pos1.x, pos1.y, pos1.z, 50, 50, 50, -1);
-
-	SHOOT_SINGLE_BULLET_BETWEEN_COORDS(pos1.x, pos1.y, pos1.z, pos2.x, pos2.y, pos2.z, 0, 0, 0x3656C8C1, owner, 1, 0, 100);
+	SHOOT_SINGLE_BULLET_BETWEEN_COORDS(pos1.x, pos1.y, pos1.z, pos2.x, pos2.y, pos2.z, 0, 0, 0x3656C8C1, PLAYER_PED_ID(), 1, 0, 100);
 	
 }
 
