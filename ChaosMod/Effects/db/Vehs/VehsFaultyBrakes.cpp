aa78fe708cb692942a8f70034468933b3be9c7e7
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-03-28 20:04:43 -0400

    Adjusted for Code Review

--- ChaosMod/Effects/db/Vehs/VehsFaultyBrakes.cpp
+++ ChaosMod/Effects/db/Vehs/VehsFaultyBrakes.cpp
@@ -1,11 +1,5 @@
 #include <stdafx.h>
 
-static void Brake(Vehicle veh)
-{
-	SET_VEHICLE_BRAKE(veh, 1);
-	SET_VEHICLE_HANDBRAKE(veh, 1);
-}
-
 static void OnTick()
 {
 	static DWORD64 lastTick = 0;
@@ -21,24 +15,16 @@ static void OnTick()
 			Vehicle veh = GET_VEHICLE_PED_IS_IN(playerPed, 0);
 			int rng_1 = g_Random.GetRandomInt(0, 100);
 			if (rng_1 > 40 && rng_1 < 60)
-			{
-				Brake(veh);
+			{	
+				SET_VEHICLE_BRAKE(veh, 1);
+				SET_VEHICLE_HANDBRAKE(veh, 1);
 			}
 		}
 	}
 }
 
-static void OnStart()
-{
-	return;
-}
 
-static void OnStop()
-{
-	return;
-}
-
-static RegisterEffect registerEffectBrakes(EFFECT_VEH_FAULTY_BRAKES, OnStart, OnStop, OnTick, EffectInfo
+static RegisterEffect registerEffectBrakes(EFFECT_VEH_FAULTY_BRAKES, nullptr, nullptr, OnTick, EffectInfo
 	{
 		.Name = "Faulty Brakes",
 		.Id = "vehs_faulty_brakes",
