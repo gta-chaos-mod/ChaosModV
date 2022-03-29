2077b92119cb1bc59f9480f898cb56d607b59eb9
OnlyRealNubs <rywoodsworth@gmail.com>
2022-03-29 09:21:38 -0400

    Added effect code

---
+++ ChaosMod/PlayerStun.cpp
@@ -0,0 +1,23 @@
+/*
+	Effect by OnlyRealNubs
+*/
+
+#include <stdafx.h>
+
+static void OnStart()
+{
+	Vector3 pos2 = GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(PLAYER_PED_ID(), 0, 1, 0);
+	Vector3 pos1 = GET_ENTITY_COORDS(PLAYER_PED_ID(), 1);
+	
+	SHOOT_SINGLE_BULLET_BETWEEN_COORDS(pos1.x, pos1.y, pos1.z, pos2.x, pos2.y, pos2.z, 0, 0, 0x3656C8C1, PLAYER_PED_ID(), 1, 0, 100);
+	
+}
+
+// Any of these functions can be omitted and either replaced with a `nullptr` or completely left out (default parameter values) in the `RegisterEffect` declaration
+static RegisterEffect registerEffect(EFFECT_PLAYER_STUN, OnStart, nullptr, nullptr, EffectInfo
+	{
+		// These are always required, you may have to add more designators depending on your effect
+		.Name = "That's Stunning",
+		.Id = "player_stun"
+	}
+);
