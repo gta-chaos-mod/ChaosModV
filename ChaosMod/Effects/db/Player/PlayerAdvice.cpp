9251e7f40b10e76761245628720b82fcdc11af03
OnlyRealNubs <rywoodsworth@gmail.com>
2022-03-31 16:24:37 -0400

    Update PlayerAdvice.cpp

--- ChaosMod/Effects/db/Player/PlayerAdvice.cpp
+++ ChaosMod/Effects/db/Player/PlayerAdvice.cpp
@@ -1,4 +1,4 @@
-/*
+﻿/*
 	Effect by OnlyRealNubs
 */
 
@@ -8,7 +8,7 @@ std::vector<std::string> facts = {
 	//For a list of things you can put here: https://pastebin.com/nqNYWMSB
 	"~INPUT_ATTACK~ Click To Shoot",
 	"If You Don't Take Damage You Won't Die",
-	"Move with ~INPUT_MOVE_UP~~INPUT_MOVE_LEFT~~INPUT_MOVE_DOWN~~INPUT_MOVE_RIGHT~",
+	"Move with ~INPUTGROUP_VEH_MOVE_ALL~",
 	"If You Die, You're Dead",
 	"Bullets Hurt",
 	"If You Don't Mission Fail That Would Be Good",
@@ -17,19 +17,19 @@ std::vector<std::string> facts = {
 	"Use The Controls To Play",
 	"This Is Text",
 	"If You Sprint, You Move Faster",
-	"~r~Th~o~is ~y~Is ~g~Rain~b~bow ~p~Text",
-	"This Game Was Made By Rockstar ~|~",
+	"~r~This ~o~Is ~y~Rainbow ~g~Text; ~b~Wow ~p~So ~HUD_COLOUR_PINK~Cool",
+	"This Game Was Made By Rockstar ~∑~",
 	"To Fly In A Car, It Has To Have Wings",
 	"This Blip ~BLIP_SIMEON_FAMILY~ Is Called \"Simeon Family\" In The Game Code",
 	"You Need To Chill",
-	"Only $5.99 a Month",
+	"Only $5.99 A Month",
 	"This Mod Is Great",
 	"This Mod Was Written In C++",
-	"~b~I'm Blue",
+	//"~b~I'm Blue", Seems to crash the game?
 	"~l~This Text Is Black",
 	"You Won A Free iFruit!",
 	"Will Sniff (Will Smith)",
-	"You Weren't So Bad You Would Be Good",
+	"If You Weren't So Bad You Would Be Good",
 	"Welcome To The New Story Mode DLC!~n~~n~Oh Wait.. Never Mind.",
 	"GTA 6 Confirmed!?!?!?!111??!11",
 	".",
@@ -38,7 +38,9 @@ std::vector<std::string> facts = {
 std::string msg;
 int scaleform = 0;
 
-static void _GetMessage(int rng) //Without underscore, it's defined somewhere idek.
+int debugIndex = 0;
+
+static void _GetMessage(int rng) //Without underscore, it's defined somewhere idek. I think it might be Microsoft
 {
 	msg = facts.at(rng);
 }
@@ -47,14 +49,14 @@ static void OnStart()
 {
 	_GetMessage(g_Random.GetRandomInt(0, facts.size()));
 	DWORD lastTick = GetTickCount64();
-	DWORD time = 3000;
-	SET_TIME_SCALE(0.2f);
+	DWORD time = 12000;
 	while (GetTickCount64() - lastTick < time)
 	{
 		WAIT(0);
-		DISPLAY_HELP_TEXT_THIS_FRAME(msg.c_str(), 0);
+		HUD::BEGIN_TEXT_COMMAND_DISPLAY_HELP("STRING");
+		HUD::ADD_TEXT_COMPONENT_SUBSTRING_PLAYER_NAME(msg.c_str());
+		HUD::END_TEXT_COMMAND_DISPLAY_HELP(0, 0, 1, -1);
 	}
-	SET_TIME_SCALE(1.f);
 }
 
 static RegisterEffect registerEffect(EFFECT_PLAYER_FACTS, OnStart, nullptr, nullptr, EffectInfo
