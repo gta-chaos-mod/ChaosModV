66f93aaeec643054a8ea8a99186d68a1981e6a31
OnlyRealNubs <rywoodsworth@gmail.com>
2022-03-31 14:11:28 -0400

    Changed to Help Text instead of Scaleform

--- ChaosMod/Effects/db/Player/PlayerAdvice.cpp
+++ ChaosMod/Effects/db/Player/PlayerAdvice.cpp
@@ -21,18 +21,6 @@ std::vector<std::string> facts = {
 std::string msg;
 int scaleform = 0;
 
-static void RequestShit()
-{
-	scaleform = REQUEST_SCALEFORM_MOVIE("MP_BIG_MESSAGE_FREEMODE");
-	while (!HAS_SCALEFORM_MOVIE_LOADED(scaleform))
-	{
-		WAIT(0);
-	}
-	BEGIN_SCALEFORM_MOVIE_METHOD(scaleform, "SHOW_SHARD_RANKUP_MP_MESSAGE");
-	SCALEFORM_MOVIE_METHOD_ADD_PARAM_PLAYER_NAME_STRING(msg.c_str());
-	END_SCALEFORM_MOVIE_METHOD();
-}
-
 static void _GetMessage(int rng) //Without underscore, it's defined somewhere idek.
 {
 	msg = facts.at(rng);
@@ -41,14 +29,13 @@ static void _GetMessage(int rng) //Without underscore, it's defined somewhere id
 static void OnStart()
 {
 	_GetMessage(g_Random.GetRandomInt(0, facts.size()));
-	RequestShit();
 	DWORD lastTick = GetTickCount64();
 	DWORD time = 3000;
 	SET_TIME_SCALE(0.2f);
 	while (GetTickCount64() - lastTick < time)
 	{
 		WAIT(0);
-		DRAW_SCALEFORM_MOVIE_FULLSCREEN(scaleform, 255, 255, 255, 255, 0);
+		DISPLAY_HELP_TEXT_THIS_FRAME(msg.c_str(), 0);
 	}
 	SET_TIME_SCALE(1.f);
 }
