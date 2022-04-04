42f94dda0f763d0a2e59482f1ef434897aab15c8
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-04-04 14:26:34 -0400

    Update MiscSpawnSlot.cpp

--- ChaosMod/Effects/db/Misc/MiscSpawnSlot.cpp
+++ ChaosMod/Effects/db/Misc/MiscSpawnSlot.cpp
@@ -13,10 +13,9 @@ static void OnStart()
 		WAIT(0);
 	}
 	Ped playerPed = PLAYER_PED_ID();
-	Vector3 pos = GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(playerPed, 0, 3, 0.3);
+	Vector3 pos = GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(playerPed, 0, 3, 0);
 	Vector3 rot = { 0, 0, -GET_ENTITY_HEADING(playerPed) };
 	int obj = CREATE_OBJECT(slotModel, pos.x, pos.y, pos.z, 1, 1, 1);
-	SET_ENTITY_ROTATION(obj, rot.x, rot.y, rot.z, 2, 1);
 }
 
 static RegisterEffect registerEffect(EFFECT_MISC_SPAWN_SLOT, OnStart, nullptr, nullptr, EffectInfo
