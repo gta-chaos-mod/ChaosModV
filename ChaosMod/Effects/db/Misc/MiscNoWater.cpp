84d087008fd072bffde45f773e462d52b6921e9a
MoneyWasted <45946092+MoneyWasted@users.noreply.github.com>
2022-04-11 17:00:55 -0400

    Improvements and Fixes
    Added a check for if the pattern is valid.
    Made the code use Pongo's Handle code.
    Added the class that handles the quad data. Maybe new effects can be made with modifying other water related data?

--- ChaosMod/Effects/db/Misc/MiscNoWater.cpp
+++ ChaosMod/Effects/db/Misc/MiscNoWater.cpp
@@ -5,19 +5,39 @@
 
 #include <stdafx.h>
 
-uint64_t WaterPattern = Memory::FindPattern("? 6B C9 1C ? 03 0D ? ? ? ? 66 ? 03 C5 66 89 05 ? ? ? ?").Addr();
-uint64_t WaterPointer;
+struct CWaterQuad
+{
+	short MinX;   // 0x0
+	short MinY;   // 0x2
+	short MaxX;   // 0x4
+	short MaxY;   // 0x6
+	uint Color;   // 0x8
+	char unk1[4]; // 0xC
+	char unk2[4]; // 0x10
+	float Z;      // 0x14
+	uint Flags;   // 0x18
+};
+static_assert(sizeof(CWaterQuad) == 0x1C);
 
-std::vector<float> WaterHeights;
+static CWaterQuad* WaterQuads;
+static std::vector<float> WaterHeights;
+
+CWaterQuad* GetWaterQuads()
+{
+	Handle handle = Memory::FindPattern("? 6B C9 1C ? 03 0D ? ? ? ? 66 ? 03 C5 66 89 05 ? ? ? ?");
+	if (handle.IsValid())
+	{
+		return *handle.At(6).Into().Get<CWaterQuad*>();
+	}
+}
 
 static void OnStart()
 {
-	WaterPointer = *(DWORD64*)((WaterPattern + *(DWORD*)(WaterPattern + 0x7)) + 0xB);
+	WaterQuads = GetWaterQuads();
 	for (int i = 0; i < 821; i++) // 821 = Max Water Items
 	{
-		uint64_t Water = WaterPointer + (i * 0x1C);
-		WaterHeights.push_back(*(float*)(Water + 0x14));
-		*(float*)(Water + 0x14) = -1000.0f; // Remove Water
+		WaterHeights.push_back(WaterQuads[i].Z); // Save Water Heights
+		WaterQuads[i].Z = -1000.0f; // Remove Water
 	}
 }
 
@@ -25,10 +45,9 @@ static void OnStop()
 {
 	for (int i = 0; i < 821; i++) // 821 = Max Water Items
 	{
-		uint64_t Water = WaterPointer + (i * 0x1C);
-		*(float*)(Water + 0x14) = WaterHeights.at(i); // Restore Water
+		WaterQuads[i].Z = WaterHeights.at(i); // Restore Water
 	}
-	WaterHeights.clear();
+	WaterHeights.clear(); // Clear Storage Vector
 }
 
 static RegisterEffect registerEffect(EFFECT_MISC_WATER, OnStart, OnStop, nullptr, EffectInfo
