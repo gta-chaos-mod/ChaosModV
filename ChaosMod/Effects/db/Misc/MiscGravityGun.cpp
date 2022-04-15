02aeae397f55a7056225a0684b0ef3e741310f32
OnlyRealNubs <rywoodsworth@gmail.com>
2022-04-15 12:49:49 -0400

    Made improvements.
    You can now grab AI vehicles. You can now hit the shoot button to send an object flying through the air. Changed to RayCasting from the gameplay cam to get entities as they wouldn't get picked up properly sometimes with the old method. Switched to detecting input instead of getting the players aiming state. You no longer have to be constantly aiming to pickup an object. You press it, it scans, if there is an object it will pick it up for as long as you have he aim button down. Thanks to pongo for helping me with some errors. And MoneyWasted for the RayCasting.

--- ChaosMod/Effects/db/Misc/MiscGravityGun.cpp
+++ ChaosMod/Effects/db/Misc/MiscGravityGun.cpp
@@ -1,8 +1,14 @@
 /*
-	Effect By OnlyRealNubs
+	Effect By OnlyRealNubs, special thanks to MoneyWasted & pongo1231 for the help
 */
 
 #include "stdafx.h"
+#include "Memory/Physics.h"
+#include "Util/Camera.h"
+using namespace Memory;
+
+float ms_fForceX = 50;
+float ms_fGrabDist = 400;
 
 static enum EentityType : int
 {
@@ -15,64 +21,62 @@ static enum EentityType : int
 static Entity selectedObject = 0;
 static EentityType entityType = EentityType::ENTITY_NONE;
 
-static Vector3 entityOffset = { 2.5 , 0, 0.5 };
-
-std::vector<Vector3> CalcDat(Ped player, Vector3 offset, float headingOffset)
+static void CleanUp()
 {
-	std::vector<Vector3> arr;
-	static Vector3 rot = GET_ENTITY_ROTATION(player, 2);
-	rot.z += headingOffset;
-	arr[0] = GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(player, offset.x, offset.y, offset.x);
-	arr[1] = rot;
-	return arr;
+	DETACH_ENTITY(selectedObject, 0, 1);
+	selectedObject = 0;
+	entityType = ENTITY_NONE;
+}
+
+bool GetAimedAtEntity(Entity* e)
+{
+	Vector3 coords, normal;
+	BOOL hit;
+	Entity out;
+	int handle = Util::RayCastGameplayCam(ms_fGrabDist, &hit, &coords, &normal, &out);
+	*e = out;
+	return (bool)hit;
 }
 
 static void OnTick()
 {
-
 	static const Player player = PLAYER_ID();
 	static const Ped playerPed = PLAYER_PED_ID();
-	if (IS_PLAYER_FREE_AIMING(player))
+
+	if (DOES_ENTITY_EXIST(selectedObject))
 	{
-		if (DOES_ENTITY_EXIST(selectedObject))
+		if (IS_CONTROL_JUST_PRESSED(0, 24))
 		{
-			Vector3 rot = GET_ENTITY_ROTATION(selectedObject, 2);
-			SET_ENTITY_ROTATION(selectedObject, rot.x, 180, rot.z, 2, 1);
-		}
-		else
-		{
-			Entity tmp;
-			if (GET_ENTITY_PLAYER_IS_FREE_AIMING_AT(player, &tmp))
-			{
-				if (GET_ENTITY_TYPE(tmp) != EentityType::ENTITY_NONE)
-				{
-					Entity wep = GET_CURRENT_PED_WEAPON_ENTITY_INDEX(playerPed);
-					int bone = GET_ENTITY_BONE_INDEX_BY_NAME(wep, "Gun_Nuzzle");
-					selectedObject = tmp;
-					SET_ENTITY_HAS_GRAVITY(selectedObject, 0);
-					entityType = (EentityType)GET_ENTITY_TYPE(tmp);
-					BOOL IS_PED = entityType == 1 ? 1 : 0;
-					ATTACH_ENTITY_TO_ENTITY(tmp, wep, bone, 2.5, 0, 0.5, 0.f, 0.f, 0.f, 0, 0, 0, IS_PED, 2, 1);
-					return;
-				}
-			}
+			DETACH_ENTITY(selectedObject, 0, 1);
+			ApplyForceToEntityCenterOfMass(selectedObject, 1, ms_fForceX, 0, 0, 1, 1, 1, 0);
+			WAIT(100);
+			CleanUp();
 		}
 	}
-	else
+	else if (IS_CONTROL_JUST_PRESSED(0, 25))
 	{
-		DETACH_ENTITY(selectedObject, 0, 1);
-		SET_ENTITY_HAS_GRAVITY(selectedObject, 1);
-		selectedObject = 0;
-		entityType = EentityType::ENTITY_NONE;
+		Entity tmp;
+		if (GetAimedAtEntity(&tmp))
+		{
+			if (GET_ENTITY_TYPE(tmp) != EentityType::ENTITY_NONE)
+			{
+				Entity wep = GET_CURRENT_PED_WEAPON_ENTITY_INDEX(playerPed);
+				selectedObject = tmp;
+				entityType = (EentityType)GET_ENTITY_TYPE(tmp);
+				ATTACH_ENTITY_TO_ENTITY(tmp, wep, GET_ENTITY_BONE_INDEX_BY_NAME(wep, "Gun_Nuzzle"), 2.5, 0, 0.5, 0.f, 0.f, 0.f, 0, 0, 0, 1, 2, 1);
+				return;
+			}
+		} 
+	}
+	else if (IS_CONTROL_JUST_RELEASED(0, 25))
+	{
+		CleanUp();
 	}
 }
 
 static void OnStop()
 {
-	DETACH_ENTITY(selectedObject, 0, 1);
-	SET_ENTITY_HAS_GRAVITY(selectedObject, 1);
-	selectedObject = 0;
-	entityType = ENTITY_NONE;
+	CleanUp();
 }
 
 static RegisterEffect reg(EFFECT_MISC_GRAVITY_GUNS, nullptr, OnStop, OnTick, EffectInfo
