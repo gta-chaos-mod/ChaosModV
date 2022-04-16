a21ab69345b399cd9f06fab0e30f6cd01f63452d
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-04-16 10:16:49 -0400

    Renamed Because Of Last0xygene's OCD.

--- ChaosMod/Effects/db/Player/PlayerRandomHud.cpp
+++ ChaosMod/Effects/db/Player/PlayerRandomHud.cpp
@@ -4,18 +4,18 @@
 
 #include "stdafx.h"
 
-static int comp = 0;
+static int selectedComponent = 0;
 
-static std::vector<int> comps = { 1, 2, 3, 6, 7, 8, 9, 10, 14, 15, 16, 19, 20, 22 };
+static std::vector<int> availableComponents = { 1, 2, 3, 6, 7, 8, 9, 10, 14, 15, 16, 19, 20, 22 };
 
 static void OnStart()
 {
-	comp = comps[g_Random.GetRandomInt(0, comps.size() - 1)];
+	selectedComponent = comps[g_Random.GetRandomInt(0, availableComponents.size() - 1)];
 }
 
 static void OnTick()
 {
-	HIDE_HUD_COMPONENT_THIS_FRAME(comp);
+	HIDE_HUD_COMPONENT_THIS_FRAME(selectedComponent);
 }
 
 static RegisterEffect reg(EFFECT_PLAYER_DISABLE_RANDOM_HUD, OnStart, nullptr, OnTick, EffectInfo
