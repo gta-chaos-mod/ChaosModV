7e5ebe33447ce36466eaa88b12924956fcb3ee4d
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-04-16 16:28:20 -0400

    Update PlayerStun.cpp

--- ChaosMod/Effects/db/Player/PlayerStun.cpp
+++ ChaosMod/Effects/db/Player/PlayerStun.cpp
@@ -8,8 +8,10 @@ std::string effectName = "Dont_tazeme_bro";
 
 static void OnStart()
 {
-	Vector3 pos1 = GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(PLAYER_PED_ID(), 0, 1, 0);
-	Vector3 pos2 = GET_ENTITY_COORDS(PLAYER_PED_ID(), 1);
+	Ped plr = PLAYER_PED_ID();
+	
+	Vector3 pos1 = GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(plr, 0, 1, 0);
+	Vector3 pos2 = GET_ENTITY_COORDS(plr, 1);
 
 	SHOOT_SINGLE_BULLET_BETWEEN_COORDS(pos1.x, pos1.y, pos1.z, pos2.x, pos2.y, pos2.z, 0, 0, 0x3656C8C1, 0, 1, 0, 100);
 	
