4f163c49201ca161032e190fedd3bafda03d811b
OnlyRealNubs <rywoodsworth@gmail.com>
2022-04-16 19:07:06 -0400

    Update MiscFakeDisconnect.cpp

--- ChaosMod/Effects/db/Misc/MiscFakeDisconnect.cpp
+++ ChaosMod/Effects/db/Misc/MiscFakeDisconnect.cpp
@@ -6,10 +6,14 @@
 #include <windows.h>
 #include <mmsystem.h>
 
-
 static void OnStart()
 {
+	static int disableTime = g_Random.GetRandomInt(900, 3000);
+	Player player = PLAYER_ID();
 	PlaySound(TEXT("C:/Windows/Media/Windows Hardware Remove.wav"), NULL, SND_FILENAME | SND_ASYNC);
+	SET_PLAYER_CONTROL(player, 0, 1 << 8);
+	WAIT(disableTime);
+	SET_PLAYER_CONTROL(player, 1, 1 << 8);
 }
 
 static RegisterEffect registerEffect(EFFECT_MISC_FAKE_DISCONNECT, OnStart, EffectInfo
