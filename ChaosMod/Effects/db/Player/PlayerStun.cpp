59437191af4fdb9410d724fe90f13e67d514e51b
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-04-21 10:30:45 -0400

    Update PlayerStun.cpp

--- ChaosMod/Effects/db/Player/PlayerStun.cpp
+++ ChaosMod/Effects/db/Player/PlayerStun.cpp
@@ -13,7 +13,7 @@ static void OnStart()
 	Vector3 pos1 = GET_OFFSET_FROM_ENTITY_IN_WORLD_COORDS(plr, 0, 1, 0);
 	Vector3 pos2 = GET_ENTITY_COORDS(plr, 1);
 
-	SHOOT_SINGLE_BULLET_BETWEEN_COORDS(pos1.x, pos1.y, pos1.z, pos2.x, pos2.y, pos2.z, 0, 0, 0x3656C8C1, 0, 1, 0, 100);
+	SHOOT_SINGLE_BULLET_BETWEEN_COORDS(pos1.x, pos1.y, pos1.z, pos2.x, pos2.y, pos2.z, 0, 0, 0x3656C8C1, 0, 0, 1, 100);
 	
 	ANIMPOSTFX_PLAY(effectName.c_str(), 0, 0);
 	WAIT(4600);
