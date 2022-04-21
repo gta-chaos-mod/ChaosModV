41eabc68e4ee9ca3cb822dd7395ca37299705956
OnlyRealNubs <rywoodsworth@gmail.com>
2022-04-21 09:01:03 -0400

    Update PlayerFPS.cpp

--- ChaosMod/Effects/db/Player/PlayerFPS.cpp
+++ ChaosMod/Effects/db/Player/PlayerFPS.cpp
@@ -16,18 +16,19 @@ static void OnStart()
 
 static void OnTick()
 {
-	const std::string fps = std::to_string(GET_FRAME_COUNT() / GET_FRAME_TIME()) + " FPS";
+	float fps = 1 / GET_FRAME_TIME();
 	BEGIN_SCALEFORM_MOVIE_METHOD(scaleform, "SHOW_SHARD_RANKUP_MP_MESSAGE");
-	SCALEFORM_MOVIE_METHOD_ADD_PARAM_PLAYER_NAME_STRING(fps.c_str());
+	SCALEFORM_MOVIE_METHOD_ADD_PARAM_PLAYER_NAME_STRING(std::to_string((int)fps).append(" FPS").c_str());
 	END_SCALEFORM_MOVIE_METHOD();
 	DRAW_SCALEFORM_MOVIE_FULLSCREEN(scaleform, 255, 255, 255, 255, 0);
 }
 
-static RegisterEffect reg(EFFECT_PLAYER_FPS, nullptr, nullptr, OnTick, EffectInfo
+static RegisterEffect reg(EFFECT_PLAYER_FPS, OnStart, nullptr, OnTick, EffectInfo
 	{
 		.Name = "FPS Counter",
 		.Id = "player_fps",
 		.IsTimed = true,
-		.IsShortDuration = false
+		.IsShortDuration = false,
+		.IncompatibleWith = {EFFECT_VEH_SPEED_MINIMUM, EFFECT_PLAYER_SIMEONSAYS}
 	}
 );
