6054f3b93a8c1112ee40d7dbae0811bec13ab4e4
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-05-02 12:06:00 -0400

    Now plays cheering sound

--- ChaosMod/Effects/db/Player/PlayerBirthday.cpp
+++ ChaosMod/Effects/db/Player/PlayerBirthday.cpp
@@ -1,5 +1,5 @@
 /*
-	Effect By OnlyRealNubs, made in celebration of my birthday
+	Effect By OnlyRealNubs, made in celebration of my birthday, Last0xygen for pointing out improvements, MoneyWasted for audio
 */
 
 #include "stdafx.h"
@@ -9,13 +9,17 @@ std::string ptfxDict = "core";
 std::string ptfxName = "ent_dst_gen_gobstop";
 std::string pedModel = "s_m_y_clown_01";
 std::string cakeModel = "v_res_cakedome";
-std::string audDict = "DLC_TG_Running_Back_Sounds";
-std::string audName = "Cheers";
+std::string audBank = "DLC_BATTLE/BTL_CLUB_DJ_CALLOUT_CROWD_CHEER";
+std::string audDict = "dlc_btl_club_dj_callout_crowd_cheers_sounds";
+std::string audName = "dj_crowd_cheer";
 
-int soundId = 0;
+int soundId = -1;
 
-static void LoadModelsAndPtfx()
-{
+static void LoadAssets()
+{	
+	//load audio
+	REQUEST_SCRIPT_AUDIO_BANK(audBank.c_str(), 0, -1);
+
 	//load models
 	std::vector<std::string> modelsToLoad = {pedModel, cakeModel};
 	for (int i = 0; i < modelsToLoad.size(); i++)
@@ -23,7 +27,6 @@ static void LoadModelsAndPtfx()
 		REQUEST_MODEL(GET_HASH_KEY(modelsToLoad[i].c_str()));
 		while (!HAS_MODEL_LOADED(GET_HASH_KEY(modelsToLoad[i].c_str())))
 		{
-			
 			WAIT(0);
 		}
 	}
@@ -32,13 +35,15 @@ static void LoadModelsAndPtfx()
 	REQUEST_NAMED_PTFX_ASSET(ptfxDict.c_str());
 	while (!HAS_NAMED_PTFX_ASSET_LOADED(ptfxDict.c_str()))
 	{
-		
 		WAIT(0);
 	}
 }
 
-static void UnloadModelsAndPtfx()
+static void UnloadAssets()
 {
+	//unload audio
+	RELEASE_NAMED_SCRIPT_AUDIO_BANK(audBank.c_str());
+
 	//unload models
 	std::vector<std::string> modelsToLoad = {pedModel, cakeModel};
 	for (int i = 0; i < modelsToLoad.size(); i++)
@@ -53,19 +58,18 @@ static void UnloadModelsAndPtfx()
 
 static void OnStart()
 {
-	LoadModelsAndPtfx();
+	//Load assets
+	LoadAssets();
 
 	Ped playerPed = PLAYER_PED_ID();
 	
 	//Create Relationship
 	static const Hash playerGroup = GET_HASH_KEY("PLAYER");
-	static const Hash civGroup = GET_HASH_KEY("CIVMALE");
-	static const Hash femCivGroup = GET_HASH_KEY("CIVFEMALE");
 
 	Hash relationshipGroup;
 	ADD_RELATIONSHIP_GROUP("_BRTHDAY_CLOWN", &relationshipGroup);
-	SET_RELATIONSHIP_BETWEEN_GROUPS(0, relationshipGroup, GET_HASH_KEY("PLAYER"));
-	SET_RELATIONSHIP_BETWEEN_GROUPS(0, GET_HASH_KEY("PLAYER"), relationshipGroup);
+	SET_RELATIONSHIP_BETWEEN_GROUPS(0, relationshipGroup, playerGroup);
+	SET_RELATIONSHIP_BETWEEN_GROUPS(0, playerGroup, relationshipGroup);
 
 	//Check if the player is in a vehicle
 	Vehicle plrVeh = 0;
@@ -92,7 +96,11 @@ static void OnStart()
 	USE_PARTICLE_FX_ASSET(ptfxDict.c_str());
 	START_PARTICLE_FX_NON_LOOPED_ON_PED_BONE(ptfxName.c_str(), playerPed, 0.f, 0.f, 0.2f, 0.f, 0.f, 0.f, GET_PED_BONE_INDEX(playerPed, 0x796e), 1.5f, false, false, false);
 
-	UnloadModelsAndPtfx();
+	//Play sound
+	PLAY_SOUND_FROM_ENTITY(soundId, audName.c_str(), playerPed, audDict.c_str(), false, 0);
+
+	//Unload assets
+	UnloadAssets();
 }
 
 static RegisterEffect registerEffect(EFFECT_PLAYER_BIRTHDAY, OnStart, EffectInfo
