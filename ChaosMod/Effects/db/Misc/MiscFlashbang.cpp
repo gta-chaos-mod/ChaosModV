8856c207ab09897431af00bc1f85594116534344
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-05-04 07:53:12 -0400

    Updated for review

--- ChaosMod/Effects/db/Misc/MiscFlashbang.cpp
+++ ChaosMod/Effects/db/Misc/MiscFlashbang.cpp
@@ -1,5 +1,5 @@
 /*
-	Effect by <Your Name>
+	Effect by OnlyRealNubs
 */
 
 #include <stdafx.h>
@@ -7,20 +7,15 @@
 static void OnStart()
 {
 	DWORD ticksToDraw = GetTickCount() + 3000;
-	int screenx;
-	int screeny;
-	GET_SCREEN_RESOLUTION(&screenx, &screeny);
 	while (GetTickCount() < ticksToDraw)
 	{
 		WAIT(0);
-		DRAW_RECT(0,0, screenx, screeny, 255, 255, 255, 255, 0);
+		DRAW_RECT(0,0, 1.f, 1.f, 255, 255, 255, 255, 0);
 	}
 }
 
-// Any of these functions can be omitted and either replaced with a `nullptr` or completely left out (default parameter values) in the `RegisterEffect` declaration
 static RegisterEffect registerEffect(EFFECT_MISC_FLASHBANG, OnStart, nullptr, nullptr, EffectInfo
 	{
-		// These are always required, you may have to add more designators depending on your effect
 		.Name = "Flashbang",
 		.Id = "misc_flashbang"
 	}
