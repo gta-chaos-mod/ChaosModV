7f49b5f0b8a0f4a13d2b5e5de722ffdd603990a3
petter.ohlen@gmail.com <petter.ohlen@gmail.com>
2022-05-05 20:01:11 +0200

    Add effect "In The Spotlight"

--- ConfigApp/Effects.cs
+++ ConfigApp/Effects.cs
@@ -350,6 +350,7 @@ namespace ConfigApp
             EFFECT_MISC_WATER,
             EFFECT_VEHS_PROP_MODELS,
             EFFECT_VEHS_TINY,
+            EFFECT_MISC_SPOTLIGHT,
             _EFFECT_ENUM_MAX
         }
 
@@ -664,6 +665,7 @@ namespace ConfigApp
             {EffectType.EFFECT_MISC_WATER, new EffectInfo("Drought", EffectCategory.MISC, "misc_remove_water", true)},
             {EffectType.EFFECT_VEHS_PROP_MODELS, new EffectInfo("Prop Cars", EffectCategory.VEHICLE, "vehs_prop_models", true)},
             {EffectType.EFFECT_VEHS_TINY, new EffectInfo("Tiny Vehicles", EffectCategory.VEHICLE, "vehs_tiny", true)},
+            {EffectType.EFFECT_MISC_SPOTLIGHT, new EffectInfo("In The Spotlight", EffectCategory.MISC, "misc_spotlight", true)},
         };
     }
 }
