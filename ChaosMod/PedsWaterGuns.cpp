4d8d2ea3c977f8ad8039b330d1f7fc6b46de5ed9
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-05-07 17:13:49 -0400

    Update PedsWaterGuns.cpp

--- ChaosMod/PedsWaterGuns.cpp
+++ ChaosMod/PedsWaterGuns.cpp
@@ -1,43 +1,94 @@
 /*
-	Effect By OnlyRealNubs
+	Effect By OnlyRealNubs, Borrowed Last0xygen's code ;)
 */
 
 #include <stdafx.h>
 
-const char* particleDict = "core";
+ static struct ShooterInfo
+ {
+ 	float fullDuration = 0;
+ 	float durationSinceLastShot = 0;
+ 	int fxHandle = 0;
+ };
 
+ static int MAX_DURATION_BETWEEN_SHOTS = 10;
+ static int MAX_DURATION_ANIMATION = 150;
 
+ static std::map<Ped, ShooterInfo> animationHandleByPed;
 
-static void OnTick()
-{
-	static const char* particleName = "water_cannon_jet";
-	for (Ped ped : GetAllPeds())
-	{
-		if (IS_PED_SHOOTING(ped))
-		{
-			USE_PARTICLE_FX_ASSET(particleDict);
-			int wep = GET_CURRENT_PED_WEAPON_ENTITY_INDEX(ped);
-			SET_PARTICLE_FX_SHOOTOUT_BOAT(1);
-			int ptfx = START_PARTICLE_FX_LOOPED_ON_ENTITY(particleName, wep, 0.f, 0, -0.15f, 0, 0, -90.f, 1, 0, 0, 0);
-			WAIT(300);
-			STOP_PARTICLE_FX_LOOPED(ptfx, 0);
-		}
-	}
-}
+ static void OnStop()
+ {
+ 	REMOVE_NAMED_PTFX_ASSET("core");
+ }
 
-static void OnStart()
-{
-	REQUEST_NAMED_PTFX_ASSET(particleDict);
-	while (!HAS_NAMED_PTFX_ASSET_LOADED(particleDict))
-	{
-		WAIT(0);
-	}
-}
+ static void OnTick()
+ {
+ 	REQUEST_NAMED_PTFX_ASSET("core");
+ 	while (!HAS_NAMED_PTFX_ASSET_LOADED("core"))
+ 	{
+ 		WAIT(0);
+ 	}
+ 	std::vector<Ped> firingPeds;
 
-static void OnStop()
-{
-	REMOVE_NAMED_PTFX_ASSET(particleDict);
-}
+ 	for (Ped ped : GetAllPeds())
+ 	{
+ 		if (IS_PED_SHOOTING(ped))
+ 		{
+ 			Entity weapon = GET_SELECTED_PED_WEAPON(ped);
+ 			if (GET_WEAPON_DAMAGE_TYPE(weapon) == 3)
+ 			{
+ 				firingPeds.push_back(ped);
+ 			}
+ 		}
+ 	}
+
+ 	// Remove not needed peds and cancel animations
+ 	int delayRemovePeds = 25;
+ 	for (auto it = animationHandleByPed.cbegin(); it != animationHandleByPed.cend();)
+ 	{
+ 		ShooterInfo animationInfo = it->second;
+ 		if (!DOES_ENTITY_EXIST(it->first) || animationInfo.fxHandle <= 0 || it->second.fullDuration > MAX_DURATION_ANIMATION || ((!IS_PED_SHOOTING(it->first) && IS_PED_WEAPON_READY_TO_SHOOT(it->first)) && animationInfo.durationSinceLastShot > MAX_DURATION_BETWEEN_SHOTS))
+ 		{
+ 			STOP_PARTICLE_FX_LOOPED(animationInfo.fxHandle, false);
+ 			animationHandleByPed.erase(it++);
+ 		}
+ 		else
+ 		{
+ 			animationHandleByPed[it->first].fullDuration += 1.f;
+ 			animationHandleByPed[it->first].durationSinceLastShot += 1.f;
+ 			++it;
+ 		}
+ 		if (--delayRemovePeds == 0)
+ 		{
+ 			delayRemovePeds = 25;
+ 			WAIT(0);
+ 		}
+ 	}
+
+ 	// Check for needed animation starts
+ 	int delayAnimationStart = 25;
+ 	for (Ped ped : firingPeds)
+ 	{
+ 		if (animationHandleByPed.count(ped) < 1)
+ 		{
+ 			USE_PARTICLE_FX_ASSET("core");
+ 			Entity weapon = GET_CURRENT_PED_WEAPON_ENTITY_INDEX(ped);
+ 			int handle = START_PARTICLE_FX_LOOPED_ON_ENTITY("water_cannon_jet", weapon, 1, 0, 0, 90, 0, 90, 2, false, false, false);
+ 			ShooterInfo animInfo;
+ 			animInfo.fxHandle = handle;
+ 			animationHandleByPed[ped] = animInfo;
+ 		}
+ 		else
+ 		{
+ 			animationHandleByPed[ped].durationSinceLastShot = 0;
+ 		}
+ 		if (--delayAnimationStart == 0)
+ 		{
+ 			delayAnimationStart = 25;
+ 			WAIT(0);
+ 		}
+ 	}
+ }
 
 static RegisterEffect registerEffect(EFFECT_PEDS_WATER_GUNS, OnStart, OnStop, OnTick, EffectInfo
 	{
