088eb014957c14bffc031a4dadf9550c47792668
Reguas <64607261+Reguas@users.noreply.github.com>
2022-05-07 08:55:58 +0300

    Changed float -> int in native call

--- ChaosMod/Effects/db/Vehs/VehsWiggleWiggle.cpp
+++ ChaosMod/Effects/db/Vehs/VehsWiggleWiggle.cpp
@@ -10,14 +10,13 @@ static void OnTick()
 	static int lastTick = 0;
 	static bool flag = false;
 
-	int tickDelta = currentTick - lastTick;
 	if (currentTick - lastTick >= 1000)
 	{
 		for (Vehicle veh : GetAllVehs())
 		{
 			if (!IS_VEHICLE_SEAT_FREE(veh, -1, false))
 			{
-				TASK_VEHICLE_TEMP_ACTION(GET_PED_IN_VEHICLE_SEAT(veh, -1, false), veh, flag ? 7 : 8, 250.f); // 7 = left; 8 = right
+				TASK_VEHICLE_TEMP_ACTION(GET_PED_IN_VEHICLE_SEAT(veh, -1, false), veh, flag ? 7 : 8, 250); // 7 = left; 8 = right
 			}
 		}
 		flag = !flag;
