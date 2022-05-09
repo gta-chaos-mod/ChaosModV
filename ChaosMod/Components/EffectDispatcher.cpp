9dd71e675b64a5e750d1f56db2bccce10bc027d7
Reguas <64607261+Reguas@users.noreply.github.com>
2022-05-09 13:38:28 +0300

    Fixed timer going on during pause

--- ChaosMod/Components/EffectDispatcher.cpp
+++ ChaosMod/Components/EffectDispatcher.cpp
@@ -80,7 +80,8 @@ void EffectDispatcher::UpdateTimer()
 {
 	DWORD64 ullCurrentUpdateTime = GetTickCount64();
 
-	if (!m_bEnableNormalEffectDispatch || m_bPauseTimer || MetaModifiers::m_bDisableChaos)
+	if (!m_bEnableNormalEffectDispatch || m_bPauseTimer || MetaModifiers::m_bDisableChaos
+	    || ullCurrentUpdateTime - m_ullTimerTimer > 1000)
 	{
 		m_ullTimerTimer = ullCurrentUpdateTime;
 		return;
@@ -634,12 +635,8 @@ void EffectDispatcher::Reset()
 
 void EffectDispatcher::ResetTimer()
 {
-<<<<<<< HEAD
-	m_fTimerPercentage = 0;
-	m_ullEffectsTimer  = GetTickCount64();
-	== == == = m_fTimerPercentage = 0.f;
+	m_fTimerPercentage = 0.f;
 	m_ullTimerTimer = m_ullEffectsTimer = GetTickCount64();
->>>>>>> c52da04 (Fixed timer going on while mod is disabled)
 }
 
 float EffectDispatcher::GetEffectTopSpace()
