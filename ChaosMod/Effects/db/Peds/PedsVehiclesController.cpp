2ab067d72dc0d52d00da8459758c650f86d1c863
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-05-11 12:04:15 -0400

    Initial Commit

---
+++ ChaosMod/Effects/db/Peds/PedsVehiclesController.cpp
@@ -0,0 +1,85 @@
+#include "stdafx.h"
+
+static void OnStartScooters()
+{
+	static const Hash faggioHash = GET_HASH_KEY("FAGGIO");
+	SetSurroundingPedsInVehicles(faggioHash, -1);
+}
+
+static RegisterEffect registerEffect(EFFECT_PEDS_SCOOTERBROTHERS, OnStartScooters, EffectInfo //by pongo1231
+	{
+		.Name = "Scooter Brothers",
+		.Id = "peds_scooterbrothers",
+		.EEffectGroupType = EEffectGroupType::TrafficSpawner
+	}
+);
+
+static void OnStartBuses()
+{
+	static const Hash busHash = GET_HASH_KEY("BUS");
+	SetSurroundingPedsInVehicles(busHash, 120.f);
+}
+
+static RegisterEffect registerEffect2(EFFECT_PEDS_BUSBOIS, OnStartBuses, EffectInfo //by ProfessorBiddle
+	{
+		.Name = "Bus Bois",
+		.Id = "peds_busbois",
+		.EEffectGroupType = EEffectGroupType::TrafficSpawner
+	}
+);
+
+static void OnStartTanks()
+{
+	static const Hash tankHash = GET_HASH_KEY("rhino");
+	SetSurroundingPedsInVehicles(tankHash, 120.f);
+}
+
+static RegisterEffect registerEffect3(EFFECT_PEDS_TANK_BOIS, OnStartTanks, nullptr, nullptr, EffectInfo // by Last0xygen
+	{
+		.Name = "Tanks A Lot",
+		.Id = "peds_tank_bois",
+		.EEffectGroupType = EEffectGroupType::TrafficSpawner
+	}
+);
+
+static void OnStartTrains()
+{
+	static const Hash trainHash = GET_HASH_KEY("FREIGHT");
+	SetSurroundingPedsInVehicles(trainHash, -1);
+}
+
+static RegisterEffect registerEffect4(EFFECT_PEDS_TRAINS, OnStartTrains, EffectInfo //by OnlyRealNubs
+	{
+		.Name = "Freight Friends",
+		.Id = "peds_trains",
+		.EEffectGroupType = EEffectGroupType::TrafficSpawner
+	}
+);
+
+static void OnStartDozers()
+{
+	static const Hash dozerHash = GET_HASH_KEY("BULLDOZER");
+	SetSurroundingPedsInVehicles(dozerHash, -1);
+}
+
+static RegisterEffect registerEffect5(EFFECT_PEDS_DOZERS, OnStartDozers, EffectInfo //by OnlyRealNubs
+	{
+		.Name = "Dozer Dudes",
+		.Id = "peds_dozers",
+		.EEffectGroupType = EEffectGroupType::TrafficSpawner
+	}
+);
+
+static void OnStartMowers()
+{
+	static const Hash mowerHash = GET_HASH_KEY("MOWER");
+	SetSurroundingPedsInVehicles(mowerHash, -1);
+}
+
+static RegisterEffect registerEffect6(EFFECT_PEDS_MOWERS, OnStartMowers, EffectInfo //by OnlyRealNubs
+	{
+		.Name = "Mower Mates",
+		.Id = "peds_mowers",
+		.EEffectGroupType = EEffectGroupType::TrafficSpawner
+	}
+);
--- ConfigApp/Effects.cs
+++ ConfigApp/Effects.cs
@@ -355,6 +355,9 @@ namespace ConfigApp
             EFFECT_MISC_WATER,
             EFFECT_VEHS_PROP_MODELS,
             EFFECT_VEHS_TINY,
+            EFFECT_PEDS_TRAINS,
+            EFFECT_PEDS_DOZERS,
+            EFFECT_PEDS_MOWERS,
             _EFFECT_ENUM_MAX
         }
 
@@ -674,6 +677,9 @@ namespace ConfigApp
             {EffectType.EFFECT_MISC_WATER, new EffectInfo("Drought", EffectCategory.MISC, "misc_remove_water", true)},
             {EffectType.EFFECT_VEHS_PROP_MODELS, new EffectInfo("Prop Cars", EffectCategory.VEHICLE, "vehs_prop_models", true)},
             {EffectType.EFFECT_VEHS_TINY, new EffectInfo("Tiny Vehicles", EffectCategory.VEHICLE, "vehs_tiny", true)},
+            {EffectType.EFFECT_PEDS_TRAINS, new EffectInfo("Freight Friends", EffectCategory.PEDS, "peds_trains")},
+            {EffectType.EFFECT_PEDS_DOZERS, new EffectInfo("Dozer Dudes", EffectCategory.PEDS, "peds_dozers")},
+            {EffectType.EFFECT_PEDS_MOWERS, new EffectInfo("Mower Mates", EffectCategory.PEDS, "peds_mowers")},
         };
     }
 }
