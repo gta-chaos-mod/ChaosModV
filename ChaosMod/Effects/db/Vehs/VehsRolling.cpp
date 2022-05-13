fa08cbb7f2ec4f4c6e92c0af2dcad6a4cbc29e3b
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-05-13 14:14:32 -0400

    Update VehsRolling.cpp

--- ChaosMod/Effects/db/Vehs/VehsRolling.cpp
+++ ChaosMod/Effects/db/Vehs/VehsRolling.cpp
@@ -4,28 +4,31 @@
 
 #include "stdafx.h"
 
-static const float force = 13.f;
+static const float force = 20.f;
 static const float offsetX = 0.3;
 
 static void OnTick()
 {
-	static int count = 5;
+	static int count = 7;
 
 	for (Vehicle veh : GetAllVehs())
 	{
-		bool available = IS_VEHICLE_SEAT_FREE(veh, -1, false) ? true : !IS_PED_A_PLAYER(GET_PED_IN_VEHICLE_SEAT(veh, -1, false));
-
-		if (available)
+		if (IS_VEHICLE_SEAT_FREE(veh, -1, false) ? true : !IS_PED_A_PLAYER(GET_PED_IN_VEHICLE_SEAT(veh, -1, false)))
 		{
 			Memory::ApplyForceToEntity(veh, 3, 0.f, 0.f, force, -offsetX, 0.f, 0.f, 0, true, true, true, false, true);
 			Memory::ApplyForceToEntity(veh, 3, 0.f, 0.f, -force, offsetX, 0.f, 0.f, 0, true, true, true, false, true);
 			SET_ENTITY_INVINCIBLE(veh, true);
+
+			if (GET_ENTITY_SPEED(veh) < 10)
+			{
+				Vector3 velocity = GET_ENTITY_SPEED_VECTOR(veh, true);
+				SET_ENTITY_VELOCITY(veh, velocity.x * 4, velocity.y, velocity.z);
+			}
 		}
 
 		if (--count == 0)
 		{
-			count = 5;
-
+			count = 7;
 			WAIT(0);
 		}
 	}
@@ -37,11 +40,11 @@ static void OnStop()
 		SET_ENTITY_INVINCIBLE(veh, false);
 }
 
-static RegisterEffect registerEffect(EFFECT_VEHS_ROLLING, nullptr, nullptr, OnTick, EffectInfo
+static RegisterEffect registerEffect(EFFECT_VEHS_ROLLING, nullptr, OnStop, OnTick, EffectInfo
 	{
 		.Name = "Rolling Stones",
 		.Id = "vehs_rolling",
 		.IsTimed = true,
-		.IsShortDuration = true
+		.IsShortDuration = true,
 	}
 );
