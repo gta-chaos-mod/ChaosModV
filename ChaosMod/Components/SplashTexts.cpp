ccbd5cadd2db4d0372205402827b78301802d953
pongo1231 <pongo1999712@gmail.com>
2022-05-13 20:29:26 +0200

    Bump version to 1.10.4

--- ChaosMod/Components/SplashTexts.cpp
+++ ChaosMod/Components/SplashTexts.cpp
@@ -8,7 +8,7 @@ void SplashTexts::OnRun()
 
 	if (m_fStartupSplashTime > 0)
 	{
-		DrawScreenText("Chaos Mod v1.10.3\n\nSee credits.txt for a list of contributors",
+		DrawScreenText("Chaos Mod v1.10.4\n\nSee credits.txt for a list of contributors",
 			{ .2f, .3f }, .65f, { 60, 245, 190 }, true);
 #ifdef _DEBUG
 		DrawScreenText("DEBUG BUILD!", { .2f, .5f }, .7f, { 255, 0, 0 }, true);
