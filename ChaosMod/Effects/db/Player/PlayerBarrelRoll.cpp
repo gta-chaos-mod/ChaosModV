814aee1902a91a9706779fca6ed63a50a12665bd
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-05-13 12:16:31 -0400

    Re-worked effect

--- ChaosMod/Effects/db/Player/PlayerBarrelRoll.cpp
+++ ChaosMod/Effects/db/Player/PlayerBarrelRoll.cpp
@@ -4,54 +4,20 @@
 
 #include "stdafx.h"
 
-Hash vehModel = 1077420264;
-float powerModifier = 4.f;
-
 static void OnStart()
 {
-	Player plr = PLAYER_ID();
-	Ped plrPed = PLAYER_PED_ID();
-	Vehicle veh = 0;
-	if (IS_PED_IN_ANY_VEHICLE(plrPed, false))
+	Ped plr = PLAYER_PED_ID();
+	Vehicle veh = GET_VEHICLE_PED_IS_IN(plr, false);
+
+	if (!DOES_ENTITY_EXIST(veh))
 	{
-		veh = GET_VEHICLE_PED_IS_IN(plrPed, false);
+		Vector3 p = GET_ENTITY_COORDS(plr);
+		Vehicle veh = CreatePoolVehicle(GET_HASH_KEY("buffalo"), p.x, p.y, p.z, GET_ENTITY_HEADING(plr));
+		SET_PED_INTO_VEHICLE(plr, veh, -1);
 	}
-	Vector3 pos = GET_ENTITY_COORDS(plrPed, true);
 
-	Vehicle newVeh = CreatePoolVehicle(vehModel, pos.x, pos.y, 2500, 0);
-	SET_VEHICLE_ENGINE_ON(newVeh, true, true, true);
-	SET_ENTITY_INVINCIBLE(newVeh, true);
-	float speed = GET_VEHICLE_MODEL_ESTIMATED_MAX_SPEED(vehModel) * powerModifier;
-	CONTROL_LANDING_GEAR(newVeh, 3);
-	MODIFY_VEHICLE_TOP_SPEED(newVeh, speed);
-	SET_VEHICLE_FORWARD_SPEED(newVeh, speed);
-
-	if (DOES_ENTITY_EXIST(veh))
-	{	
-		bool hook = IS_ENTITY_A_MISSION_ENTITY(veh);
-
-		int seats = GET_VEHICLE_MODEL_NUMBER_OF_SEATS(GET_ENTITY_MODEL(veh));
-		for (int i = -1; i < seats-1; i++)
-		{
-			Ped ped = GET_PED_IN_VEHICLE_SEAT(veh, i, 0);
-			int seatIndex = i == -1 ? -1 : -2;
-			SET_PED_INTO_VEHICLE(ped, veh, seatIndex);
-		}
-
-		if (hook)
-		{
-			Hooks::EnableScriptThreadBlock();
-			Entity copy = veh;
-			SET_ENTITY_AS_MISSION_ENTITY(copy, true, true);
-			DELETE_VEHICLE(&copy);
-			Hooks::ProxyEntityHandle(veh, newVeh);
-			Hooks::DisableScriptThreadBlock();
-		}
-	}
-	else
-	{
-		SET_PED_INTO_VEHICLE(plrPed, newVeh, -1);
-	}
+	//Roll the vehicle
+	APPLY_FORCE_TO_ENTITY(veh, 3, 0.f, 0.f, 13.f, -0.3f, 0.f, 0.f, 0, true, true, true, false, true);
 }
 
 static RegisterEffect reg(EFFECT_PLAYER_BARREL_ROLL, OnStart, EffectInfo
