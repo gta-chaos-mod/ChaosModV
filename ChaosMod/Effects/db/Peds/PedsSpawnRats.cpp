1d2be8de8eb2dd1768322b49500f4e902702f136
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-05-15 12:11:59 -0400

    Added effect "Spawn Rats"
    Spawns 7 rats at the player. Running out of ideas.

---
+++ ChaosMod/Effects/db/Peds/PedsSpawnRats.cpp
@@ -0,0 +1,25 @@
+/*
+	Effect By OnlyRealNubs
+*/
+
+#include "stdafx.h"
+
+static const int numRats = 7;
+
+static void OnStart()
+{
+	Vector3 pos = GET_ENTITY_COORDS(PLAYER_PED_ID(), true);
+	for (int i = 0; i < numRats; i++)
+	{
+		Ped ped = CreatePoolPed(28, GET_HASH_KEY("a_c_rat"), pos.x, pos.y, pos.z, g_Random.GetRandomFloat(0, 360));
+		SET_PED_AS_GROUP_MEMBER(ped, GET_PLAYER_GROUP(PLAYER_ID()));
+	}
+}
+
+static RegisterEffect registerEffect(EFFECT_SPAWN_RATS, OnStart, EffectInfo
+	{
+		.Name = "Spawn Rats",
+		.Id = "spawn_rats",
+		.EEffectGroupType = EEffectGroupType::SpawnGeneric
+	}
+);
