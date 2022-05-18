ad32d0a0a9d25cf8c0afc02082c28f8fbae8fae2
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-05-18 07:15:54 -0400

    Update Effects.cs

--- ConfigApp/Effects.cs
+++ ConfigApp/Effects.cs
@@ -363,7 +363,7 @@ namespace ConfigApp
             { "misc_invertedcolors", new EffectInfo("Inverted Colors", EffectCategory.MISC, true) },
             { "misc_fourthdimension", new EffectInfo("Fourth Dimension", EffectCategory.MISC, true, true) },
             { "misc_rgbland", new EffectInfo("RGB Land", EffectCategory.MISC, true) },
-            { "player_zeus", new EffectInfo("Smite", EffectCategory.PLAYER, true, false)},
+            { "player_zeus", new EffectInfo("Smite", EffectCategory.PLAYER, true) },
         };
     }
 }
