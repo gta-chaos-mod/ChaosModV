77e2f37de7bcfc65d3b11ca891564a37cd9a4081
Reguas <64607261+Reguas@users.noreply.github.com>
2022-05-26 01:47:07 +0300

    Fixed effect category

--- ConfigApp/Effects.cs
+++ ConfigApp/Effects.cs
@@ -372,7 +372,7 @@ namespace ConfigApp
             { "screen_colorfulworld", new EffectInfo("Colorful World", EffectCategory.Screen, true) },
             { "screen_arc", new EffectInfo("Arced Screen", EffectCategory.Screen, true, true) },
             { "world_blackhole", new EffectInfo("Black Hole", EffectCategory.Misc, true, true) },
-            { "misc_solid_props", new EffectInfo("Solid Props", EffectCategory.MISC, true)},
+            { "misc_solid_props", new EffectInfo("Solid Props", EffectCategory.Misc, true)},
         };
     }
 }
