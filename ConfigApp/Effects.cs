5fea8d31d258854c7c8c6127f27cc3cd61f85ecc
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2022-06-03 11:21:24 -0400

    Added "A Dark World" Effect

--- ConfigApp/Effects.cs
+++ ConfigApp/Effects.cs
@@ -372,6 +372,7 @@ namespace ConfigApp
             { "screen_colorfulworld", new EffectInfo("Colorful World", EffectCategory.Screen, true) },
             { "screen_arc", new EffectInfo("Arced Screen", EffectCategory.Screen, true, true) },
             { "world_blackhole", new EffectInfo("Black Hole", EffectCategory.Misc, true, true) },
+            { "timecycle_darkworld", new EffectInfo("A Dark World", EffectCategory.Screen, true, true) },
         };
     }
 }
