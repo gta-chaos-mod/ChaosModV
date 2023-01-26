c6dce9fa20a2aeac771e76d2a8f05513bb071eba
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2023-01-26 10:36:02 -0500

    Added 'Random Gravity" effect (#3051)
    * Added 'Random Gravity" effect
    
    Every 5 seconds, random gravity is set (direction:).
    
    * Update MiscGravityController.cpp
    
    * Update Effects.cs
    
    * Update .gitmodules
    
    * Fix gitmodiles diff
    
    Co-authored-by: pongo1231 <pongo1999712@gmail.com>

--- ConfigApp/Effects.cs
+++ ConfigApp/Effects.cs
@@ -372,6 +372,7 @@ namespace ConfigApp
             { "screen_colorfulworld", new EffectInfo("Colorful World", EffectCategory.Screen, true) },
             { "screen_arc", new EffectInfo("Arced Screen", EffectCategory.Screen, true, true) },
             { "world_blackhole", new EffectInfo("Black Hole", EffectCategory.Misc, true, true) },
+            { "misc_randomgravity", new EffectInfo("Random Gravity", EffectCategory.Misc, true) },
             { "vehs_disassemble", new EffectInfo("Disassemble Current Vehicle", EffectCategory.Vehicle) },
             { "vehs_detach_wheel", new EffectInfo("Detach Random Wheel", EffectCategory.Vehicle) },
             { "screen_maximap", new EffectInfo("Maximap", EffectCategory.Misc, true) },
