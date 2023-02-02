1a75e755c4f801e61bab9c329090eeaefdd35e8c
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2023-02-01 20:21:47 -0500

    Update TimeController.cpp

--- ChaosMod/Effects/db/Time/TimeController.cpp
+++ ChaosMod/Effects/db/Time/TimeController.cpp
@@ -5,59 +5,68 @@ static void OnStartMorning()
 	SET_CLOCK_TIME(8, 0, 0);
 }
 
-static RegisterEffect registerEffect(OnStartMorning, EffectInfo
+// clang-format off
+REGISTER_EFFECT(OnStartMorning, nullptr, nullptr, EffectInfo
 	{
 		.Name = "Set Time To Morning",
 		.Id = "time_morning",
 		.EffectGroupType = EEffectGroupType::TimeChange
 	}
 );
+// clang-format on
 
 static void OnStartDay()
 {
 	SET_CLOCK_TIME(12, 0, 0);
 }
 
-static RegisterEffect registerEffect2(OnStartDay, EffectInfo
+// clang-format off
+REGISTER_EFFECT(OnStartDay, nullptr, nullptr, EffectInfo
 	{
 		.Name = "Set Time To Daytime",
 		.Id = "time_day",
 		.EffectGroupType = EEffectGroupType::TimeChange
 	}
 );
+// clang-format on
 
 static void OnStartEvening()
 {
 	SET_CLOCK_TIME(18, 0, 0);
 }
 
-static RegisterEffect registerEffect3(OnStartEvening, EffectInfo
+// clang-format off
+REGISTER_EFFECT(OnStartEvening, nullptr, nullptr, EffectInfo
 	{
 		.Name = "Set Time To Evening",
 		.Id = "time_evening",
 		.EffectGroupType = EEffectGroupType::TimeChange
 	}
 );
+// clang-format on
 
 static void OnStartNight()
 {
 	SET_CLOCK_TIME(0, 0, 0);
 }
 
-static RegisterEffect registerEffect4(OnStartNight, EffectInfo
+// clang-format off
+REGISTER_EFFECT(OnStartNight, nullptr, nullptr, EffectInfo
 	{
 		.Name = "Set Time To Night",
 		.Id = "time_night",
 		.EffectGroupType = EEffectGroupType::TimeChange
 	}
 );
+// clang-format on
 
 static void OnTickTimelapse()
 {
 	ADD_TO_CLOCK_TIME(0, 1, 0);
 }
 
-static RegisterEffect registerEffect5(nullptr, nullptr, OnTickTimelapse, EffectInfo
+// clang-format off
+REGISTER_EFFECT(nullptr, nullptr, OnTickTimelapse, EffectInfo
 	{
 		.Name = "Timelapse",
 		.Id = "time_quickday",
@@ -65,6 +74,7 @@ static RegisterEffect registerEffect5(nullptr, nullptr, OnTickTimelapse, EffectI
 		.EffectGroupType = EEffectGroupType::TimeChange
 	}
 );
+// clang-format on
 
 static void OnStartSystem()
 {
@@ -75,7 +85,8 @@ static void OnStartSystem()
 	SET_CLOCK_DATE(day, month, year);
 }
 
-static RegisterEffect registerEffect6(OnStartSystem, EffectInfo
+// clang-format off
+REGISTER_EFFECT(OnStartSystem, nullptr, nullptr, EffectInfo
 	{
 		.Name = "Local Time",
 		.Id = "time_local_time",
