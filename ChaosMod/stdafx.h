a64b8cb6c2dac935b20a5edba5eb7531100287f4
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2023-03-14 09:02:41 -0400

    Fix stdafx.h

--- ChaosMod/stdafx.h
+++ ChaosMod/stdafx.h
@@ -2,45 +2,44 @@
 
 #include "Effects/Effect.h"
 
-#include "Util/Logging.h"
-#include "Util/Colour.h"
-#include "Util/Random.h"
-#include "Util/Hash.h"
+#include "Util/EntityIterator.h"
 #include "Util/File.h"
-#include "Util/TryParse.h"
-#include "Util/Script.h"
+#include "Util/Hash.h"
+#include "Util/Logging.h"
+#include "Util/Model.h"
 #include "Util/Natives.h"
 #include "Util/PoolSpawner.h"
-#include "Util/Model.h"
-#include "Util/EntityIterator.h"
+#include "Util/Random.h"
+#include "Util/Script.h"
+#include "Util/TryParse.h"
 
-
+#include <Patterns/Patterns.h>
+#include <minhook/include/MinHook.h>
 #include <scripthookv/inc/main.h>
 #include <scripthookv/inc/natives.h>
-#include <minhook/include/MinHook.h>
-#include <Patterns/Patterns.h>
 #define SOL_ALL_SAFETIES_ON 1
 #define SOL_SAFE_NUMERICS 1
 #include <sol3/sol.hpp>
 
 #define WIN32_LEAN_AND_MEAN
-#include <windows.h>
-#include <winuser.h>
-#include <psapi.h>
-#include <dbghelp.h>
-#include <tlhelp32.h>
-#include <mmsystem.h>
-#include <dxgi.h>
 #include <d3d11.h>
 #include <d3dcompiler.h>
+#include <dbghelp.h>
+#include <dxgi.h>
+#include <mmsystem.h>
+#include <psapi.h>
+#include <tlhelp32.h>
+#include <windows.h>
+#include <winuser.h>
 
-#include <fstream>
 #include <array>
-#include <string>
-#include <sstream>
-#include <memory>
-#include <list>
 #include <filesystem>
-#include <map>
-#include <unordered_map>
+#include <fstream>
 #include <future>
+#include <list>
+#include <map>
+#include <memory>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
