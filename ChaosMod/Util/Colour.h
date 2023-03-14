4843ffa288c9a6c6deaff35fa4b7839f3589624e
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2023-03-14 09:08:03 -0400

    Update Colour.h

--- ChaosMod/Util/Colour.h
+++ ChaosMod/Util/Colour.h
@@ -1,4 +1,20 @@
-struct CRGBA
+#pragma once
+
+#include <stdafx.h>
+
+struct RGBA
 {
 	unsigned int R, G, B, A;
 };
+
+// Returns a random RGBA value without a random alpha: 255 or max.
+inline RGBA GetRandomColourRGB(unsigned int min = 0, unsigned int max = 255)
+{
+	return { (unsigned int)GetRandomInt(min, max), (unsigned int)GetRandomInt(min, max), (unsigned int)GetRandomInt(min, max), max };
+}
+
+	// Returns a random RGBA value with a random alpha.
+inline RGBA GetRandomColourRGBA(unsigned int min = 0, unsigned int max = 255)
+{
+	return { (unsigned int)GetRandomInt(min, max), (unsigned int)GetRandomInt(min, max), (unsigned int)GetRandomInt(min, max), (unsigned int)GetRandomInt(min, max) };
+}
