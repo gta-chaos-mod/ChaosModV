c5b922153f7874e6c030549d4a01231698c98efa
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2023-03-14 09:11:59 -0400

    Update MiscEsp.cpp

--- ChaosMod/Effects/db/Misc/MiscEsp.cpp
+++ ChaosMod/Effects/db/Misc/MiscEsp.cpp
@@ -1,10 +1,12 @@
-#include "stdafx.h"
+#include <stdafx.h>
+
 #include "Util/Graphics.h"
+#include "Util/Colour.h"
 
 static const float maxDistance = 75.f;
 static const float lineThicknes = 0.001f;
 
-static CRGBA RGBA;
+static RGBA boxColour;
 
 static bool WithinDistance(Ped from, Ped to)
 {
@@ -18,19 +20,18 @@ static bool WithinDistance(Ped from, Ped to)
 
 static void OnStart()
 {
-	RGBA = g_Random.GetRandomColourRGB();
+	boxColour = GetRandomColourRGB();
 }
 
 static void OnTick()
 {
-	static Ped playerPed = PLAYER_PED_ID();
 	for (Ped ped : GetAllPeds())
 	{
-		if (IS_ENTITY_ON_SCREEN(ped) && !IS_ENTITY_DEAD(ped, false) && ped != playerPed && WithinDistance(playerPed, ped))
+		if (IS_ENTITY_ON_SCREEN(ped) && !IS_ENTITY_DEAD(ped, false) && !IS_PED_A_PLAYER(ped) && WithinDistance(playerPed, ped))
 		{
 			Vector3 coords = GET_PED_BONE_COORDS(ped, 0x796E, 0.f, 0.f, 0.f); // Head
 			SET_DRAW_ORIGIN(coords.x, coords.y, coords.z, 0);
-			Util::DRAW_2D_BOX(0, 0, 0.032, 0.073, RGBA, 0.001);
+			Util::DRAW_2D_BOX(0, 0, 0.032, 0.073, boxColour, 0.001);
 			CLEAR_DRAW_ORIGIN();
 		}
 	}
