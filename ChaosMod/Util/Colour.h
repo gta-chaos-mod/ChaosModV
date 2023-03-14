514f4112ca38c33f808c472672c7367d28d59f88
OnlyRealNubs <91900600+OnlyRealNubs@users.noreply.github.com>
2023-03-14 09:08:46 -0400

    Fix comment alignement

--- ChaosMod/Util/Colour.h
+++ ChaosMod/Util/Colour.h
@@ -13,7 +13,7 @@ inline RGBA GetRandomColourRGB(unsigned int min = 0, unsigned int max = 255)
 	return { (unsigned int)GetRandomInt(min, max), (unsigned int)GetRandomInt(min, max), (unsigned int)GetRandomInt(min, max), max };
 }
 
-	// Returns a random RGBA value with a random alpha.
+// Returns a random RGBA value with a random alpha.
 inline RGBA GetRandomColourRGBA(unsigned int min = 0, unsigned int max = 255)
 {
 	return { (unsigned int)GetRandomInt(min, max), (unsigned int)GetRandomInt(min, max), (unsigned int)GetRandomInt(min, max), (unsigned int)GetRandomInt(min, max) };
