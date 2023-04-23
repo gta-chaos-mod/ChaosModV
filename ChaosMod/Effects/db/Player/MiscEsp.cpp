067fdbe7edc0b12bc1b928c566f7c8040fa07e0f
MoneyWasted <45946092+MoneyWasted@users.noreply.github.com>
2023-04-23 17:57:51 -0400

    code optimization

--- ChaosMod/Effects/db/Player/MiscEsp.cpp
+++ ChaosMod/Effects/db/Player/MiscEsp.cpp
@@ -12,13 +12,21 @@ static const float thickness   = 0.001f;
 static Color lineColor;
 
 // clang-format off
-static const int BONE_IDS[] = {
+static const std::array<int, 19> BONE_IDS = {
 	0x796e, 0x9995, 0xfcd9, 0x58b7,
 	0xb1c5, 0xeeeb, 0x49d9, 0x29d2,
 	0x0bb0, 0x9d4d, 0x6e5c, 0xdead,
 	0x2e28, 0xe39f, 0xf9bb, 0xca72,
 	0x9000, 0x3779, 0xcc4d
 };
+
+constexpr std::array<std::array<int, 2>, 14> connections = {{
+	{  0,  1 }, {  1,  4 }, {  4,  5 }, {  5,  6 },
+	{  1,  9 }, {  9, 10 }, { 10, 11 }, {  1, 12 },
+	{ 12, 13 }, { 13, 14 }, { 12, 15 }, { 15, 16 },
+	{ 14, 17 }, { 16, 18 },
+}};
+// clang-format on
 
 Vec2 GetBoneScreenCoords(Ped ped, int boneID)
 {
@@ -28,21 +36,22 @@ Vec2 GetBoneScreenCoords(Ped ped, int boneID)
 	return screenCoords;
 }
 
-void DrawSkeleton(Vec2 points[], int connections[][2], int num_connections, Color lineColor, float thickness)
+void DrawSkeleton(const std::array<Vec2, 19> &points, const std::array<std::array<int, 2>, 14> &connections,
+                  Color lineColor, float thickness)
 {
-    for (int i = 0; i < num_connections; i++)
-    {
-        Vec2 pointA = points[connections[i][0]];
-        Vec2 pointB = points[connections[i][1]];
+	for (const auto &connection : connections)
+	{
+		Vec2 pointA      = points[connection[0]];
+		Vec2 pointB      = points[connection[1]];
 
-        bool validPointA = pointA.x > 0 && pointA.x < 1 && pointA.y > 0 && pointA.y < 1;
-        bool validPointB = pointB.x > 0 && pointB.x < 1 && pointB.y > 0 && pointB.y < 1;
+		bool validPointA = pointA.x > 0 && pointA.x < 1 && pointA.y > 0 && pointA.y < 1;
+		bool validPointB = pointB.x > 0 && pointB.x < 1 && pointB.y > 0 && pointB.y < 1;
 
-        if (validPointA && validPointB)
-        {
-            Hooks::DrawLine(pointA, pointB, lineColor, thickness);
-        }
-    }
+		if (validPointA && validPointB)
+		{
+			Hooks::DrawLine(pointA, pointB, lineColor, thickness);
+		}
+	}
 }
 
 static bool WithinDistance(Ped from, Ped to)
@@ -64,26 +73,18 @@ static void OnStart()
 static void OnTick()
 {
 	Ped playerPed = PLAYER_PED_ID();
-	for (Ped ped : GetAllPeds())
+	for (auto ped : GetAllPeds())
 	{
 		if (IS_ENTITY_ON_SCREEN(ped) && !IS_ENTITY_DEAD(ped, false) && !IS_PED_A_PLAYER(ped)
 		    && WithinDistance(playerPed, ped))
 		{
-			Vec2 points[sizeof(BONE_IDS) / sizeof(BONE_IDS[0])] {};
-			for (size_t i = 0; i < sizeof(BONE_IDS) / sizeof(BONE_IDS[0]); i++)
+			std::array<Vec2, BONE_IDS.size()> points {};
+			for (size_t i = 0; i < BONE_IDS.size(); i++)
 			{
 				points[i] = GetBoneScreenCoords(ped, BONE_IDS[i]);
 			}
 
-			// clang-format off
-			int connections[][2] = {
-				{  0,  1 }, {  1,  4 }, {  4,  5 }, {  5,  6 },
-				{  1,  9 }, {  9, 10 }, { 10, 11 }, {  1, 12 },
-				{ 12, 13 }, { 13, 14 }, { 12, 15 }, { 15, 16 },
-				{ 14, 17 }, { 16, 18 },
-			};
-
-			DrawSkeleton(points, connections, sizeof(connections) / sizeof(connections[0]), lineColor, thickness);
+			DrawSkeleton(points, connections, lineColor, thickness);
 		}
 	}
 }
