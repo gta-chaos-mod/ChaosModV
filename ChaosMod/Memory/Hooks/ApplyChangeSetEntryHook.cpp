39d487da2f5b49088a930ff6f9cd80084cef0ce3
pongo1231 <pongo1999712@gmail.com>
2023-06-17 18:52:43 +0200

    ChaosMod: Add b2944 nng dlc stub files to blacklist
    R* sure loves teasing us with broken assets from e&e but god forbid we get the actual real deal

--- ChaosMod/Memory/Hooks/ApplyChangeSetEntryHook.cpp
+++ ChaosMod/Memory/Hooks/ApplyChangeSetEntryHook.cpp
@@ -52,6 +52,17 @@ void HK_ApplyChangeSetEntryStub(ChangeSetEntry *entry)
 		"dlc_mpSum2_g9ec:/common/data/effects/peds/first_person_alternates.meta",
 		"dlc_mpSum2_g9ec:/common/data/effects/peds/first_person.meta",
 		"dlc_mpSum2_g9ecCRC:/common/data/pedalternatevariations.meta",
+
+		"dlc_mpChristmas3_G9EC:/x64/levels/mpChristmas3_G9EC/vehiclemods/entity3hsw_mods.rpf",
+		"dlc_mpChristmas3_G9EC:/x64/levels/mpChristmas3_G9EC/vehiclemods/issi8hsw_mods.rpf",
+
+		"dlc_mp2023_01_G9EC:/common/data/overlayinfo.xml",
+		"dlc_mp2023_01_G9EC:/x64/levels/gta5/mp2023_01_g9ec_additions/mp2023_01_g9ec_additions.rpf",
+		"dlc_mp2023_01_G9EC:/x64/levels/gta5/mp2023_01_g9ec_additions/mp2023_01_g9ec_additions_metadata.rpf",
+		"dlc_mp2023_01_G9EC:/x64/levels/mp2023_01_G9EC/vehiclemods/buffalo5hsw_mods.rpf",
+		"dlc_mp2023_01_G9EC:/x64/levels/mp2023_01_G9EC/vehiclemods/coureurhsw_mods.rpf",
+		"dlc_mp2023_01_G9EC:/x64/levels/mp2023_01_G9EC/vehiclemods/monstrocitihsw_mods.rpf",
+		"dlc_mp2023_01_G9EC:/x64/levels/mp2023_01_G9EC/vehiclemods/stingertthsw_mods.rpf",
 	};
 
 	if (entry->type == 6 || entry->type == 7 || !entry->dataFile || !badFiles.contains(entry->dataFile->name))
