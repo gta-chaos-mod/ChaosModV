f22fe6de22d44e7b24d3902364d71cbc19ed09a7
Reguas <64607261+Reguas@users.noreply.github.com>
2023-07-06 21:46:06 +0300

    Added effect "Fake U-Turn"

--- ChaosMod/Effects/db/Misc/MiscUTurn.cpp
+++ ChaosMod/Effects/db/Misc/MiscUTurn.cpp
@@ -1,5 +1,7 @@
 #include <stdafx.h>
 
+#include <Components/EffectDispatcher.h>
+
 /*
  * Effect by juhana
  */
@@ -36,10 +38,31 @@ static void OnStart()
 	SET_GAMEPLAY_CAM_RELATIVE_HEADING(camHeading);
 }
 
+// effect by Reguas
+
+static void OnStartFake()
+{
+	OnStart();
+
+	GetComponent<EffectDispatcher>()->OverrideEffectNameId("misc_fakeuturn", "misc_uturn");
+
+	WAIT(g_Random.GetRandomInt(6000, 9000));
+
+	OnStart();
+}
+
 // clang-format off
 REGISTER_EFFECT(OnStart, nullptr, nullptr, EffectInfo
 	{
 		.Name = "U-Turn",
 		.Id = "misc_uturn"
+	}
+);
+
+// clang-format off
+REGISTER_EFFECT(OnStartFake, nullptr, nullptr, EffectInfo
+	{
+		.Name = "Fake U-Turn",
+		.Id = "misc_fakeuturn"
 	}
 );
