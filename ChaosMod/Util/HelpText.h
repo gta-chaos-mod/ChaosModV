718c0a11481e1f7a8950554d10b1c09702e3e3cb
pongo1231 <pongo1999712@gmail.com>
2023-07-25 15:33:33 +0200

    ChaosMod: Use hashed text as label in DisplayHelpText
    This would always use the same label before due to the __FILE__ macro evaluating to the same file

--- ChaosMod/Util/HelpText.h
+++ ChaosMod/Util/HelpText.h
@@ -8,31 +8,20 @@
 
 __forceinline void DisplayHelpText(const std::string &text, std::uint8_t durationSecs = 0)
 {
-#define __FILENAME__                                        \
-	(strrchr(__FILE__, '\\')  ? strrchr(__FILE__, '\\') + 1 \
-	 : strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1  \
-	                          : __FILE__)
-#define STRINGIFY(a) #a
-
-	auto label = STRINGIFY(__FILENAME__) STRINGIFY(__LINE__);
-
-	Hooks::AddCustomLabel(label, text);
-
-#undef STRINGIFY
-#undef __FILENAME__
+	Hooks::AddCustomLabel(text, text);
 
 	if (durationSecs == 0)
 	{
 		if (!IS_HELP_MESSAGE_BEING_DISPLAYED()
-		    || (BEGIN_TEXT_COMMAND_IS_THIS_HELP_MESSAGE_BEING_DISPLAYED(label),
+		    || (BEGIN_TEXT_COMMAND_IS_THIS_HELP_MESSAGE_BEING_DISPLAYED(text.data()),
 		        END_TEXT_COMMAND_IS_THIS_HELP_MESSAGE_BEING_DISPLAYED(0)))
 		{
-			BEGIN_TEXT_COMMAND_DISPLAY_HELP(label);
+			BEGIN_TEXT_COMMAND_DISPLAY_HELP(text.data());
 			END_TEXT_COMMAND_DISPLAY_HELP(0, false, false, 0);
 		}
 	}
 	else if (ComponentExists<HelpTextQueue>())
 	{
-		GetComponent<HelpTextQueue>()->DisplayLabel(label, durationSecs);
+		GetComponent<HelpTextQueue>()->DisplayLabel(text, durationSecs);
 	}
 }
