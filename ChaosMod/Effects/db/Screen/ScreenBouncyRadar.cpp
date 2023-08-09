ad6f8e5978c5f929a577c8a784ff99e6263e0ca4
SRG27 <59260202+SRG27@users.noreply.github.com>
2023-08-09 17:24:36 +0200

    Ran clang-format; changed 9/16 to 9.f/16

--- ChaosMod/Effects/db/Screen/ScreenBouncyRadar.cpp
+++ ChaosMod/Effects/db/Screen/ScreenBouncyRadar.cpp
@@ -1,5 +1,5 @@
-#include <stdafx.h>
 #include "Memory/UI.h"
+#include <stdafx.h>
 
 static float xOffset, yOffset, xVelocity, yVelocity, xMin, xMax, yMin, yMax;
 
@@ -9,19 +9,22 @@ static void OnStart()
 	int screenWidth, screenHeight;
 	_GET_ACTIVE_SCREEN_RESOLUTION(&screenWidth, &screenHeight);
 
-	float multiplier = ((float)screenWidth / (float)screenHeight) * (9 / 16);
+	float multiplier = ((float)screenWidth / (float)screenHeight) * (9.f / 16);
 
-	xOffset = 0.f;
-	yOffset = 0.f;
-
-	if (multiplier > 1) {
+	if (multiplier > 1)
+	{
 		xMin = -0.005f * safezoneSize * multiplier - (multiplier - 1) / 2;
-	} else {
+	}
+	else
+	{
 		xMin = -0.005f * safezoneSize * multiplier;
 	}
-	xMax = multiplier - 0.14f + xMin;
-	yMax = 0.005f * safezoneSize;
-	yMin = yMax - 0.825f;
+	xMax      = multiplier - 0.14f + xMin;
+	yMax      = 0.005f * safezoneSize;
+	yMin      = yMax - 0.825f;
+
+	xOffset   = 0.f;
+	yOffset   = 0.f;
 
 	xVelocity = g_Random.GetRandomFloat(0.0025f, 0.01f) * (g_Random.GetRandomInt(0, 1) ? -1 : 1);
 	yVelocity = g_Random.GetRandomFloat(0.0025f, 0.01f) * (g_Random.GetRandomInt(0, 1) ? -1 : 1);
