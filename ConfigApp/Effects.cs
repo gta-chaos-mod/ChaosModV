462d8f323a7c3648bb0fa5ba5ed2693b49846684
Hamish <134974775+Veloscocity@users.noreply.github.com>
2023-09-11 18:39:51 -0400

    New effect: Cocktail shaker
    final

--- ConfigApp/Effects.cs
+++ ConfigApp/Effects.cs
@@ -411,6 +411,7 @@ namespace ConfigApp
             { "misc_fakeuturn", new EffectInfo("Fake U-Turn", EffectCategory.Misc) },
             { "misc_esp", new EffectInfo("ESP", EffectCategory.Misc, true) },
             { "screen_bouncyradar", new EffectInfo("Bouncy Radar", EffectCategory.Screen, true) },
+            { "cocktail_shaker", new EffectInfo("Cocktail Shaker", EffectCategory.Misc, true) },
         };
     }
 }
