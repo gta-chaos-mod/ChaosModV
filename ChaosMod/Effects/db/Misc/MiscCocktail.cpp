de5239c728a14b5d026c1d47d79dc8ddfa6d765e
Hamish <134974775+Veloscocity@users.noreply.github.com>
2023-09-12 00:07:25 -0400

    Clang format

--- ChaosMod/Effects/db/Misc/MiscCocktail.cpp
+++ ChaosMod/Effects/db/Misc/MiscCocktail.cpp
@@ -2,9 +2,9 @@
 // effect by veloscocity
 //
 
+#include "Memory/Gravity.h"
 #include <math.h>
 #include <stdafx.h>
-#include "Memory/Gravity.h"
 
 #define PI 3.14159265
 
@@ -13,10 +13,12 @@ static float x, y;
 static DWORD lastTick = 0;
 static void OnStart()
 {
-	Ped playerPed                 = PLAYER_PED_ID();
-	float rot = GET_ENTITY_HEADING(IS_PED_IN_ANY_VEHICLE(playerPed, false) ? GET_VEHICLE_PED_IS_IN(playerPed, false) : playerPed); //Shake perpendicular to the player
-	x         = sin((360 - rot) * PI / 180) * 1.33;
-	y         = -cos((360 - rot) * PI / 180) * 1.33;
+	Ped playerPed = PLAYER_PED_ID();
+	float rot =
+	    GET_ENTITY_HEADING(IS_PED_IN_ANY_VEHICLE(playerPed, false) ? GET_VEHICLE_PED_IS_IN(playerPed, false)
+	                                                               : playerPed); // Shake perpendicular to the player
+	x = sin((360 - rot) * PI / 180) * 1.33;
+	y = -cos((360 - rot) * PI / 180) * 1.33;
 }
 static void OnTick()
 {
@@ -24,9 +26,9 @@ static void OnTick()
 	if (lastTick < curTick - 500)
 	{
 		lastTick = curTick;
-		x = -x;
-		y = -y;
-    }
+		x        = -x;
+		y        = -y;
+	}
 
 	for (auto object : GetAllProps())
 		Memory::ApplyForceToEntityCenterOfMass(object, 1, y, x, 0, false, false, true, false);
