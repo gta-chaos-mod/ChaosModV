6e3eb84863ed11b456fe0c6e8287954f9a881970
Veloscocity <134974775+Veloscocity@users.noreply.github.com>
2023-09-13 13:31:09 -0400

    New Effect: Boost Braking

---
+++ ChaosMod/Effects/db/Vehs/VehsBoostBraking.cpp
@@ -0,0 +1,31 @@
+#include <stdafx.h>
+
+#include "Memory/Physics.h"
+#include "Memory/Vehicle.h"
+
+static void BoostOnTick()
+{
+	static const Hash blimpHash = "BLIMP"_hash;
+
+	for (Vehicle veh : GetAllVehs())
+	{
+		Hash vehModel = GET_ENTITY_MODEL(veh);
+		int vehClass  = GET_VEHICLE_CLASS(veh);
+		float speed   = GET_ENTITY_SPEED_VECTOR(veh, true).y;
+
+		if (vehClass != 15 && vehModel != blimpHash && Memory::IsVehicleBraking(veh) && speed > 1.f
+		    && GET_ENTITY_HEIGHT_ABOVE_GROUND(veh) <= 2.f)
+		{
+			SET_VEHICLE_FORWARD_SPEED(veh, speed * -1.5f);
+		}
+	}
+}
+// clang-format off
+REGISTER_EFFECT(nullptr, nullptr, BoostOnTick, EffectInfo
+	{
+		.Name = "Boost Braking",
+		.Id = "veh_boostbrake",
+		.IsTimed = true,
+		.IncompatibleWith = { "veh_brakeboost" },
+	}
+);
