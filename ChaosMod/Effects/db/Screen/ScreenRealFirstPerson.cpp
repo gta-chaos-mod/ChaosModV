62ae8c86a4d343ffc98a2a0f3faa2be33cf99682
Chanka0 <hahano@gmail.com>
2023-09-15 11:21:46 -0600

    Direct bone rotation, removed bone proxy

--- ChaosMod/Effects/db/Screen/ScreenRealFirstPerson.cpp
+++ ChaosMod/Effects/db/Screen/ScreenRealFirstPerson.cpp
@@ -5,7 +5,6 @@
 #include <stdafx.h>
 
 static Cam eCamera;
-static Entity boneProxy;
 static int state = 0;
 
 static bool SafetyCheck()
@@ -25,10 +24,6 @@ static void RemoveCamera()
 {
 	RENDER_SCRIPT_CAMS(false, false, 0, true, false, 0);
 	CAN_SET_EXIT_STATE_FOR_CAMERA(true);
-	if (DOES_ENTITY_EXIST(boneProxy))
-	{
-		DELETE_ENTITY(&boneProxy);
-	}
 	if (DOES_CAM_EXIST(eCamera))
 	{
 		SET_CAM_ACTIVE(eCamera, false);
@@ -38,16 +33,11 @@ static void RemoveCamera()
 
 static void SetupCamera()
 {
-	// Not sure of a direct way to obtain bone rotation. Attaching a prop is a workaround.
 	Ped playerPed      = PLAYER_PED_ID();
 	Vector3 boneCoords = GET_PED_BONE_COORDS(playerPed, 0x322c, 0, 0, 0);
 	eCamera     = CREATE_CAM_WITH_PARAMS("DEFAULT_SCRIPTED_CAMERA", boneCoords.x, boneCoords.y, boneCoords.z, 0, 0, 0, 60,
 	                                   false, 2);
-	boneProxy = CREATE_OBJECT("p_cs_leaf_s"_hash, boneCoords.x, boneCoords.y, boneCoords.z, false, false, true);
-	SET_ENTITY_ALPHA(boneProxy, 0, false);
-	ATTACH_ENTITY_TO_ENTITY(boneProxy, playerPed, GET_PED_BONE_INDEX(playerPed, 0x322c), 0.1, 0, 0, 0, 90, 0, false,
-	                        false, false, true, 0, true);
-	ATTACH_CAM_TO_ENTITY(eCamera, boneProxy, 0, 0, 0, false);
+	ATTACH_CAM_TO_PED_BONE(eCamera, playerPed, 31086, 0, 0, 0, 0);
 
 	SET_CAM_NEAR_CLIP(eCamera, 0.2);
 	SET_CAM_AFFECTS_AIMING(eCamera, false);
@@ -90,8 +80,9 @@ static void OnTick()
 			}
 		    else
 		    {
-			    Vector3 boneRot = GET_ENTITY_ROTATION(boneProxy, 2);
-			    SET_CAM_ROT(eCamera, boneRot.x, boneRot.y, boneRot.z, 2);
+			    Ped playerPed = PLAYER_PED_ID();
+			    Vector3 boneRot = _GET_ENTITY_BONE_ROTATION(playerPed, GET_PED_BONE_INDEX(playerPed, 0x796e));
+			    SET_CAM_ROT(eCamera, boneRot.x, boneRot.y+90, boneRot.z, 2);
 			}
 			break;
 		//Recovery
