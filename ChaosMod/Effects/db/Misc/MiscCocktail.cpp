04d6f3404538325bd7f52e71ccfbcbca0665e3bb
Veloscocity <134974775+Veloscocity@users.noreply.github.com>
2023-09-22 13:39:28 -0400

    Apply suggestions from code review
    Co-authored-by: pongo1231 <pongo1999712@gmail.com>

--- ChaosMod/Effects/db/Misc/MiscCocktail.cpp
+++ ChaosMod/Effects/db/Misc/MiscCocktail.cpp
@@ -2,9 +2,9 @@
 // effect by veloscocity
 //
 
-#include "Memory/Gravity.h"
-#include <math.h>
 #include <stdafx.h>
+
+#include "Memory/Gravity.h"
 
 #define PI 3.14159265
 
@@ -17,8 +17,8 @@ static void OnStart()
 	float rot =
 	    GET_ENTITY_HEADING(IS_PED_IN_ANY_VEHICLE(playerPed, false) ? GET_VEHICLE_PED_IS_IN(playerPed, false)
 	                                                               : playerPed); // Shake perpendicular to the player
-	x = sin((360 - rot) * PI / 180) * 1.33;
-	y = -cos((360 - rot) * PI / 180) * 1.33;
+	x = sin((360 - rot) * PI / 180) * 1.33f;
+	y = -cos((360 - rot) * PI / 180) * 1.33f;
 }
 static void OnTick()
 {
@@ -31,10 +31,14 @@ static void OnTick()
 	}
 
 	for (auto object : GetAllProps())
+	{
 		Memory::ApplyForceToEntityCenterOfMass(object, 1, y, x, 0, false, false, true, false);
+	}
 
 	for (auto veh : GetAllVehs())
+	{
 		Memory::ApplyForceToEntityCenterOfMass(veh, 1, y, x, 0, false, false, true, false);
+	}
 }
 
 // clang-format off
@@ -43,7 +47,7 @@ REGISTER_EFFECT(OnStart, nullptr, OnTick, EffectInfo
 		.Name = "Cocktail Shaker",
 		.Id = "cocktail_shaker",
 		.IsTimed = true,
-        .IsShortDuration = true,
+		.IsShortDuration = true,
 		.EffectCategory = EffectCategory::Gravity,
 	}
 );
