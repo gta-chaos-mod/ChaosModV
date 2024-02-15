95e625bd318c4fd98a530f5411113660ad38d761
pongo1231 <pongo1999712@gmail.com>
2024-02-15 01:49:12 +0000

    Bump version to 2.1.3

--- ConfigApp/Info.cs
+++ ConfigApp/Info.cs
@@ -2,7 +2,7 @@
 {
     public static class Info
     {
-        public const string VERSION = "2.1.2";
+        public const string VERSION = "2.1.3";
 
         public const string WORKSHOP_DEFAULT_URL = "https://chaos.gopong.dev";
     }
