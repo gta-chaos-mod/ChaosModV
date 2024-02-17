69e51e7bcda1411bd3732bba034dc39a253044a6
Snipz <9103583+holysnipz@users.noreply.github.com>
2024-02-18 03:20:06 +0700

    caching results

--- ChaosMod/Util/Weapon.h
+++ ChaosMod/Util/Weapon.h
@@ -1,11 +1,21 @@
 #pragma once
 
+#include <unordered_map>
+
 using Hash = unsigned long;
 
 namespace Util
 {
 	inline bool IsWeaponShotgun(Hash weaponHash)
 	{
-		return GET_WEAPONTYPE_GROUP(weaponHash) == "GROUP_SHOTGUN"_hash;
+		static std::unordered_map<Hash, bool> cachedResults;
+		static const Hash shotgunGroup = "GROUP_SHOTGUN"_hash;
+
+		if (const auto result = cachedResults.find(weaponHash); result != cachedResults.end())
+		{
+			return result->second;
+		}
+
+		return cachedResults.emplace(weaponHash, (GET_WEAPONTYPE_GROUP(weaponHash) == shotgunGroup)).first->second;
 	}
 }
