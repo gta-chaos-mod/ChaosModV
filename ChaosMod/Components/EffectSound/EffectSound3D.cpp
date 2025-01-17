4bc60c1736e3a93dea1bedd62198014af9839562
pongo1231 <pongo1999712@gmail.com>
2025-01-17 21:46:23 +0000

    ChaosMod/EffectSound3D: Use camera pos for listener
    Also slightly unfucked directional audio, still not quite right tho

--- ChaosMod/Components/EffectSound/EffectSound3D.cpp
+++ ChaosMod/Components/EffectSound/EffectSound3D.cpp
@@ -7,9 +7,6 @@
 #define MINIAUDIO_IMPLEMENTATION
 #include <miniaudio.h>
 
-#define SOUND_RANGE_MULT .1f
-#define SOUND_VEL_MULT .5f
-
 EffectSound3D::EffectSound3D()
 {
 	ma_engine_init(nullptr, &m_maEngine);
@@ -36,17 +33,21 @@ void EffectSound3D::OnRun()
 	if (m_Sounds.empty())
 		return;
 
-	auto playerPed       = PLAYER_PED_ID();
+	auto playerPed    = PLAYER_PED_ID();
 
-	auto adjPlayerCoords = GET_ENTITY_COORDS(playerPed, true) * SOUND_RANGE_MULT;
-	ma_engine_listener_set_position(&m_maEngine, 0, adjPlayerCoords.x, adjPlayerCoords.y, adjPlayerCoords.z);
+	auto adjCamCoords = GET_GAMEPLAY_CAM_COORD();
+	ma_engine_listener_set_position(&m_maEngine, 0, adjCamCoords.x, adjCamCoords.y, adjCamCoords.z);
 
-	auto camRot = Util::DegToRadian(GET_GAMEPLAY_CAM_ROT(2));
+	float camHeading = GET_GAMEPLAY_CAM_RELATIVE_HEADING();
+	float camPitch   = GET_GAMEPLAY_CAM_RELATIVE_PITCH();
+	Vector3 camRot   = { std::cos(Util::DegToRadian(camHeading)) * std::cos(Util::DegToRadian(camPitch)),
+		                 std::sin(Util::DegToRadian(camPitch)),
+		                 std::sin(Util::DegToRadian(camHeading)) * std::cos(Util::DegToRadian(camPitch)) };
 	ma_engine_listener_set_direction(&m_maEngine, 0, camRot.x, camRot.y, camRot.z);
 
 	auto playerVeh        = GET_VEHICLE_PED_IS_IN(playerPed, false);
 	auto playerIsInAnyVeh = IS_PED_IN_ANY_VEHICLE(playerPed, false);
-	auto adjPlayerVel     = GET_ENTITY_VELOCITY(playerIsInAnyVeh ? playerVeh : playerPed) * SOUND_VEL_MULT;
+	auto adjPlayerVel     = GET_ENTITY_VELOCITY(playerIsInAnyVeh ? playerVeh : playerPed);
 	ma_engine_listener_set_velocity(&m_maEngine, 0, adjPlayerVel.x, adjPlayerVel.y, adjPlayerVel.z);
 
 	for (auto it = m_Sounds.begin(); it != m_Sounds.end();)
@@ -66,10 +67,12 @@ void EffectSound3D::OnRun()
 			continue;
 		}
 
+		ma_sound_set_rolloff(&sound.Handle, .1f);
+
 		switch (sound.PlayOptions.PlayType)
 		{
 		case EffectSoundPlayType::FollowPlayer:
-			ma_sound_set_position(&sound.Handle, adjPlayerCoords.x, adjPlayerCoords.y, adjPlayerCoords.z);
+			ma_sound_set_position(&sound.Handle, adjCamCoords.x, adjCamCoords.y, adjCamCoords.z);
 			break;
 		case EffectSoundPlayType::FollowEntity:
 		{
@@ -79,19 +82,18 @@ void EffectSound3D::OnRun()
 				continue;
 			}
 
-			auto adjEntityCoords = GET_ENTITY_COORDS(sound.PlayOptions.Entity, true) * SOUND_RANGE_MULT;
+			auto adjEntityCoords = GET_ENTITY_COORDS(sound.PlayOptions.Entity, true);
 			ma_sound_set_position(&sound.Handle, adjEntityCoords.x, adjEntityCoords.y, adjEntityCoords.z);
 
 			auto entityVeh        = GET_VEHICLE_PED_IS_IN(sound.PlayOptions.Entity, false);
 			auto entityIsInAnyVeh = IS_PED_IN_ANY_VEHICLE(sound.PlayOptions.Entity, false);
-			auto adjEntityVel =
-			    GET_ENTITY_VELOCITY(entityIsInAnyVeh ? entityVeh : sound.PlayOptions.Entity) * SOUND_VEL_MULT;
+			auto adjEntityVel     = GET_ENTITY_VELOCITY(entityIsInAnyVeh ? entityVeh : sound.PlayOptions.Entity);
 			ma_engine_listener_set_velocity(&m_maEngine, 0, adjEntityVel.x, adjEntityVel.y, adjEntityVel.z);
 		}
 		break;
 		case EffectSoundPlayType::AtCoords:
 		{
-			auto adjCoords = sound.PlayOptions.Coords * SOUND_RANGE_MULT;
+			auto adjCoords = sound.PlayOptions.Coords;
 			ma_sound_set_position(&sound.Handle, adjCoords.x, adjCoords.y, adjCoords.z);
 		}
 		break;
