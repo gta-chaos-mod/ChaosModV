51b4994da114a4e77a0c72d45de6f7f475999872
pongo1231 <pongo1999712@gmail.com>
2025-01-22 00:04:17 +0000

    ConfigApp: Add missing entry for "Teleport To The Opposite Side Of Waypoint" effect

--- ConfigApp/Effects.cs
+++ ConfigApp/Effects.cs
@@ -413,6 +413,7 @@
             { "screen_realfp", new EffectInfo("Real First Person", EffectCategory.Screen, true) },
             { "screen_hueshift", new EffectInfo("Hue Shift", EffectCategory.Screen, true) },
             { "player_copyforce", new EffectInfo("Use The Force", EffectCategory.Player, true, true) },
+            { "player_tptowaypointopposite", new EffectInfo("Teleport To The Opposite Side Of Waypoint", EffectCategory.Player) },
         };
     }
 }
