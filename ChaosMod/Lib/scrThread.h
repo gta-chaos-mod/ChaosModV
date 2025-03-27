802d4395c632a779095e1ecbe7919692fbf12081
Regynate <64607261+Regynate@users.noreply.github.com>
2025-03-27 13:57:46 +0300

    ChaosMod: Ported ScriptThreadRunHook to Enhanced

--- ChaosMod/Lib/scrThread.h
+++ ChaosMod/Lib/scrThread.h
@@ -2,6 +2,8 @@
 
 #include <scripthookv/inc/main.h>
 
+#include "game.h"
+
 using DWORD   = unsigned long;
 using DWORD64 = unsigned long long;
 
@@ -26,7 +28,7 @@ namespace rage
 		void **m_NativesPointer;
 		DWORD64 field_0x48;
 		DWORD64 field_0x50;
-		DWORD64 m_ScriptHash;
+		DWORD m_ScriptHash;
 		int field_0x5c;
 		void *m_ScriptNamePointer;
 		DWORD64 *m_StringBlocks;
@@ -39,43 +41,76 @@ namespace rage
 	class scrThread
 	{
 	  private:
-		DWORD m_ThreadId;
-		DWORD m_ScriptHash;
-		DWORD dwSomething2;
-		DWORD m_IP;
-		char pad[184];
-		char pad_2699[4];
-		char m_Name[32];
-		char pad3[100];
-		char chSomething3;
-		char pad4[3];
+		struct scrThreadLegacyPre2699
+		{
+			void *vft;
+			DWORD m_ThreadId;
+			DWORD m_ScriptHash;
+			DWORD dwSomething2;
+			DWORD m_IP;
+			char pad[184];
+			char m_Name[32];
+			char pad3[100];
+			char chSomething3;
+			char pad4[3];
+		};
 
-		virtual ~scrThread()                                 = 0;
+		struct scrThreadLegacy
+		{
+			void *vft;
+			DWORD m_ThreadId;
+			DWORD m_ScriptHash;
+			DWORD dwSomething2;
+			DWORD m_IP;
+			char pad[184];
+			char pad_2699[4];
+			char m_Name[32];
+			char pad3[100];
+			char chSomething3;
+			char pad4[3];
+		};
 
-		virtual DWORD *Reset(int a2, const void *a3, int a4) = 0;
-
-		virtual __int64 OnRun()                              = 0;
-
-		virtual __int64 Update()                             = 0;
-
-		virtual __int64 Kill()                               = 0;
+		struct scrThreadEnhanced
+		{
+			void *vft;
+			DWORD m_ThreadId;
+			char pad0[4];
+			DWORD m_ScriptHash;
+			DWORD dwSomething2;
+			DWORD m_IP;
+			char pad1[184];
+			char pad_Enhanced[128];
+			char m_Name[32];
+			char pad3[100];
+			char chSomething3;
+			char pad4[3];
+		};
 
 	  public:
 		DWORD GetThreadId() const
 		{
-			return m_ThreadId;
+			if (IsLegacy())
+				return reinterpret_cast<const scrThreadLegacy *>(this)->m_ThreadId;
+			else
+				return reinterpret_cast<const scrThreadEnhanced *>(this)->m_ThreadId;
 		}
 
 		DWORD GetHash() const
 		{
-			return m_ScriptHash;
+			if (IsLegacy())
+				return reinterpret_cast<const scrThreadLegacy *>(this)->m_ScriptHash;
+			else
+				return reinterpret_cast<const scrThreadEnhanced *>(this)->m_ScriptHash;
 		}
 
 		const char *GetName() const
 		{
-			return getGameVersion() < eGameVersion::VER_1_0_2699_0_STEAM ? pad_2699 : m_Name;
+			if (getGameVersion() < eGameVersion::VER_1_0_2699_0_STEAM)
+				return reinterpret_cast<const scrThreadLegacyPre2699 *>(this)->m_Name;
+			else if (IsLegacy())
+				return reinterpret_cast<const scrThreadLegacy *>(this)->m_Name;
+			else
+				return reinterpret_cast<const scrThreadEnhanced *>(this)->m_Name;
 		}
 	};
 }
-
-static_assert(sizeof(rage::scrThread) == 352);
