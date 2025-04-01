e336db06be86eec63008878e4595ba733659df56
Regynate <64607261+Regynate@users.noreply.github.com>
2025-04-01 20:56:29 +0300

    Ported minimap functions to Enhanced

--- ChaosMod/Memory/UI.h
+++ ChaosMod/Memory/UI.h
@@ -18,21 +18,21 @@ namespace Memory
 {
 	static void(__cdecl *GetRefreshMinimapFunc())()
 	{
-		Handle handle = Memory::FindPattern("?? 89 5C ?? ?? 57 ?? 83 EC ?? ?? 8D 3D ?? ?? ?? ?? ?? 8D ?? ?? ?? E8");
+		Handle handle = Memory::FindPattern("?? 89 5C ?? ?? 57 ?? 83 EC ?? ?? 8D 3D ?? ?? ?? ?? ?? 8D ?? ?? ?? E8", "56 57 48 83 EC 48 E8 ?? ?? ?? ?? 89");
 		if (!handle.IsValid())
 			return nullptr;
 
 		return handle.Get<void()>();
 	}
 
-	inline void SetRadarParams(std::function<void(MinimapData &, MinimapData)> proceed)
+	inline void SetRadarParams(std::function<void(MinimapData &, MinimapData)> process)
 	{
 		static MinimapData defaultMinimap[3];
 		static void(__cdecl * refreshMinimapFunc)();
 		static auto minimapData = []() -> MinimapData *
 		{
 			Handle handle = Memory::FindPattern(
-			    "?? 8D 15 ?? ?? ?? ?? ?? 6B C9 78 8B 44 ?? ?? 89 03 8B 44 ?? ?? 89 43 04 8A 4C ?? ??");
+			    "?? 8D 15 ?? ?? ?? ?? ?? 6B C9 78 8B 44 ?? ?? 89 03 8B 44 ?? ?? 89 43 04 8A 4C ?? ??", "48 8D 3D ?? ?? ?? ?? F2 0F 10 44 3B 6C");
 			handle = handle.At(2).Into();
 			if (!handle.IsValid())
 				return nullptr;
@@ -50,7 +50,7 @@ namespace Memory
 			return;
 
 		for (size_t i = 0; i < 3; i++)
-			proceed(minimapData[i], defaultMinimap[i]);
+			process(minimapData[i], defaultMinimap[i]);
 
 		refreshMinimapFunc();
 	}
