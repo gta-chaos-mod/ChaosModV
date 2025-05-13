8fc9eba16b1091ff3c652c74c1599389415d95c0
MoneyWasted <45946092+MoneyWasted@users.noreply.github.com>
2025-05-12 22:49:54 -0400

    Started Work on Rivers
    Had to make changes to the vector definitions to make future work easier.

--- ChaosMod/Effects/db/Misc/MiscNoWater.cpp
+++ ChaosMod/Effects/db/Misc/MiscNoWater.cpp
@@ -12,28 +12,47 @@
 CHAOS_VAR CWaterQuad *WaterQuads;
 CHAOS_VAR std::vector<float> WaterHeights;
 
+CHAOS_VAR CRiverQuad *RiverQuads;
+CHAOS_VAR std::vector<float> RiverHeights;
+
 static void OnStart()
 {
+	WaterHeights.reserve(MAXWATERQUADS);
+
 	WaterQuads = Memory::GetAllWaterQuads();
 	if (WaterQuads)
 	{
-		for (int i = 0; i < 821; i++) // 821 = Max Water Items
+		for (int i = 0; i < MAXWATERQUADS; i++)
 		{
 			WaterHeights.push_back(WaterQuads[i].Z); // Save Water Height
 			WaterQuads[i].Z = -1000.0f;              // Remove Water
+		}
+	}
+
+	RiverQuads = Memory::GetAllRiverQuads();
+	if (RiverQuads)
+	{
+		char buffer[256];
+		for (int i = 0; i < MAXRIVERQUADS; i++)
+		{
+			sprintf_s(buffer, "RiverQuads[%d].Entity: 0x%p", i, &RiverQuads[i].Entity);
+			LOG(buffer);
 		}
 	}
 }
 
 static void OnStop()
 {
-	if (WaterQuads)
+	if (WaterQuads && !WaterHeights.empty())
 	{
-		for (int i = 0; i < 821; i++) // 821 = Max Water Items
+		for (int i = 0; i < MAXWATERQUADS; i++)
 		{
-			WaterQuads[i].Z    = WaterHeights.at(i); // Restore Water
-			WaterHeights.at(i) = 0.0f;               // Clear Saved Height
+			WaterQuads[i].Z = WaterHeights[i]; // Restore Water
+			WaterHeights[i] = 0.0f;            // Clear Saved Height
 		}
+
+		WaterHeights.clear();
+		WaterHeights.shrink_to_fit();
 	}
 }
 
